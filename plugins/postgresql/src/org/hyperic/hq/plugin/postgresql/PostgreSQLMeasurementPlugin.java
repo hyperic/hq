c689536db34fa148fc7c93660a3c4826e61ab7d4
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2006-07-27 16:10:33 +0000

    Add inventory and monitoring for PostgreSQL Indexes.  Auto-discovery for these services is currently disabled. [HHQ-21]

--- plugins/postgresql/src/org/hyperic/hq/plugin/postgresql/PostgreSQLMeasurementPlugin.java
+++ plugins/postgresql/src/org/hyperic/hq/plugin/postgresql/PostgreSQLMeasurementPlugin.java
@@ -41,6 +41,8 @@ public class PostgreSQLMeasurementPlugin
 
     protected static final String JDBC_DRIVER = "org.postgresql.Driver";
 
+    protected static final String PROP_INDEX = "index";
+
     //XXX: Could default this to whatever HQ chooses as a db name
     protected static final String DEFAULT_URL = 
         "jdbc:postgresql://localhost/hq";
@@ -131,6 +133,18 @@ public class PostgreSQLMeasurementPlugin
             // Else normal query from pg_stat_user_table
             return "SELECT " + attr + " FROM pg_stat_user_tables " +
                 "WHERE relname='" + table.toLowerCase() + "'";
+        } else if (objectName.indexOf("Type=Index") != -1) {
+
+            String index = metric.getObjectProperties().getProperty(PROP_INDEX);
+            if (isAvail) {
+                // Hardcode for availability.  Cannot have the same
+                // template else we get plugin dumper errors.
+                attr = "idx_scan";
+            }
+
+            // Else normal query from pg_stat_user_table
+            return "SELECT " + attr + " FROM pg_stat_user_indexes " +
+                "WHERE indexrelname='" + index.toLowerCase() + "'";
         }
 
         // Most likely a hq-plugin.xml typo.  JDBCMeasurementPlugin
