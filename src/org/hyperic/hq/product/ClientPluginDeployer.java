3cbdd6cc0fe9e55e18a8879b042022df2d486bac
dougm <dougm@572de364-d518-0410-a359-afb9c36ce0ec>
2006-08-23 18:26:55 +0000

    deployer for agent-side plugin files, scripts/, lib/, etc.

---
+++ src/org/hyperic/hq/product/ClientPluginDeployer.java
@@ -0,0 +1,294 @@
+package org.hyperic.hq.product;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Deployment of embedded plugin files.
+ * Plugins can embed scripts, jars, native libraries, etc.
+ * This class deploys such files on the agent-side only to
+ * pdk/deploy/$type/$plugin/
+ * Where type is "scripts", "lib", etc., plugin is the plugin name.
+ * The pdk/$type directory must exist otherwise deployment is skipped.
+ */
+public class ClientPluginDeployer {
+
+    private static final Log log =
+        LogFactory.getLog(ClientPluginDeployer.class.getName());
+
+    private String plugin;
+    private String pdk;
+
+    private static HashMap handlers = new HashMap();
+
+    private static final String[] EX_DIRS = {
+        "scripts", "lib"
+    };
+
+    private static final String[] DIRS = {
+        "tmp"
+    };
+
+    static {
+        addHandlers(EX_DIRS, true);
+        addHandlers(DIRS, false);
+    }
+
+    public ClientPluginDeployer(String pdk, String type) {
+        this.pdk = pdk;
+        this.plugin = type;
+    }
+
+    public static void addHandler(String dir, boolean isExecutable) {
+        Handler handler = new Handler(dir);
+        handlers.put(handler.getName(), handler);
+        handler.setExecutable(isExecutable);        
+    }
+
+    public static void addHandlers(String[] dirs,
+                                   boolean isExecutable) {
+
+        for (int i=0; i<dirs.length; i++) {
+            addHandler(dirs[i], isExecutable);
+        }        
+    }
+
+    public Handler getHandler(String name) {
+        Handler handler = (Handler)handlers.get(name);
+        if (handler != null) {
+            return handler;
+        }
+        //e.g. "script" -> "scripts"
+        return (Handler)handlers.get(name + 's');
+    }
+
+    public static class Handler {
+        private String name;
+        private boolean isExecutable;
+
+        Handler(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public boolean isExecutable() {
+            return this.isExecutable;
+        }
+
+        public void setExecutable(boolean value) {
+            this.isExecutable = value;
+        }
+
+        public File getSubDirectory(ClientPluginDeployer deployer) {
+            //e.g. check /.../pdk/$name exists
+            File pluginDir = new File(deployer.pdk, this.name);
+            if (!pluginDir.exists()) {
+                return null;
+            }
+            log.debug(pluginDir + " exists");
+
+            String subdir =
+                "deploy" + File.separator +
+                this.name + File.separator +
+                deployer.plugin;
+
+            File dir = new File(deployer.pdk, subdir);
+
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    log.error("mkdir(" + dir + ") failed for plugin: " +
+                              deployer.plugin);
+                    return null;
+                }
+                else {
+                    log.debug("mkdir(" + dir + ") succeeded for plugin: " +
+                              deployer.plugin);
+                }
+            }
+
+            return dir;
+        }
+    }
+
+    public List unpackJar(String jar)
+        throws IOException {
+
+        ArrayList jars = new ArrayList();
+
+        ZipFile zipfile = new ZipFile(jar);
+        for (Enumeration e = zipfile.entries(); e.hasMoreElements();) {
+            ZipEntry entry = (ZipEntry)e.nextElement();
+            String name = entry.getName();
+
+            if (entry.isDirectory()) {
+                continue;
+            }
+
+            int ix = name.indexOf('/');
+            if (ix == -1) {
+                continue;
+            }
+
+            String prefix = name.substring(0, ix);
+            name = name.substring(ix+1);
+
+            File file = getFile(prefix, name);
+            if (file == null) {
+                continue;
+            }
+
+            InputStream is = zipfile.getInputStream(entry);
+            try {
+                write(is, file);
+
+                if (name.endsWith(".jar")) {
+                    jars.add(file.toString());
+                }
+            } catch (IOException ioe) {
+                zipfile.close();
+                throw ioe;
+            } finally {
+                is.close();
+            }
+        }
+
+        zipfile.close();
+
+        return jars;
+    }
+
+    public class PluginFile extends File {
+        //shutup eclipse warning
+        static final long serialVersionUID = 0xff;
+
+        Handler handler;
+
+        public PluginFile(String pathname) {
+            super(pathname);
+        }
+
+        public PluginFile(File parent, String child) {
+            super(parent, child);
+        }
+
+        public PluginFile(String parent, String child) {
+            super(parent, child);
+        }
+    }
+
+    public boolean isDeployableType(String type) {
+        return getHandler(type) != null;
+    }
+
+    public File getFile(String type, String file) {
+
+        Handler handler = getHandler(type);
+
+        if (handler == null) {
+            return null;
+        }
+
+        File dir =
+            handler.getSubDirectory(this);
+
+        if (dir == null) {
+            log.debug("Unable to determine subdirectory to write: " +
+                      file);
+            return null;
+        }
+
+        PluginFile pluginFile = new PluginFile(dir, file);
+        pluginFile.handler = handler;
+        return pluginFile;
+    }
+
+    public void write(String data, File file)
+        throws IOException {
+
+        write(new ByteArrayInputStream(data.getBytes()), file);
+    }
+
+    public void write(InputStream is, File file)
+        throws IOException {
+
+        if (file == null) {
+            return;
+        }
+
+        FileOutputStream os;
+
+        try {
+            os = new FileOutputStream(file);
+            log.debug("Wrote file: " + file);
+        } catch (IOException e) {
+            if (file.exists() && (file.length() > 0)) {
+                //e.g. on win32, agent running w/ dll loaded
+                //PluginDumper cannot overwrite file inuse.
+                return;
+            }
+            else {
+                String msg =
+                    "Error writing " + file +
+                    ": " + e.getMessage();
+                throw new IOException(msg);
+            }
+        }
+
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = is.read(buffer)) > 0) {
+            os.write(buffer, 0, len);
+        }
+        os.close();
+        
+        if (file instanceof PluginFile) {
+            Handler handler = ((PluginFile)file).handler;
+            if (handler.isExecutable()) {
+                chmodx(file);
+            }
+        }
+
+    }
+
+    public boolean chmod(File file, String mode) {
+
+        if (GenericPlugin.isWin32()) {
+            return false;
+        }
+
+        try {
+            //XXX lame.
+            String cmd = "chmod " + mode + " " + file;
+            Process process =
+                Runtime.getRuntime().exec(cmd);
+
+            try {
+                return process.waitFor() == 0;
+            } catch (InterruptedException e) {
+                return false;
+            }
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public boolean chmodx(File file) {
+        return chmod(file, "+x");
+    }
+}
