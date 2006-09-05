8693217bc3638d2bae753ada25bf3cad94ba69ca
dougm <dougm@572de364-d518-0410-a359-afb9c36ce0ec>
2006-09-05 21:30:25 +0000

    [HHQ-188] Add support for ServiceBindingManager configured JNP ports

--- plugins/jboss/src/org/hyperic/hq/plugin/jboss/JBossConfig.java
+++ plugins/jboss/src/org/hyperic/hq/plugin/jboss/JBossConfig.java
@@ -25,148 +25,321 @@
 
 package org.hyperic.hq.plugin.jboss;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.File;
 import java.io.FileInputStream;
-
+import java.io.IOException;
 import java.util.HashMap;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.parsers.SAXParser;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hyperic.util.StringUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.Attributes;
-import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * Config parsing intended only for JBoss server auto discovery.
  */
 public class JBossConfig {
+    private static final Log log =
+        LogFactory.getLog(JBossConfig.class.getName());
+
+    private static final String NAMING_MBEAN =
+        "jboss:service=Naming";
+
+    private static final String BINDING_MBEAN =
+        "jboss.system:service=ServiceBindingManager";
 
     private static HashMap cache = null;
+    private File serviceXML;
+    private File bindingXML;
     private String jnpPort;
-    private long lastModified = 0;
+    private String jnpPortBinding;
+    private String serverBinding;
+    private long lastModified;
+    private long lastModifiedBinding;
 
-    private JBossConfig() {}
+    private JBossConfig(File serviceXML) {
+        this.serviceXML = serviceXML;
+        this.lastModified = this.serviceXML.lastModified();
+    }
 
     public static synchronized JBossConfig getConfig(File configXML) {
         if (cache == null) {
             cache = new HashMap();
         }
 
-        JBossConfig cfg = (JBossConfig)cache.get(configXML);
+        JBossConfig cfg =
+            (JBossConfig)cache.get(configXML);
 
-        long lastModified = configXML.lastModified();
+        if ((cfg == null) || cfg.hasChanged()) {
+            if (cfg == null) {
+                log.debug("Parsing unseen: " + configXML);
+            }
+            else {
+                log.debug("Parsing modified: " + configXML);
+            }
 
-        if ((cfg == null) || (lastModified != cfg.lastModified)) {
-            cfg = new JBossConfig();
-            cfg.lastModified = lastModified;
+            cfg = new JBossConfig(configXML);
             cache.put(configXML, cfg);
 
+            Document doc = null;
             try {
-                cfg.read(configXML);
+                doc = cfg.parse(cfg.serviceXML);
+                cfg.findJnpPort(doc);
             } catch (IOException e) {
+                log.error("Error parsing " + cfg.serviceXML, e);
+            } finally {
+                doc = null;
             }
+        }
+        else {
+            log.debug("Unchanged: " + configXML);
         }
 
         return cfg;
     }
 
-    public String getJnpPort() {
-        return this.jnpPort;
+    private boolean hasChanged() {
+        long lastModified = this.serviceXML.lastModified();
+        if (lastModified != this.lastModified) {
+            this.lastModified = lastModified;
+            return true;
+        }
+
+        if (this.bindingXML != null) {
+            lastModified = this.bindingXML.lastModified();
+            if (lastModified != this.lastModifiedBinding) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    private void read(File file)
+    public String getJnpPort() {
+        return
+            (this.jnpPortBinding == null) ?
+             this.jnpPort : this.jnpPortBinding;
+    }
+
+    private String getAttribute(Node node, String name) {
+        NamedNodeMap attrs = node.getAttributes();
+        if (attrs == null) {
+            return null;
+        }
+        Node item = attrs.getNamedItem(name);
+        if (item == null) {
+            return null;
+        }
+        return item.getNodeValue();
+    }
+
+    private String getText(Node node) {
+        return node.getFirstChild().getNodeValue().trim();
+    }
+
+    private static boolean hasValue(String val) {
+        return (val != null) && (val.length() != 0);
+    }
+
+    private File getJBossHome(File configXML) {
+        File home = configXML.getParentFile();
+        while (home != null) {
+            boolean isServer = home.getName().equals("server");
+            home = home.getParentFile();
+            if (isServer) {
+                break;
+            }
+        }
+        return home;
+    }
+
+    private boolean findJnpPortBinding(Document doc) {
+        NodeList servers =
+            doc.getDocumentElement().getElementsByTagName("server");
+
+        for (int i=0; i<servers.getLength(); i++) {
+            Node server = servers.item(i);
+
+            if (!server.hasAttributes()) {
+                continue;
+            }
+
+            String name = getAttribute(server, "name");
+
+            if (!name.equals(this.serverBinding)) {
+                continue;
+            }
+
+            NodeList configs = server.getChildNodes();
+            for (int j=0; j<configs.getLength(); j++) {
+                Node config = configs.item(j);
+
+                if (!config.hasAttributes()) {
+                    continue;
+                }
+
+                if (!"service-config".equals(config.getNodeName())) {
+                    continue;
+                }
+
+                name = getAttribute(config, "name");
+                if (!name.equals(NAMING_MBEAN)) {
+                    continue;
+                }
+
+                NodeList bindings = config.getChildNodes();
+                for (int k=0; k<bindings.getLength(); k++) {
+                    Node binding = bindings.item(k);
+
+                    if (!"binding".equals(binding.getNodeName())) {
+                        continue;
+                    }
+
+                    if (binding.hasAttributes()) {
+                        String val = getAttribute(binding, "port");
+                        if (hasValue(val)) {
+                            this.jnpPortBinding = val;
+                            return true;
+                        }
+                    }
+                    break;
+                }
+                break;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean findJnpPortBinding(Node mbean)
         throws IOException {
 
-        FileInputStream is = null;
+        NodeList attrs = mbean.getChildNodes();
+
+        for (int i=0; i<attrs.getLength(); i++) {
+            Node attr = attrs.item(i);
+            if (!attr.hasAttributes()) {
+                continue;
+            }
+
+            String attrName = getAttribute(attr, "name");
+
+            if ("ServerName".equals(attrName)) {
+                this.serverBinding = getText(attr);
+                log.debug("ServerName=" + this.serverBinding);
+            }
+            else if ("StoreURL".equals(attrName)) {
+                String url = getText(attr);
+
+                File home = getJBossHome(this.serviceXML);
+                if (home != null) {
+                    log.debug("jboss.home.url=" + home);
+                    url = StringUtil.replace(url,
+                                             "${jboss.home.url}",
+                                             home.toString());
+                }
+
+                File bindings = new File(url);
+                boolean exists = bindings.exists();
+                log.debug("StoreURL exists=" + exists + " (" + url + ")");
+
+                if (exists) {
+                    this.bindingXML = bindings;
+                    this.lastModifiedBinding =
+                        bindings.lastModified();
+
+                    Document bindingDoc;
+                    try {
+                        bindingDoc = parse(bindings);
+                        return findJnpPortBinding(bindingDoc);
+                    } finally {
+                        bindingDoc = null;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean findJnpPort(Node mbean) {
+        NodeList attrs = mbean.getChildNodes();
+
+        for (int i=0; i<attrs.getLength(); i++) {
+            Node attr = attrs.item(i);
+            if (!attr.hasAttributes()) {
+                continue;
+            }
+            if ("Port".equals(getAttribute(attr, "name"))) {
+                String val = getText(attr);
+                if (hasValue(val)) {
+                    this.jnpPort = val;
+                    return true;
+                }
+                break;
+            }
+        }
+
+        return false;
+    }
+
+    private void findJnpPort(Document doc)
+        throws IOException {
+
+        NodeList mbeans =
+            doc.getDocumentElement().getElementsByTagName("mbean");
+
+        for (int i=0; i<mbeans.getLength(); i++) {
+            Node mbean = mbeans.item(i);
+
+            if (!mbean.hasAttributes()) {
+                continue;
+            }
+
+            String name = getAttribute(mbean, "name");
+            if (NAMING_MBEAN.equals(name)) {
+                findJnpPort(mbean);
+                log.debug(NAMING_MBEAN + " port=" + this.jnpPort);
+            }
+            else if (BINDING_MBEAN.equals(name)) {
+                findJnpPortBinding(mbean);
+                log.debug(BINDING_MBEAN + " port=" + this.jnpPortBinding);
+            }
+        }
+
+        log.debug("JNP port=" + getJnpPort());
+    }
+
+    private Document parse(File file)
+        throws IOException {
+
+        FileInputStream fis = null;
 
         try {
-            is = new FileInputStream(file);
-            parse(is);
-        } catch (SAXException e) {
-            throw new IllegalArgumentException(e.getMessage());
+            fis = new FileInputStream(file);
+            DocumentBuilderFactory factory =
+                DocumentBuilderFactory.newInstance();
+            DocumentBuilder parser = factory.newDocumentBuilder();
+            return parser.parse(new InputSource(fis));
         } catch (ParserConfigurationException e) {
             throw new IllegalArgumentException(e.getMessage());
+        } catch (SAXException e) {
+            throw new IllegalArgumentException(e.getMessage());
         } finally {
-            if (is != null) {
-                try { is.close(); } catch (IOException e) { }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {}
             }
-        }
-    }
-
-    private void parse(InputStream is)
-        throws IOException,
-               SAXException,
-               ParserConfigurationException {
-
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        SAXParser parser = factory.newSAXParser();
-        JBossServiceHandler handler = new JBossServiceHandler();
-        parser.parse(is, handler);
-
-        this.jnpPort = handler.getJnpPort();
-    }
-
-    private class JBossServiceHandler extends DefaultHandler {
-
-        private boolean inNaming = false;
-        private boolean inNamingPort = false;
-        private StringBuffer jnpPort = new StringBuffer();
-
-        public void characters(char[] ch,
-                               int start,
-                               int length)
-            throws SAXException {
-
-            if (inNamingPort) {
-                jnpPort.append(ch, start, length);
-                return;
-            }
-        }
-
-        public void endElement(String uri,
-                               String localName,
-                               String qName) {
-
-            if (inNamingPort && qName.equals("attribute")) {
-                inNamingPort = false;
-                return;
-            }
-
-            if (inNaming && qName.equals("mbean")) {
-                inNaming = false;
-                return;
-            }
-        }
-
-        public void startElement(String uri,
-                                 String localName,
-                                 String qName,
-                                 Attributes attributes)
-            throws SAXException {
-
-            if (inNaming && qName.equals("attribute")) {
-                String name = attributes.getValue("name");
-                if (name != null && name.equals("Port")) {
-                    inNamingPort = true;
-                    return;
-                }
-            }
-
-            if (qName.equals("mbean")) {
-                String name = attributes.getValue("name");
-                if (name != null && name.equals("jboss:service=Naming")) {
-                    inNaming = true;
-                    return;
-                }
-            }
-        }
-
-        protected String getJnpPort() {
-            return jnpPort.toString();
         }
     }
 
