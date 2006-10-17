afc8b1dff65bfec298f224c6cd8ed89a4906938e
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2006-10-17 22:07:27 +0000

    Add tirgger DAO

--- src/org/hyperic/dao/DAOFactory.java
+++ src/org/hyperic/dao/DAOFactory.java
@@ -1,17 +1,42 @@
-package org.hyperic.dao;
+/*
+ * NOTE: This copyright does *not* cover user programs that use HQ
+ * program services by normal system calls through the application
+ * program interfaces provided as part of the Hyperic Plug-in Development
+ * Kit or the Hyperic Client Development Kit - this is merely considered
+ * normal use of the program, and does *not* fall under the heading of
+ * "derived work".
+ * 
+ * Copyright (C) [2004, 2005, 2006], Hyperic, Inc.
+ * This file is part of HQ.
+ * 
+ * HQ is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
 
-import org.hyperic.hibernate.dao.CpropDAO;
-import org.hyperic.hibernate.dao.HibernateDAOFactory;
+ package org.hyperic.dao;
+
+import org.hibernate.Session;
 import org.hyperic.hibernate.dao.AgentDAO;
 import org.hyperic.hibernate.dao.ApplicationDAO;
+import org.hyperic.hibernate.dao.ConfigResponseDAO;
+import org.hyperic.hibernate.dao.CpropDAO;
 import org.hyperic.hibernate.dao.CpropKeyDAO;
+import org.hyperic.hibernate.dao.HibernateDAOFactory;
+import org.hyperic.hibernate.dao.HibernateMockDAOFactory;
 import org.hyperic.hibernate.dao.PlatformDAO;
 import org.hyperic.hibernate.dao.ServerDAO;
 import org.hyperic.hibernate.dao.ServiceDAO;
-import org.hyperic.hibernate.dao.HibernateMockDAOFactory;
-import org.hyperic.hibernate.dao.ConfigResponseDAO;
-import org.hyperic.hibernate.dao.ServiceTypeDAO;
-import org.hibernate.Session;
+import org.hyperic.hibernate.dao.TriggerDAO;
 
 /**
  *
@@ -31,6 +56,7 @@ public abstract class DAOFactory
     public abstract PlatformDAO getPlatformDAO();
     public abstract ServerDAO getServerDAO();
     public abstract ServiceDAO getServiceDAO();
+    public abstract TriggerDAO getTriggerDAO();
     public abstract ServiceTypeDAO getServiceTypeDAO();
 
     public static ThreadLocal defaultSession = new ThreadLocal();
