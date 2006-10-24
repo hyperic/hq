70c6f73b5a49855fd3f3f9bf5bc483ad7de59328
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2006-10-24 07:21:17 +0000

    Add ResourceGroup testing.  testSimpleFind() is valid, but is not currently passing.

---
+++ src/org/hyperic/hq/authz/test/ResourceGroupTest.java
@@ -0,0 +1,22 @@
+package org.hyperic.hq.authz.test;
+
+import org.hyperic.hq.authz.shared.AuthzConstants;
+import org.hyperic.hq.authz.shared.ResourceGroupManagerLocal;
+import org.hyperic.hq.authz.shared.ResourceGroupManagerUtil;
+import org.hyperic.hq.authz.shared.ResourceGroupValue;
+import org.hyperic.hq.test.HQEJBTestBase;
+
+public class ResourceGroupTest extends HQEJBTestBase {
+    public ResourceGroupTest(String testName) {
+        super(testName);
+    }
+
+    public void testSimpleFind() throws Exception {
+        ResourceGroupManagerLocal rman =
+            ResourceGroupManagerUtil.getLocalHome().create();
+        ResourceGroupValue resGrp =
+            rman.findResourceGroupByName(getOverlord(),
+                                         AuthzConstants.rootResourceGroupName);
+        assertEquals(AuthzConstants.rootResourceGroupName, resGrp.getName());
+    }
+}
