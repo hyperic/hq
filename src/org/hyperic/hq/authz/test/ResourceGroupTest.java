443315708ba0893fd3c2e26612981fbc18f843b5
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2006-10-25 03:41:24 +0000

    Add test to create and delete resource group

--- src/org/hyperic/hq/authz/test/ResourceGroupTest.java
+++ src/org/hyperic/hq/authz/test/ResourceGroupTest.java
@@ -1,27 +1,64 @@
 package org.hyperic.hq.authz.test;
 
+import org.hyperic.hq.appdef.shared.AppdefEntityConstants;
 import org.hyperic.hq.authz.shared.AuthzConstants;
+import org.hyperic.hq.authz.shared.AuthzSubjectValue;
 import org.hyperic.hq.authz.shared.ResourceGroupManagerLocal;
 import org.hyperic.hq.authz.shared.ResourceGroupManagerUtil;
 import org.hyperic.hq.authz.shared.ResourceGroupValue;
 import org.hyperic.hq.test.HQEJBTestBase;
 
 public class ResourceGroupTest extends HQEJBTestBase {
+    private static String BOGUS_NAME = "foobar";
+    
+    private ResourceGroupManagerLocal rman;
+    
     public ResourceGroupTest(String testName) {
         super(testName);
     }
 
-    public void testSimpleFind() throws Exception {
-        runInTransaction(
-            new TransactionBlock() {
-                public void run() throws Exception {
-                    ResourceGroupManagerLocal rman =
-                        ResourceGroupManagerUtil.getLocalHome().create();
-                    ResourceGroupValue resGrp =
-                        rman.findResourceGroupByName(getOverlord(),
-                                                     AuthzConstants.rootResourceGroupName);
-                    assertEquals(AuthzConstants.rootResourceGroupName, resGrp.getName());
-                }
-            });
+    public void setUp() throws Exception {
+        super.setUp();
+        rman = ResourceGroupManagerUtil.getLocalHome().create();
     }
+
+    public void testSimpleFind() throws Exception {
+        rman = ResourceGroupManagerUtil.getLocalHome().create();
+        ResourceGroupValue resGrp = rman
+                .findResourceGroupByName(
+                                         getOverlord(),
+                                         AuthzConstants.rootResourceGroupName);
+        assertEquals(AuthzConstants.rootResourceGroupName, resGrp.getName());
+    }
+
+    public void testSimpleCreate() throws Exception {
+        final AuthzSubjectValue overlord = getOverlord();
+        
+        // Create the bogus resource group
+        runInTransaction(new TransactionBlock() {
+            public void run() throws Exception {
+                ResourceGroupValue resGrp = new ResourceGroupValue();
+                resGrp.setName(BOGUS_NAME);
+                resGrp.setGroupEntResType(
+                    AppdefEntityConstants.APPDEF_TYPE_PLATFORM);
+                resGrp.setGroupType(
+                    AppdefEntityConstants.APPDEF_TYPE_GROUP_COMPAT_PS);
+                resGrp = rman.createResourceGroup(overlord, resGrp, null, null);
+                assertEquals(BOGUS_NAME, resGrp.getName());
+            }
+        });
+        
+        // Now look up the resource group that we just created
+        final ResourceGroupValue resGrp =
+            rman.findResourceGroupByName(overlord, BOGUS_NAME);
+        assertNotNull(resGrp);
+        
+        // Now delete it
+        runInTransaction(new TransactionBlock() {
+            public void run() throws Exception {
+                rman.removeResourceGroup(overlord, resGrp);
+            }
+        });
+    }
+    
 }
