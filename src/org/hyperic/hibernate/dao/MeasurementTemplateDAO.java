7631eba1e5e6e5a41154f1828cd378a3989eff8c
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2006-10-25 21:27:10 +0000

    Add update() method, fix/add additional finders.

--- src/org/hyperic/hibernate/dao/MeasurementTemplateDAO.java
+++ src/org/hyperic/hibernate/dao/MeasurementTemplateDAO.java
@@ -30,10 +30,13 @@ import java.util.List;
 
 import org.hibernate.Session;
 
+import org.hyperic.dao.DAOFactory;
 import org.hyperic.hq.measurement.Category;
+import org.hyperic.hq.measurement.MeasurementConstants;
 import org.hyperic.hq.measurement.MeasurementTemplate;
 import org.hyperic.hq.measurement.MonitorableType;
 import org.hyperic.hq.measurement.shared.MeasurementTemplateLiteValue;
+import org.hyperic.hq.product.MeasurementInfo;
 
 /**
  * CRUD methods, finders, etc. for MeasurementTemplate
@@ -87,6 +90,37 @@ public class MeasurementTemplateDAO extends HibernateDAO
         return mt;
     }
 
+    public void update(MeasurementTemplate mt, String pluginName,
+                       MeasurementInfo info) {
+        // Load category
+        CategoryDAO catDAO = DAOFactory.getDAOFactory().getCategoryDAO();
+        Category cat = catDAO.findByName(info.getCategory());
+        if (cat == null) {
+            cat = catDAO.create(info.getCategory());
+        }
+
+        // Update raw template
+        mt.setTemplate(info.getTemplate());
+        mt.setCollectionType(info.getCollectionType());
+        mt.setPlugin(pluginName);
+        mt.setCategory(cat);
+        save(mt);
+
+        // Update the derived template
+        MeasurementTemplate dmt = 
+            findByArgAndTemplate(mt.getId(),
+                                 MeasurementConstants.TEMPL_IDENTITY);
+        
+        dmt.setAlias(info.getAlias());
+        dmt.setDesignate(info.isIndicator());
+        dmt.setUnits(info.getUnits());
+        dmt.setCollectionType(info.getCollectionType());
+        dmt.setDefaultOn(info.isDefaultOn());
+        dmt.setDefaultInterval(info.getInterval());
+        dmt.setCategory(cat);
+        save(dmt);
+    }
+
     public List findTemplates(Integer ids[]) {
         StringBuffer buf = 
             new StringBuffer("from MeasurementTemplate where id IN (");
@@ -103,7 +137,7 @@ public class MeasurementTemplateDAO extends HibernateDAO
         String sql = 
             "from MeasurementTemplate t " +
             "join fetch t.monitorableType mt " +
-            "where mt.name=?";
+            "where mt.name=? and t.defaultInterval > 0";
         return getSession().createQuery(sql).
             setString(0, type).list();
     }
@@ -114,9 +148,67 @@ public class MeasurementTemplateDAO extends HibernateDAO
             "from MeasurementTemplate t " +
             "join fetch t.monitorableType mt " +
             "join fetch t.category cat " +
-            "where mt.name=? and cat.name=?";
+            "where mt.name=? and t.defaultInterval > 0 and cat.name=?";
         return getSession().createQuery(sql)
             .setString(0, type)
             .setString(1, cat).list();
-    }        
+    }
+
+    public List findDefaultsByMonitorableType(String mt, int appdefType) {
+        String sql =
+            "from MeasurementTemplate t " +
+            "join fetch t.monitorableType mt " +
+            "where mt.name=? and mt.appdefType=? " +
+            "and t.defaultInterval > 0 and t.defaultOn = true " +
+            "order by mt.name";
+ 
+        return getSession().createQuery(sql)
+            .setString(0, mt)
+            .setInteger(1, appdefType).list();
+    }
+
+    public List findDesignatedByMonitorableType(String mt, int appdefType) {
+        String sql =
+            "from MeasurementTemplate t " +
+            "join fetch t.monitorableType mt " +
+            "where mt.name=? and mt.appdefType=? " +
+            "and t.defaultInterval > 0 and t.designate = true " +
+            "order by mt.name";
+ 
+        return getSession().createQuery(sql)
+            .setString(0, mt)
+            .setInteger(1, appdefType).list();
+    }
+
+    public List findRawByMonitorableType(Integer mtId) {
+        String sql =
+            "from MeasurementTemplate t " +
+            "join fetch t.monitorableType mt " +
+            "where mt.id=? and t.defaultInterval=0";
+
+        return getSession().createQuery(sql)
+            .setInteger(0, mtId.intValue()).list();
+    }
+
+    public List findByMeasurementArg(Integer tId) {
+        String sql =
+            "from MeasurementTemplate t " +
+            "join fetch t.measurementArgs args " +
+            "where args.templateArg.id=?";
+
+        return getSession().createQuery(sql)
+            .setInteger(0, tId.intValue()).list();
+    }
+
+    public MeasurementTemplate findByArgAndTemplate(Integer tId,
+                                                    String template) {
+        String sql =
+            "from MeasurementTemplate t " +
+            "join fetch t.measurementArgs args " +
+            "where args.templateArg.id=? and t.template=?";
+        
+        return (MeasurementTemplate)getSession().createQuery(sql)
+            .setInteger(0, tId.intValue())
+            .setString(1, template).uniqueResult();
+    }
 }
