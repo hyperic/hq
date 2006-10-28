8ae7c1dd7e5e5790fa816d3aee324325c183b6c0
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2006-10-28 00:20:32 +0000

    Test adding a resource to a group

--- src/org/hyperic/hq/authz/test/ResourceGroupTest.java
+++ src/org/hyperic/hq/authz/test/ResourceGroupTest.java
@@ -6,7 +6,13 @@ import org.hyperic.hq.authz.shared.AuthzSubjectValue;
 import org.hyperic.hq.authz.shared.ResourceGroupManagerLocal;
 import org.hyperic.hq.authz.shared.ResourceGroupManagerUtil;
 import org.hyperic.hq.authz.shared.ResourceGroupValue;
+import org.hyperic.hq.authz.shared.ResourceManagerLocal;
+import org.hyperic.hq.authz.shared.ResourceManagerUtil;
+import org.hyperic.hq.authz.shared.ResourcePK;
+import org.hyperic.hq.authz.shared.ResourceTypeValue;
 import org.hyperic.hq.test.HQEJBTestBase;
+import org.hyperic.util.pager.PageControl;
+import org.hyperic.util.pager.PageList;
 
 public class ResourceGroupTest extends HQEJBTestBase {
     private static String BOGUS_NAME = "foobar";
@@ -53,6 +59,56 @@ public class ResourceGroupTest extends HQEJBTestBase {
             rman.findResourceGroupByName(overlord, BOGUS_NAME);
         assertNotNull(resGrp);
         
+        // Now delete it
+        runInTransaction(new TransactionBlock() {
+            public void run() throws Exception {
+                rman.removeResourceGroup(overlord, resGrp);
+            }
+        });
+    }
+    
+    public void testSimpleAdd() throws Exception {
+        final AuthzSubjectValue overlord = getOverlord();
+        
+        // Create the bogus resource group
+        runInTransaction(new TransactionBlock() {
+            public void run() throws Exception {
+                ResourceGroupValue resGrp = new ResourceGroupValue();
+                resGrp.setName(BOGUS_NAME);
+                resGrp.setGroupEntResType(
+                    AppdefEntityConstants.APPDEF_TYPE_PLATFORM);
+                resGrp.setGroupType(
+                    AppdefEntityConstants.APPDEF_TYPE_GROUP_COMPAT_PS);
+                resGrp = rman.createResourceGroup(overlord, resGrp, null, null);
+                assertEquals(BOGUS_NAME, resGrp.getName());
+            }
+        });
+        
+        // Now look up the resource group that we just created
+        final ResourceGroupValue resGrp =
+            rman.findResourceGroupByName(overlord, BOGUS_NAME);
+        assertNotNull(resGrp);
+        
+        // Create a platform resource
+        runInTransaction(new TransactionBlock() {
+            public void run() throws Exception {
+                ResourceManagerLocal remg =
+                    ResourceManagerUtil.getLocalHome().create();
+                ResourceTypeValue rtv =
+                    remg.findResourceTypeByName(AuthzConstants.platformResType);
+                assertEquals(AuthzConstants.platformResType, rtv.getName());
+                ResourcePK pk = remg.createResource(overlord, rtv,
+                                                    new Integer(9999),
+                                                    "Platform " + BOGUS_NAME);
+                rman.addResource(overlord, resGrp, pk.getId(), rtv);
+            }
+        });
+        
+        // Look up the group again
+        PageList resources = rman.getResources(overlord, resGrp,
+                                               PageControl.PAGE_ALL);
+        assertEquals(1, resources.size());
+
         // Now delete it
         runInTransaction(new TransactionBlock() {
             public void run() throws Exception {
