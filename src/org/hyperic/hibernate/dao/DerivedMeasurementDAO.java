4b5ee783fd0564aa2e368b89920b7578bf0bcfc0
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2006-10-29 04:25:22 +0000

    Add update methods and finders

--- src/org/hyperic/hibernate/dao/DerivedMeasurementDAO.java
+++ src/org/hyperic/hibernate/dao/DerivedMeasurementDAO.java
@@ -25,10 +25,13 @@
 
 package org.hyperic.hibernate.dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
 
 import org.hyperic.hq.measurement.DerivedMeasurement;
 import org.hyperic.hq.measurement.MeasurementTemplate;
+import org.hyperic.hq.measurement.shared.DerivedMeasurementValue;
 
 /**
  * CRUD methods, finders, etc. for DerivedMeasurement
@@ -59,14 +62,118 @@ public class DerivedMeasurementDAO extends HibernateDAO
         super.remove(entity);
     }
 
+    /**
+     * Update the interval for a derived measurement
+     */
+    public DerivedMeasurement update(DerivedMeasurement dm, long interval) {
+        dm.setEnabled(interval != 0);
+        dm.setInterval(interval);
+        save(dm);
+        return dm;
+    }
+
+    /**
+     * Enable a derived measurement
+     */
+    public DerivedMeasurement update(DerivedMeasurement dm, boolean enabled) {
+        dm.setEnabled(enabled);
+        save(dm);
+        return dm;
+    }
+
     public DerivedMeasurement create(Integer instanceId,
                                      MeasurementTemplate mt,
                                      long interval) {
         DerivedMeasurement dm = new DerivedMeasurement(instanceId, mt,
                                                        interval);
-        dm.setEnabled(true);
+
+        dm.setEnabled(interval != 0);
         dm.setFormula(mt.getTemplate());
         save(dm);
         return dm;
+    }
+
+    public List findByIds(Integer ids[]) {
+        StringBuffer buf = 
+            new StringBuffer("from DerivedMeasurement  where id IN (");
+        int len = ids.length;
+        for (int i = 0; i < len - 1; i++) {
+            buf.append(ids[i] + ", ");
+        }
+        buf.append(ids[len - 1] + ")");
+
+        return getSession().createQuery(buf.toString()).list();
+    }
+
+    public DerivedMeasurement findByTemplateForInstance(Integer tid, 
+                                                        Integer iid) {
+        String sql =
+            "from DerivedMeasurement m " +
+            "join fetch m.template as t " +
+            "where t.id=? and m.instanceId=?";
+
+        return (DerivedMeasurement)getSession().createQuery(sql)
+            .setInteger(0, tid.intValue())
+            .setInteger(1, iid.intValue()).uniqueResult();
+    }
+
+    public List findByInstance(int type, int id) {
+        String sql =
+            "from DerivedMeasurement m " +
+            "join fetch m.template at t " +
+            "join fetch m.template.monitorableType mt " +
+            "where mt.appdefType=? and m.instanceId=? and " +
+            "m.interval is not null";
+
+        return getSession().createQuery(sql)
+            .setInteger(0, type)
+            .setInteger(1, id).list();
+    }
+
+    public List findByInstance(int type, int id, boolean enabled) {
+        String sql =
+            "from DerivedMeasurement m " +
+            "join fetch m.template at t " +
+            "join fetch m.template.monitorableType mt " +
+            "where mt.appdefType=? and m.instanceId=? and " +
+            "m.enabled = ? and m.interval is not null";
+
+        return getSession().createQuery(sql)
+            .setInteger(0, type)
+            .setInteger(1, id)
+            .setBoolean(2, enabled).list();
+    }
+
+    public DerivedMeasurement findByAliasAndID(String alias,
+                                               int appdefType, int appdefId) {
+
+        String sql =
+            "from DerivedMeasurement m " +
+            "join fetch m.tempalte as t " +
+            "join fetch m.monitorableType as mt " +
+            "where t.alias = ? and mt.appdefType = ? " +
+            "and m.instanceId = ? and m.interval is not null";
+
+        return (DerivedMeasurement)getSession().createQuery(sql)
+            .setString(0, alias)
+            .setInteger(1, appdefType)
+            .setInteger(2, appdefId).uniqueResult();
+    }
+
+    public List findDesignatedByInstanceForCategory(int appdefType, int iid,
+                                                    String cat) {
+        String sql =
+            "from DerivedMeasurement m " +
+            "join fetch m.template as t " +
+            "join fetch m.monitorableType as mt " +
+            "where m.instanceId = ? " +
+            "and t.designate = true " +
+            "and mt.appdefType = ? " +
+            "and t.category.name = ?";
+
+        return getSession().createQuery(sql)
+            .setInteger(0, iid)
+            .setInteger(1, appdefType)
+            .setString(2, cat).list();
     }
 }
