2f2817035984cc0625598c1aa9013bf3e5d71c53
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2006-10-30 17:12:32 +0000

    Add getScheduleRevNumValue()

--- src/org/hyperic/hq/measurement/ScheduleRevNum.java
+++ src/org/hyperic/hq/measurement/ScheduleRevNum.java
@@ -25,6 +25,8 @@
 
 package org.hyperic.hq.measurement;
 
+import org.hyperic.hq.measurement.shared.ScheduleRevNumValue;
+
 public class ScheduleRevNum implements java.io.Serializable {
 
     // Fields    
@@ -120,6 +122,23 @@ public class ScheduleRevNum implements java.io.Serializable {
         result = 37*result + (_id != null ? _id.hashCode() : 0);
 
         return result;
+    }
+
+    /**
+     * Legacy EJB DTO pattern
+     * @deprecated Use (this) ScheduleRevNum object instead
+     */
+    public ScheduleRevNumValue getScheduleRevNumValue() {
+        ScheduleRevNumValue val = new ScheduleRevNumValue();
+
+        val.setAppdefType(getId().getAppdefType());
+        val.setInstanceId(getId().getInstanceId());
+        val.setSRN(getSrn().intValue());
+        val.setMinInterval(getMinInterval());
+        val.setLastReported(getLastReported());
+        val.setPending(isPending());
+
+        return val;
     }
 }
 
