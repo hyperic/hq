e38dee8ac5341b0eeb6bf439be9716f6bddc461f
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2006-11-24 18:24:43 +0000

    Add copyright notice

--- src/org/hyperic/hq/ui/action/resource/common/control/QuickControlForm.java
+++ src/org/hyperic/hq/ui/action/resource/common/control/QuickControlForm.java
@@ -1,3 +1,28 @@
+/*
+ * NOTE: This copyright does *not* cover user programs that use HQ
+ * program services by normal system calls through the application
+ * program interfaces provided as part of the Hyperic Plug-in Development
+ * Kit or the Hyperic Client Development Kit - this is merely considered
+ * normal use of the program, and does *not* fall under the heading of
+ * "derived work".
+ * 
+ * Copyright (C) [2004, 2005, 2006], Hyperic, Inc.
+ * This file is part of HQ.
+ * 
+ * HQ is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
+
 package org.hyperic.hq.ui.action.resource.common.control;
 
 import java.util.List;
@@ -55,8 +80,8 @@ public class QuickControlForm extends BaseValidatorForm  {
         super.reset(mapping, request);
     }
 
-    public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
-        ActionErrors ae = new ActionErrors();
+    public ActionErrors validate(ActionMapping mapping,
+                                 HttpServletRequest request) {
         // XXX make sure that is a valid resourceType
         return super.validate(mapping, request);
     }
