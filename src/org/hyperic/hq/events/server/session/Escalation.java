1c7bcf0adb47ee01214e41092f5b3de1b93ae9a4
youngl <youngl@572de364-d518-0410-a359-afb9c36ce0ec>
2006-12-01 08:31:26 +0000

    tag outer most json element with "escalation" attribute

--- src/org/hyperic/hq/events/server/session/Escalation.java
+++ src/org/hyperic/hq/events/server/session/Escalation.java
@@ -187,7 +187,7 @@ public class Escalation extends PersistedObject
         if (getId() != null) {
             json.put("id", getId());
         }
-        return json;
+        return new JSONObject().put("escalation", json);
     }
 
     public boolean equals(Object obj) {
