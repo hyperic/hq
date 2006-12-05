6c6bd59c30649487bc4fecfe672e6cb7e99e38b5
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2006-12-05 18:52:03 +0000

    Add EscalationActionBean to capture the action configuration values

--- src/org/hyperic/hq/ui/action/resource/common/monitor/alerts/config/EscalationSchemeForm.java
+++ src/org/hyperic/hq/ui/action/resource/common/monitor/alerts/config/EscalationSchemeForm.java
@@ -25,14 +25,22 @@
 
 package org.hyperic.hq.ui.action.resource.common.monitor.alerts.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts.action.ActionMapping;
 import org.hyperic.hq.ui.action.resource.ResourceForm;
+import org.hyperic.hq.ui.beans.EscalationActionBean;
 
 public class EscalationSchemeForm extends ResourceForm {
     private String _escId;
     private String _escName;
+    private List   _actions;
+    private boolean _allowPause;
+    private long    _pauseRange;
+    private int     _notification;
 
     public String getEscId() {
         return _escId;
@@ -57,9 +65,64 @@ public class EscalationSchemeForm extends ResourceForm {
         _escName = escName;
     }
 
+    public boolean isAllowPause() {
+        return _allowPause;
+    }
+
+    public void setAllowPause(boolean allowPause) {
+        this._allowPause = allowPause;
+    }
+
+    public int getNotification() {
+        return _notification;
+    }
+
+    public void setNotification(int notification) {
+        this._notification = notification;
+    }
+
+    public long getPauseRange() {
+        return _pauseRange;
+    }
+
+    public void setPauseRange(long pauseRange) {
+        this._pauseRange = pauseRange;
+    }
+
+    public EscalationActionBean[] getActions() {
+        EscalationActionBean[] actions =
+            new EscalationActionBean[_actions.size()];
+        return (EscalationActionBean[]) _actions.toArray(actions);
+    }
+
+    public EscalationActionBean getAction(int index) {
+        if (index >= _actions.size()) {
+            setNumActions(index + 1);
+        }
+        return (EscalationActionBean) _actions.get(index);
+    }
+
+    public void setNumActions(int numActions) {
+        while (_actions.size() < numActions) {
+            _actions.add(new EscalationActionBean());
+        }
+
+        // Remove extra conditions if necessary
+        if (_actions.size() > numActions) {
+            for (int i = _actions.size(); i > numActions; i--)
+                _actions.remove(i - 1);
+        }
+    }
+
+    public void resetActions() {
+        _actions = new ArrayList();
+        setNumActions(1);
+    }
+
     public void reset(ActionMapping mapping, HttpServletRequest request) {
         super.reset(mapping, request);
         _escId = "";
         _escName = "";
+        resetActions();
     }
 }
