5f412e391d4fe97160ffb8e4d555287ee31c0e1b
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2006-12-06 05:38:09 +0000

    Remove alert creation out of abstract trigger

--- src/org/hyperic/hq/events/ext/AbstractTrigger.java
+++ src/org/hyperic/hq/events/ext/AbstractTrigger.java
@@ -25,8 +25,6 @@
 
 package org.hyperic.hq.events.ext;
 
-import java.util.HashMap;
-
 import javax.ejb.CreateException;
 import javax.ejb.FinderException;
 import javax.management.AttributeNotFoundException;
@@ -47,35 +45,19 @@ import org.hyperic.hq.common.SystemException;
 import org.hyperic.hq.common.util.Messenger;
 import org.hyperic.hq.events.AbstractEvent;
 import org.hyperic.hq.events.ActionExecuteException;
-import org.hyperic.hq.events.ActionInterface;
 import org.hyperic.hq.events.AlertCreateException;
-import org.hyperic.hq.events.AlertFiredEvent;
 import org.hyperic.hq.events.EventConstants;
-import org.hyperic.hq.events.InvalidActionDataException;
 import org.hyperic.hq.events.TriggerFiredEvent;
 import org.hyperic.hq.events.TriggerInterface;
 import org.hyperic.hq.events.TriggerNotFiredEvent;
-import org.hyperic.hq.events.server.session.EscalationMediator;
-import org.hyperic.hq.events.server.session.Alert;
-import org.hyperic.hq.events.server.session.AlertConditionLog;
 import org.hyperic.hq.events.server.session.AlertDefinition;
 import org.hyperic.hq.events.server.session.Escalation;
-import org.hyperic.hq.events.shared.ActionValue;
-import org.hyperic.hq.events.shared.AlertActionLogValue;
-import org.hyperic.hq.events.shared.AlertConditionLogValue;
-import org.hyperic.hq.events.shared.AlertConditionValue;
+import org.hyperic.hq.events.server.session.EscalationMediator;
 import org.hyperic.hq.events.shared.AlertDefinitionManagerLocal;
 import org.hyperic.hq.events.shared.AlertDefinitionManagerUtil;
-import org.hyperic.hq.events.shared.AlertManagerLocal;
-import org.hyperic.hq.events.shared.AlertManagerUtil;
-import org.hyperic.hq.events.shared.AlertValue;
 import org.hyperic.hq.events.shared.RegisteredTriggerValue;
 import org.hyperic.hq.events.shared.TriggerTrackerLocal;
 import org.hyperic.hq.events.shared.TriggerTrackerUtil;
-import org.hyperic.util.config.ConfigResponse;
-import org.hyperic.util.config.EncodingException;
-import org.hyperic.util.config.InvalidOptionException;
-import org.hyperic.util.config.InvalidOptionValueException;
 
 /** Abstract class that defines a trigger, which can fire actions
  */
@@ -148,19 +130,14 @@ public abstract class AbstractTrigger implements TriggerInterface {
         if (!isSystemReady())
             return;
             
-        ActionValue[] actions;
-        AlertValue alert = new AlertValue();
-        
         // No matter what, send a message to let people know that this trigger
         // has fired
         publishEvent(event);
 
         AlertDefinitionManagerLocal aman;
-        AlertManagerLocal alman;
         AlertDefinition adBasic;
         try {
             aman = AlertDefinitionManagerUtil.getLocalHome().create();
-            alman = AlertManagerUtil.getLocalHome().create();
         } catch (NamingException e) {
             throw new SystemException(e);
         } catch (CreateException e) {
@@ -212,37 +189,9 @@ public abstract class AbstractTrigger implements TriggerInterface {
             if (log.isDebugEnabled())
                 log.debug("Firing trigger " + getId() + " actions");
 
-            // Start the Alert object
-            alert.setAlertDefId(adBasic.getId());
-
-            // Create time is the same as the fired event
-            alert.setCtime(event.getTimestamp());
-
-            // Now create the alert
-            alert = alman.createAlert(alert);
-
-            // Create the trigger event map
-            HashMap trigMap = new HashMap();
-            TriggerFiredEvent[] tfes = event.getRootEvents();
-            for (int i = 0; i < tfes.length; i++) {
-                trigMap.put(tfes[i].getInstanceId(), tfes[i]);
-            }
-        
-            // Create a alert condition logs for every condition
-            AlertConditionValue[] conds = aman.getConditionsById(adId);
-            for (int i = 0; i < conds.length; i++) {
-                AlertConditionLogValue clog = new AlertConditionLogValue();
-                clog.setCondition(conds[i]);
-                if (trigMap.containsKey(conds[i].getTriggerId())) {
-                    clog.setValue(trigMap.get(
-                        conds[i].getTriggerId()).toString());
-                } 
-                alert.addConditionLog(clog);
-            }
-        
-            // Store the alert
-            alman.updateAlert(alert);
-            actions = aman.getActionsById(adId);
+            // Now start escalation
+            EscalationMediator emed = EscalationMediator.getInstance();
+            emed.startEscalation(adBasic.getId(), event);
         } catch (FinderException e) {
             throw new ActionExecuteException(
                 "Alert Definition not found for trigger: " + getId());
@@ -256,74 +205,6 @@ public abstract class AbstractTrigger implements TriggerInterface {
             throw new ActionExecuteException(
                 "Cannot look up AuthzSubjectManagerLocal interface");
         }
-
-        // Regardless of whether or not the actions succeed, we will send an
-        // AlertFiredEvent
-        publishEvent(new AlertFiredEvent(event, alert.getId(), adBasic));
-
-        // get alert pojo so retrieve array of AlertCondtionLogs
-        Alert alertpojo = alman.findAlertById(alert.getId());
-        Escalation esc = alertpojo.getAlertDefinition().getEscalation();
-        if (esc != null) {
-            // invoke escalation chain
-            EscalationMediator emed = EscalationMediator.getInstance();
-            emed.startEscalation(esc.getId(), alert.getId());
-        } else {
-            AlertConditionLog[] logs =
-                (AlertConditionLog[])alertpojo
-                    .getConditionLog().toArray(new AlertConditionLog[0]);
-            // Iterate through the actions
-            for (int i = 0; i < actions.length; i++) {
-                ActionValue aval = actions[i];
-
-                try {
-                    Class ac = Class.forName(aval.getClassname());
-                    ActionInterface action = (ActionInterface) ac.newInstance();
-
-                    // Initialize action
-                    action.init(ConfigResponse.decode(action.getConfigSchema(),
-                        aval.getConfig()));
-
-                    String detail = action.execute(adBasic, logs, alert.getId());
-                                   
-                    AlertActionLogValue alog = new AlertActionLogValue();
-                    alog.setActionId(aval.getId());
-                    alog.setDetail(detail);
-                
-                    alert.addActionLog(alog);
-                } catch (ClassNotFoundException e) {
-                    // Can't execute if we can't lookup up the class
-                    throw new ActionExecuteException(
-                        "Action class not found for ID " + aval.getId(), e);
-                } catch (InstantiationException e) {
-                    // Can't execute if we can't instantiate the object
-                    throw new ActionExecuteException(
-                        "Cannot instantiate action for ID " + aval.getId(), e);
-                } catch (InvalidActionDataException e) {
-                    // Can't execute if we can't instantiate the object
-                    throw new ActionExecuteException(
-                        "Cannot initialize action for ID " + aval.getId(), e);
-                } catch (IllegalAccessException e) {
-                    // Can't execute if we can't access the class
-                    throw new ActionExecuteException(
-                        "Cannot access action for ID " + aval.getId(), e);
-                } catch (EncodingException e) {
-                    // Can't execute if we can't decode the config
-                    throw new ActionExecuteException(
-                        "Cannot decode action config for ID " + aval.getId(), e);
-                } catch (InvalidOptionException e) {
-                    // Can't execute if we can't decode the config
-                    throw new ActionExecuteException(
-                        "Action config contains invalid option for ID " +
-                            aval.getId(), e);
-                } catch (InvalidOptionValueException e) {
-                    // Can't execute if we don't have good config, just log it
-                    log.debug("Bad action config value for ID " + aval.getId(), e);
-                }
-            } // for
-        } // else
-            // Store the alert
-        alman.updateAlert(alert);
     }
     
     public Integer getId() {
