c527868b79a39c41ec63d7f12371ed0d60df55ce
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2007-01-03 01:23:49 +0000

    Add debugging statement

--- src/org/hyperic/hq/ui/json/action/escalation/finder/JsonEscalationByAlertDefId.java
+++ src/org/hyperic/hq/ui/json/action/escalation/finder/JsonEscalationByAlertDefId.java
@@ -1,21 +1,25 @@
 package org.hyperic.hq.ui.json.action.escalation.finder;
 
+import java.rmi.RemoteException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hyperic.hq.auth.shared.SessionNotFoundException;
+import org.hyperic.hq.auth.shared.SessionTimeoutException;
+import org.hyperic.hq.authz.shared.PermissionException;
+import org.hyperic.hq.ui.json.JSONResult;
+import org.hyperic.hq.ui.json.action.JsonActionContext;
 import org.hyperic.hq.ui.json.action.escalation.BaseAction;
 import org.hyperic.hq.ui.json.action.escalation.EscalationWebMediator;
-import org.hyperic.hq.ui.json.action.JsonActionContext;
-import org.hyperic.hq.ui.json.JSONResult;
-import org.hyperic.hq.authz.shared.PermissionException;
-import org.hyperic.hq.auth.shared.SessionTimeoutException;
-import org.hyperic.hq.auth.shared.SessionNotFoundException;
-import org.json.JSONObject;
 import org.json.JSONException;
-
-import java.rmi.RemoteException;
+import org.json.JSONObject;
 
 /**
  */
 public class JsonEscalationByAlertDefId extends BaseAction
 {
+    private Log log = LogFactory.getLog(JsonEscalationByAlertDefId.class);
+    
     public void execute(JsonActionContext context)
         throws JSONException,
                PermissionException, SessionTimeoutException,
@@ -25,6 +29,10 @@ public class JsonEscalationByAlertDefId extends BaseAction
                 .jsonEscalationByAlertDefId(
                         context, context.getSessionId(), context.getId(),
                         context.getAlertDefType());
+        if (log.isDebugEnabled()) {
+            log.debug("JsonEscalationByAlertDefId: " + context.getId() +
+                      ": " + escalation);
+        }
         context.setJSONResult(new JSONResult(escalation));
     }
 }
