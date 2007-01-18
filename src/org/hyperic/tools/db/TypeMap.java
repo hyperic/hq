35ccc6908d7c49b0e71289f3be177fa1ecb761c9
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-01-18 23:46:53 +0000

    Stylize

--- src/org/hyperic/tools/db/TypeMap.java
+++ src/org/hyperic/tools/db/TypeMap.java
@@ -39,109 +39,94 @@ import org.w3c.dom.NodeList;
 
 public class TypeMap
 {
-    private String    m_strGenericType;
-    private Hashtable m_mapDBType = new Hashtable();
+    private String    _strGenericType;
+    private Hashtable _mapDBType = new Hashtable();
     
-    protected TypeMap(Node node) throws SAXException
-    {
-        if(TypeMap.isTypeMap(node) == true)
-        {
-            NamedNodeMap type = node.getAttributes();
+    protected TypeMap(Node node) throws SAXException {
+        if (TypeMap.isTypeMap(node) == false)
+            throw new SAXException("node is not a TypeMap.");
             
-            for(int iType = 0;iType < type.getLength();iType ++)
-            {
-                Node   nodeType = type.item(iType);
-                String strName  = nodeType.getNodeName();
-                String strValue = nodeType.getNodeValue();
+        NamedNodeMap type = node.getAttributes();
             
-                if(strName.equalsIgnoreCase("type") == true)
-                {
-                    this.m_strGenericType = strValue;
+        for(int iType = 0;iType < type.getLength();iType ++) {
+            Node   nodeType = type.item(iType);
+            String strName  = nodeType.getNodeName();
+            String strValue = nodeType.getNodeValue();
+            
+            if(strName.equalsIgnoreCase("type") == true) {
+                _strGenericType = strValue;
                 
-                    NodeList listMaps = node.getChildNodes();
+                NodeList listMaps = node.getChildNodes();
                     
-                    for(int iMap = 0;iMap < listMaps.getLength();iMap ++)
-                    {
-                        node = listMaps.item(iMap);
+                for(int iMap = 0;iMap < listMaps.getLength();iMap ++) {
+                    node = listMaps.item(iMap);
                         
-                        if(node.getNodeName().equalsIgnoreCase("map") == true)
-                        {
-                            String  strDB         = null;
-                            String  strMappedType = null;
+                    if(node.getNodeName().equalsIgnoreCase("map") == true) {
+                        String  strDB         = null;
+                        String  strMappedType = null;
 
-                            NamedNodeMap map = node.getAttributes();
+                        NamedNodeMap map = node.getAttributes();
                             
-                            for(int iAttr = 0;iAttr < map.getLength();iAttr ++)
-                            {
-                                node     = map.item(iAttr);
-                                strName  = node.getNodeName();
-                                strValue = node.getNodeValue();
+                        for(int iAttr = 0;iAttr < map.getLength();iAttr ++) {
+                            node     = map.item(iAttr);
+                            strName  = node.getNodeName();
+                            strValue = node.getNodeValue();
 
-                                if(strName.equalsIgnoreCase("db") == true)
-                                {
-                                    // Get the Database
-                                    strDB = strValue;
-                                }
-                                else if(strName.equalsIgnoreCase("type") == true)
-                                {
-                                    // Get the Column Type
-                                    strMappedType = strValue;
-                                }
-                                else
-                                {
-                                    System.out.println("Unknown attribute \'" + node.getNodeName() + "\' in tag \'table\'");
-                                }
+                            if(strName.equalsIgnoreCase("db") == true) {
+                                // Get the Database
+                                strDB = strValue;
+                            } else if(strName.equalsIgnoreCase("type") == true){
+                                // Get the Column Type
+                                strMappedType = strValue;
+                            } else {
+                                System.out.println("Unknown attribute \'" + 
+                                                   node.getNodeName() + 
+                                                   "\' in tag \'table\'");
                             }
-            
-                            this.m_mapDBType.put(strDB, strMappedType);
                         }
+            
+                        _mapDBType.put(strDB, strMappedType);
                     }
                 }
             }
         }
-        else throw new SAXException("node is not a TypeMap.");
     }
     
-    protected boolean contains(String database)
-    {
-        return this.m_mapDBType.contains(database);
+    protected boolean contains(String database) {
+        return _mapDBType.contains(database);
     }
     
-    protected String getGenericType()
-    {
-        return this.m_strGenericType;
+    protected String getGenericType() {
+        return _strGenericType;
     }
     
-    public String getMappedType(String type, String database)
-    {
+    public String getMappedType(String type, String database) {
         String  strResult;
         
-        if(this.m_strGenericType.equalsIgnoreCase(type) == true)
-            strResult = (String)this.m_mapDBType.get(database);
+        if(_strGenericType.equalsIgnoreCase(type) == true)
+            strResult = (String)_mapDBType.get(database);
         else
             strResult = null;
             
         return strResult;
     }
     
-    protected String getMappedType(String type, int database)
-    {
-        return this.getMappedType(type, JDBC.toName(database));
+    protected String getMappedType(String type, int database) {
+        return getMappedType(type, JDBC.toName(database));
     }
 
-    public static String getMappedType(Collection typemaps, 
-                                       String type,
-                                       int database) {
+    public static String getMappedType(Collection typemaps, String type, 
+                                       int database) 
+    {
         return getMappedType(typemaps, type, JDBC.toName(database));
     }
 
-    public static String getMappedType(Collection typemaps, 
-                                       String type,
-                                       String database) {
+    public static String getMappedType(Collection typemaps, String type, 
+                                       String database) 
+    {
         Iterator iter = typemaps.iterator();
         String typeName = null;
-        while(iter.hasNext())
-        {
+        while(iter.hasNext()) {
             TypeMap map = (TypeMap)iter.next();
             typeName = map.getMappedType(type, 
                                          database);
@@ -152,34 +137,26 @@ public class TypeMap
         return typeName;
     }
 
-    protected static boolean isTypeMap(Node node)
-    {
+    protected static boolean isTypeMap(Node node) {
         String strTmp = node.getNodeName();
         return strTmp.equalsIgnoreCase("typemap");
     }
 
-    protected static Collection readTypeMaps(Node node)
-    {
+    protected static Collection readTypeMaps(Node node) {
         Collection collResult = new StrongCollection("org.hyperic.tools.db.TypeMap");
         NodeList   listMaps   = node.getChildNodes();
 
-        for(int iMap = 0;iMap < listMaps.getLength();iMap ++)
-        {
+        for(int iMap = 0;iMap < listMaps.getLength();iMap ++) {
             Node   nodeMap = listMaps.item(iMap);
             String strTmp  = nodeMap.getNodeName();
             
-            if(strTmp.equalsIgnoreCase("mapping") == true)
-            {
+            if(strTmp.equalsIgnoreCase("mapping") == true) {
                 NodeList listTypes = nodeMap.getChildNodes();
 
-                for(int iType = 0;iType < listTypes.getLength(); iType ++)
-                {
-                    try
-                    {
+                for(int iType = 0;iType < listTypes.getLength(); iType ++) {
+                    try {
                         collResult.add(new TypeMap(listTypes.item(iType)));
-                    }
-                    catch(SAXException e)
-                    {
+                    } catch(SAXException e) {
                     }
                 }
             }
@@ -189,8 +166,8 @@ public class TypeMap
     }
 
     public static Collection loadTypeMapFromFile ( File f ) 
-        throws IOException, SAXException {
-
+        throws IOException, SAXException 
+    {
         Node typeMapNode;
         DBSetup dbsetup = new DBSetup();
         typeMapNode = dbsetup.readDocument(f.getAbsolutePath()).getFirstChild();
