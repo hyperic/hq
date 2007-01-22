4d75ab2b0f97cf1f6d1590f9ff4b5d98a1996392
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2007-01-22 21:50:36 +0000

    Too much s/public/protected/g

--- src/org/hyperic/hq/ui/action/BaseAction.java
+++ src/org/hyperic/hq/ui/action/BaseAction.java
@@ -10,14 +10,14 @@
  * This file is part of HQ.
  * 
  * HQ is free software; you can redistribute it and/or modify
- * it under the terms version 2 of the GNU General protected License as
+ * it under the terms version 2 of the GNU General Public License as
  * published by the Free Software Foundation. This program is distributed
  * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General protected License for more
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  * 
- * You should have received a copy of the GNU General protected License
+ * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA.
