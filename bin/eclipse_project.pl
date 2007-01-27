3cca903e404886ee9ff363e166539f76477abc1a
dougm <dougm@572de364-d518-0410-a359-afb9c36ce0ec>
2007-01-27 20:46:03 +0000

    add tools/txsnatch/src

--- bin/eclipse_project.pl
+++ bin/eclipse_project.pl
@@ -48,7 +48,7 @@ sub add_lib {
 my(@jar_dirs);
 
 for my $path (@dirs) {
-    for my $src (qw(src build/src installer/src)) {
+    for my $src (qw(src build/src installer/src tools/txsnatch/src)) {
 	my $src_dir = "$path/$src";
 	push @src, $src_dir if -d $src_dir;
     }
