47f51d063621f8df6057b75bfac0dd24947198be
heather <heather@572de364-d518-0410-a359-afb9c36ce0ec>
2007-02-21 21:37:53 +0000

    fix for duplicate rows/headers being created in the dashboard portlets in Safari, fix for column header wrapping in the metric portlet in IE

--- web/js/requests.js
+++ web/js/requests.js
@@ -1,3 +1,109 @@
+var BrowserDetect = {
+	init: function () {
+		this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+		this.version = this.searchVersion(navigator.userAgent)
+			|| this.searchVersion(navigator.appVersion)
+			|| "an unknown version";
+		this.OS = this.searchString(this.dataOS) || "an unknown OS";
+	},
+	searchString: function (data) {
+		for (var i=0;i<data.length;i++)	{
+			var dataString = data[i].string;
+			var dataProp = data[i].prop;
+			this.versionSearchString = data[i].versionSearch || data[i].identity;
+			if (dataString) {
+				if (dataString.indexOf(data[i].subString) != -1)
+					return data[i].identity;
+			}
+			else if (dataProp)
+				return data[i].identity;
+		}
+	},
+	searchVersion: function (dataString) {
+		var index = dataString.indexOf(this.versionSearchString);
+		if (index == -1) return;
+		return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+	},
+	dataBrowser: [
+		{ 	string: navigator.userAgent,
+			subString: "OmniWeb",
+			versionSearch: "OmniWeb/",
+			identity: "OmniWeb"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Apple",
+			identity: "Safari"
+		},
+		{
+			prop: window.opera,
+			identity: "Opera"
+		},
+		{
+			string: navigator.vendor,
+			subString: "iCab",
+			identity: "iCab"
+		},
+		{
+			string: navigator.vendor,
+			subString: "KDE",
+			identity: "Konqueror"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Firefox",
+			identity: "Firefox"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Camino",
+			identity: "Camino"
+		},
+		{		// for newer Netscapes (6+)
+			string: navigator.userAgent,
+			subString: "Netscape",
+			identity: "Netscape"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "MSIE",
+			identity: "Explorer",
+			versionSearch: "MSIE"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Gecko",
+			identity: "Mozilla",
+			versionSearch: "rv"
+		},
+		{ 		// for older Netscapes (4-)
+			string: navigator.userAgent,
+			subString: "Mozilla",
+			identity: "Netscape",
+			versionSearch: "Mozilla"
+		}
+	],
+	dataOS : [
+		{
+			string: navigator.platform,
+			subString: "Win",
+			identity: "Windows"
+		},
+		{
+			string: navigator.platform,
+			subString: "Mac",
+			identity: "Mac"
+		},
+		{
+			string: navigator.platform,
+			subString: "Linux",
+			identity: "Linux"
+		}
+	]
+
+};
+BrowserDetect.init();
+
 {
     Ajax.Responders.register({
         onCreate: function() {
@@ -27,10 +133,18 @@
             if (mList && mList.length > 0) {
 
                 var tbody = problemTable.getElementsByTagName('tbody')[0];
-                for (var i = tbody.childNodes.length - 1; i > 1; i--) {
-                    tbody.removeChild(tbody.childNodes[i]);
-                }
+                var browser = BrowserDetect.browser;
 
+                if (browser == "Safari") {
+                    for (var i = tbody.childNodes.length; i > 1; i--) {
+                    tbody.removeChild(tbody.childNodes[i]);
+                    }
+                } else {
+                    for (var i = tbody.childNodes.length - 1; i > 1; i--) {
+                    tbody.removeChild(tbody.childNodes[i]);
+                    }
+                }
+                
                 for (i = 0; i < mList.length; i++) {
 
                     var tr = document.createElement('tr');
@@ -269,8 +383,18 @@
             $(noAvailTable).style.display = '';
         } else {
             var tbody = availTable.getElementsByTagName('tbody')[0];
-            for (var i = tbody.childNodes.length - 1; i > 1; i--) {
+            var browser = BrowserDetect.browser;
+            
+            if (browser == "Safari") {
+
+            for (var i = tbody.childNodes.length; i > 1; i--) {
                 tbody.removeChild(tbody.childNodes[i]);
+                }
+            } else {
+
+             for (var i = tbody.childNodes.length - 1; i > 1; i--) {
+                tbody.removeChild(tbody.childNodes[i]);
+                 }
             }
 
             for (var i = 0; i < availList.length; i++) {
@@ -363,11 +487,19 @@
             metricFunc = 'requestMetricsResponse()';
         }
 
-        var tbody = metricTable.getElementsByTagName('tbody')[0];
-
         if (metricValues.values) {
-            for (var a = tbody.childNodes.length - 1; a > 0; a--) {
+
+            var tbody = metricTable.getElementsByTagName('tbody')[0];
+            var browser = BrowserDetect.browser;
+            
+            if (browser == "Safari") {
+                for (var a = tbody.childNodes.length; a > 0; a--) {
                 tbody.removeChild(tbody.childNodes[a]);
+                }
+            } else {
+                for (var a = tbody.childNodes.length - 1; a > 0; a--) {
+                tbody.removeChild(tbody.childNodes[a]);
+                }
             }
 
             // Create table headers
@@ -393,7 +525,7 @@
             th2.style.padding = "3px";
             th2.style.textAlign = "left";
             th2.style.borderBottom = "1px solid #D5D8DE";
-            th2.setAttribute("nowrap", true);
+            th2.setAttribute("noWrap", true);
             th2.appendChild(document.createTextNode(resourceLoadTypeHeader));
 
             for (i = 0; i < metricValues.values.length; i++) {
@@ -594,3 +726,5 @@ function refreshDate() {
 function reportError(originalRequest) {
     alert('Error ' + originalRequest.status + ' -- ' + originalRequest.statusText);
 }
+
+
