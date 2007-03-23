99b12fac8cc8cce419bdd0f816624c8f828337a0
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2007-03-23 20:43:39 +0000

    Add MxLiveData plugin.

--- src/org/hyperic/hq/product/jmx/MxLiveDataPlugin.java
+++ src/org/hyperic/hq/product/jmx/MxLiveDataPlugin.java
@@ -27,20 +27,119 @@ package org.hyperic.hq.product.jmx;
 
 import org.hyperic.hq.product.LiveDataPlugin;
 import org.hyperic.hq.product.PluginException;
+import org.hyperic.hq.product.TypeInfo;
 import org.hyperic.util.config.ConfigResponse;
+import org.hyperic.util.config.ConfigSchema;
+import org.hyperic.util.config.StringConfigOption;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javax.management.MBeanServerConnection;
+import javax.management.ObjectName;
+import javax.management.MBeanInfo;
+import javax.management.MBeanOperationInfo;
+import javax.management.MBeanParameterInfo;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.CompositeType;
+import java.util.Properties;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
 
 public class MxLiveDataPlugin extends LiveDataPlugin {
+
+    private static Log _log = LogFactory.getLog(MxLiveDataPlugin.class);
+
+    private static final String PROP_OBJNAME = "ObjectName";
+    private static final String PROP_METHOD  = "method";
+
+    private static final String CMD_GET    = "get";
+    private static final String CMD_INVOKE = "invoke";
+
+    private static final String[] _COMMANDS = {
+        CMD_GET,
+        CMD_INVOKE
+    };
 
     public Object getData(String command, ConfigResponse config)
         throws PluginException
     {
-        return new Object();
+        Properties props = config.toProperties();
+        _log.info("Using properties: " + props);
+
+        try {
+            MBeanServerConnection mBeanServer = MxUtil.getMBeanServer(props);
+            ObjectName oName = new ObjectName(props.getProperty(PROP_OBJNAME));
+            String method = props.getProperty(PROP_METHOD);
+
+            Object res;
+            if (command.equals(CMD_INVOKE)) {
+                res = mBeanServer.invoke(oName, method, new Object[0],
+                                         new String[0]);
+            } else if (command.equals(CMD_GET)) {
+                res = mBeanServer.getAttribute(oName, method);
+            } else {
+                throw new PluginException("Unkown command " + command);
+            }
+
+            return res;
+
+        } catch (Exception e) {
+            throw new PluginException(e);
+        }
     }
 
     public String[] getCommands() {
-        return new String[0];
+        return _COMMANDS;
     }
 
-    public static void main(String[] args) throws Exception {
+    public Object getOldData(String command, ConfigResponse config)
+        throws PluginException
+    {
+        Properties props = config.toProperties();
+        _log.info("Using properties: " + props);
+
+        try {
+            ObjectName oName = new ObjectName("java.lang:type=Threading");
+            MBeanServerConnection mBeanServer = MxUtil.getMBeanServer(props);
+
+            CompositeData data = (CompositeData)
+                mBeanServer.invoke(oName, "getThreadInfo",
+                                   new Long[] { new Long(1) },
+                                   new String[] { Long.TYPE.getName() } );
+
+            CompositeType type = data.getCompositeType();
+            Set keys = type.keySet();
+
+            JSONArray arr = new JSONArray();
+            JSONObject json = new JSONObject();
+            for (Iterator i = keys.iterator(); i.hasNext(); ) {
+                String key = (String)i.next();
+                Object o = data.get(key);
+                json.put(key, o);
+            }
+            arr.put(json);
+
+            return arr;
+
+        } catch (Exception e) {
+            throw new PluginException(e);
+        }
+    }
+
+    public ConfigSchema getConfigSchema() {
+        ConfigSchema schema = new ConfigSchema();
+
+        StringConfigOption object =
+            new StringConfigOption(PROP_OBJNAME, "Object name");
+        StringConfigOption method =
+            new StringConfigOption(PROP_METHOD, "Method to invoke");
+
+        schema.addOption(object);
+        schema.addOption(method);
+
+        return schema;
     }
 }
