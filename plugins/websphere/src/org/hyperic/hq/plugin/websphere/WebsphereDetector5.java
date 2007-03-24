fed3c9b2371c933f8e7e3198293be84b42954390
dougm <dougm@572de364-d518-0410-a359-afb9c36ce0ec>
2007-03-24 03:21:50 +0000

    remove unused import

--- plugins/websphere/src/org/hyperic/hq/plugin/websphere/WebsphereDetector5.java
+++ plugins/websphere/src/org/hyperic/hq/plugin/websphere/WebsphereDetector5.java
@@ -27,7 +27,6 @@ package org.hyperic.hq.plugin.websphere;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
