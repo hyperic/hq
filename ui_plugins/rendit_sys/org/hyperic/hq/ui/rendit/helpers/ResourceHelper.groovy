c1a0d5647bdcb757e0b83fb19664fc22c63bdd16
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-03-25 17:14:41 +0000

    Only parse strings into ints .. not ints.  Also add find by resource ent ID

--- ui_plugins/rendit_sys/org/hyperic/hq/ui/rendit/helpers/ResourceHelper.groovy
+++ ui_plugins/rendit_sys/org/hyperic/hq/ui/rendit/helpers/ResourceHelper.groovy
@@ -2,6 +2,7 @@ package org.hyperic.hq.ui.rendit.helpers
 
 import org.hyperic.util.pager.PageControl
 
+import org.hyperic.hq.appdef.shared.AppdefEntityValue
 import org.hyperic.hq.appdef.shared.AppdefResourceValue
 import org.hyperic.hq.appdef.server.session.AppdefGroupManagerEJBImpl
 import org.hyperic.hq.appdef.server.session.PlatformManagerEJBImpl
@@ -36,7 +37,7 @@ class ResourceHelper
     /**
      * Generic method to find resources.  The results are constrained by the
      * authorization of the current user.  The result objects are subclasses  
-     * of AppdefResourceValue
+     * of AppdefResourceValue.
      * 
      * Examples:
      * 
@@ -48,12 +49,22 @@ class ResourceHelper
      *
      *     To find all the platforms:
      *       > find all:'platforms'
+     *
+     *     To find a resource (by appdef entityID)
+     *       > find resource:entId
      */
     def find(args) {
-         if (args.containsKey('all')) {
+        if (args.containsKey('resource'))
+            return findByEntId(args) 
+            
+        if (args.containsKey('all')) 
             return findAll(args)
-        }
+
         findSingle(args)
+    }
+    
+    private def findByEntId(args) {
+        new AppdefEntityValue(args['resource'], userVal).resourceValue
     }
     
     private def findAll(args) {
@@ -86,11 +97,13 @@ class ResourceHelper
 
         def resourceVal = args[resourceType]
         def argType = (resourceVal instanceof String) ? 'str' : 'int'
-        
-        try { // If it's a string with an integer inside, try that
-            resourceVal = Integer.valueOf(resourceVal)
-            argType = 'int'
-        } catch(NumberFormatException e) {
+
+        if (argType == 'str') {
+            try { // If it's a string with an integer inside, try that
+                resourceVal = Integer.valueOf(resourceVal)
+                argType = 'int'
+            } catch(NumberFormatException e) {
+            }
         }
         NAME_FINDERS[resourceType][argType](resourceVal)
     }
