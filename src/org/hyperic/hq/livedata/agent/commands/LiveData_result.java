d55a4f76a8a7be45604cee5bc1133e703b2be23b
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2007-03-25 22:21:07 +0000

    Compress livedata results.

--- src/org/hyperic/hq/livedata/agent/commands/LiveData_result.java
+++ src/org/hyperic/hq/livedata/agent/commands/LiveData_result.java
@@ -28,8 +28,18 @@ package org.hyperic.hq.livedata.agent.commands;
 import org.hyperic.hq.agent.AgentRemoteValue;
 import org.hyperic.hq.agent.AgentAssertionException;
 import org.hyperic.hq.agent.AgentRemoteException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+import java.util.zip.DataFormatException;
 
 public class LiveData_result extends AgentRemoteValue {
+
+    private static final Log _log = LogFactory.getLog(LiveData_result.class);
 
     private static final String PARAM_RESULT = "result";
 
@@ -42,18 +52,78 @@ public class LiveData_result extends AgentRemoteValue {
     }
 
     public LiveData_result(AgentRemoteValue val)
-        throws AgentRemoteException {
-
-        String result = val.getValue(PARAM_RESULT);
-
-        setResult(result);
+        throws AgentRemoteException
+    {
+        String resultAlreadyCompressed = val.getValue(PARAM_RESULT);
+        // Only used from the LiveDataClient, where the result value has
+        // already been compressed.
+        setResultCompressed(resultAlreadyCompressed);
     }
 
-    public void setResult(String result) {
-        super.setValue(PARAM_RESULT, result);
+    public void setResultCompressed(String alreadyCompressed) {
+        super.setValue(PARAM_RESULT, alreadyCompressed);
     }
 
-    public String getResult() {
-        return super.getValue(PARAM_RESULT);
+    public void setResult(String result)
+        throws IOException
+    {
+        String compressed = compress(result);
+        _log.debug("Compressed " + result.length() + " bytes to " +
+                   compressed.length() + " bytes.");
+
+        super.setValue(PARAM_RESULT, compressed);
+    }
+
+    public String getResult()
+        throws IOException, DataFormatException
+    {
+        String compressed = super.getValue(PARAM_RESULT);
+        _log.debug("Decompressing " + compressed.length() + " bytes");
+        return decompress(compressed);
+    }
+
+    private String compress(String s)
+        throws IOException
+    {
+        Deflater compressor = new Deflater();
+        compressor.setInput(s.getBytes());
+        compressor.finish();
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        int total = 0;
+        byte[] buf = new byte[1024];
+        while (!compressor.finished()) {
+            int count = compressor.deflate(buf);
+            total += count;
+            bos.write(buf, 0, count);
+        }
+
+        bos.close();
+
+        byte[] compressedData = bos.toByteArray();
+        return new String(compressedData, 0, total);
+    }
+
+    public static String decompress(String s)
+        throws IOException, DataFormatException
+    {
+        Inflater decompressor = new Inflater();
+        decompressor.setInput(s.getBytes());
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        int total = 0;
+        byte[] buf = new byte[1024];
+        while (!decompressor.finished()) {
+            int count = decompressor.inflate(buf);
+            total += count;
+            bos.write(buf, 0, count);
+        }
+
+        bos.close();
+
+        byte[] decompressedData = bos.toByteArray();
+        return new String(decompressedData, 0, total);
     }
 }
