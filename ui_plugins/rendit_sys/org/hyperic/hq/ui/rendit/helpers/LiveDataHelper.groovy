afe3839d6c225ad3992a3035e18fca74758a1b4a
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-03-27 23:42:45 +0000

    Use categories, and simplify APIs so that we only deal with entity IDs and not resource value objects

--- ui_plugins/rendit_sys/org/hyperic/hq/ui/rendit/helpers/LiveDataHelper.groovy
+++ ui_plugins/rendit_sys/org/hyperic/hq/ui/rendit/helpers/LiveDataHelper.groovy
@@ -2,7 +2,6 @@ package org.hyperic.hq.ui.rendit.helpers
 
 import org.hyperic.hq.product.PluginNotFoundException
 import org.hyperic.hq.appdef.shared.AppdefEntityID
-import org.hyperic.hq.appdef.shared.AppdefResourceValue
 import org.hyperic.util.config.ConfigResponse
 import org.hyperic.hq.livedata.server.session.LiveDataManagerEJBImpl
 import org.hyperic.hq.livedata.shared.LiveDataResult
@@ -17,10 +16,6 @@ class LiveDataHelper
 
     private getDataMan() { LiveDataManagerEJBImpl.one }
     
-    String[] getCommands(AppdefResourceValue resource) {
-        dataMan.getCommands(userVal, resource.entityId)
-    }
-
     String[] getCommands(AppdefEntityID id) {
         dataMan.getCommands(userVal, id)
     }
@@ -29,19 +24,21 @@ class LiveDataHelper
         dataMan.getData(userVal, commands)
     }
 
-    LiveDataResult getData(LiveDataCommand command)
-    {
+    LiveDataResult getData(AppdefEntityID ent, String command, config) { 
+        def cmd = [ent, command, config as ConfigResponse] as LiveDataCommand 
+        dataMan.getData(userVal, cmd)
+    }
+
+    LiveDataResult getData(LiveDataCommand command) {
         dataMan.getData(userVal, command)
     }
     
     /** 
      * Check if a resource supports a command.  
-     * XXX:  Would be cool if we could just attach this functionality directly
-     *       to the entity ID or the resource via categories or metaclasses!
      */
     boolean resourceSupports(AppdefEntityID id, String command) {
         try {
-            return (dataMan.getCommands(userVal, id) as List).contains(command)
+            return (id.liveDataCommands as List).contains(command) 
         } catch(PluginNotFoundException e) {
             return false
         }
