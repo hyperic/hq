7ec43b47cf5fa207ba25dcf2b0444a22da1c700d
scottmf <scottmf@572de364-d518-0410-a359-afb9c36ce0ec>
2007-04-18 22:43:59 +0000

    [HHQ-191] Informix Plugin with Autodiscovery

--- plugins/informix/src/org/hyperic/hq/plugin/informix/InformixMeasurementPlugin.java
+++ plugins/informix/src/org/hyperic/hq/plugin/informix/InformixMeasurementPlugin.java
@@ -1,4 +1,4 @@
-/*
+/**
  * NOTE: This copyright does *not* cover user programs that use HQ
  * program services by normal system calls through the application
  * program interfaces provided as part of the Hyperic Plug-in Development
@@ -21,14 +21,18 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA.
- */
+**/
 
 package org.hyperic.hq.plugin.informix;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -40,16 +44,29 @@ import org.hyperic.util.StringUtil;
 import org.hyperic.util.config.ConfigResponse;
 import org.hyperic.util.config.ConfigSchema;
 import org.hyperic.util.config.SchemaBuilder;
+import org.hyperic.util.jdbc.DBUtil;
+import org.hyperic.hq.product.PluginException;
+import org.hyperic.hq.product.MetricUnreachableException;
+import org.hyperic.hq.product.MetricInvalidException;
+import org.hyperic.hq.product.MetricNotFoundException;
+import org.hyperic.hq.product.MetricValue;
 
 public class InformixMeasurementPlugin extends JDBCMeasurementPlugin
 {
-    private static final boolean DEBUG = true;
-    private static final String JDBC_DRIVER = "com.informix.jdbc.IfxDriver";
+    private static final boolean DEBUG = InformixPluginUtil.DEBUG;
 
-    private static final String PROP_DBNAME = "dbname",
-                                PROP_TABLENAME = "tablename";
+    static final String PROP_DBNAME    = InformixPluginUtil.PROP_DBNAME,
+                        PROP_TABLENAME = InformixPluginUtil.PROP_TABLENAME,
+                        PROP_CHUNK     = InformixPluginUtil.PROP_CHUNK,
+                        JDBC_DRIVER    = InformixPluginUtil.JDBC_DRIVER,
+                        LOCKREQUESTS   = "lockreqs",
+                        LOCKWAITS      = "lockwts",
+                        DEADLOCKS      = "deadlks",
+                        IO_READS       = "bufreads",
+                        IO_WRITES      = "bufwrites";
 
-    private Map myQueries = new HashMap();
+    private Map myQueries = new HashMap(),
+                myFields = new HashMap();
 
     protected void getDriver() throws ClassNotFoundException
     {
@@ -64,33 +81,25 @@ public class InformixMeasurementPlugin extends JDBCMeasurementPlugin
 
     protected String getDefaultURL()
     {
-        return "jdbc:informix-sqli://"+getProperty("hostname")+":"+getProperty("port")+
-               "/sysmaster:informixserver="+getProperty("servername");
+        return "jdbc:informix-sqli://vmlin-002:3500/sysmaster:informixserver=test_shm";
+//        return "jdbc:informix-sqli://localhost:3500/sysmaster:informixserver=test_shm";
     }
 
     protected void initQueries() 
     {
-        String sqlstmt = "";
-/*
-        myQueries.put("SpaceAvailable", sqlstmt);
-        sqlstmt = "";
-        myQueries.put("ChunkIOWrites", sqlstmt);
-        sqlstmt = "";
-        myQueries.put("ChunkIOReads", sqlstmt);
-        sqlstmt = "";
-        myQueries.put("Availability", sqlstmt);
-        sqlstmt = "";
-        myQueries.put("NumberOfLockWaits", sqlstmt);
-        sqlstmt = "";
-        myQueries.put("NumberOfUsersWaitingForLocks", sqlstmt);
-        sqlstmt = "";
-        myQueries.put("NumberOfActiveLocks", sqlstmt);
-*/
-
+        String sqlstmt = "select c.nfree*c.pagesize free_space "+
+                         "from sysdbspaces d, syschunks c "+
+                         "where d.dbsnum = c.dbsnum and d.name = '%chunk%' "+
+                         "group by 1";
+        myQueries.put("ChunkSpaceAvailable", sqlstmt);
+        myQueries.put("TotNumCurrentSessionWrites", getCurrentSessionStatsSQL(IO_WRITES));
+        myQueries.put("TotNumCurrentSessionReads", getCurrentSessionStatsSQL(IO_READS));
+        myQueries.put("NumberOfLockWaits", getTableLockStatsSQL(LOCKWAITS));
+        myQueries.put("NumberOfDeadLocks", getTableLockStatsSQL(DEADLOCKS));
         sqlstmt = "select count(*) from sysdatabases";
         myQueries.put("Availability", sqlstmt);
         sqlstmt = "select sum(lockwts) as numlockwaits from sysptprof";
-        myQueries.put("TotNumLocksWaits", sqlstmt);
+        myQueries.put("TotNumLockWaits", sqlstmt);
         sqlstmt = "select sum(lockreqs) as numlockreqs from syssesprof, syssessions "+
                   "where syssesprof.sid = syssessions.sid";
         myQueries.put("TotNumUserWaitLocks", sqlstmt);
@@ -98,16 +107,143 @@ public class InformixMeasurementPlugin extends JDBCMeasurementPlugin
         myQueries.put("TotNumDeadLocks", sqlstmt);
     }
 
+    public MetricValue getValue(Metric metric)
+        throws PluginException,
+               MetricUnreachableException,
+               MetricInvalidException,
+               MetricNotFoundException
+    {
+        String objectName = metric.getObjectName(),
+               attr       = metric.getAttributeName();
+        if (objectName.indexOf("DBSpace") == -1 || attr.indexOf("ChunkIO") == -1)
+            return super.getValue(metric);
+
+        try
+        {
+            String chunkName = metric.getObjectProperty(PROP_CHUNK);
+            Connection conn = getCachedConnection(metric);
+            long value = getChunkIOStats(attr, chunkName, conn).longValue();
+            return new MetricValue(value, System.currentTimeMillis());
+        }
+        catch (SQLException e) {
+            String msg = "Query failed for "+attr+": "+e.getMessage();
+            throw new MetricNotFoundException(msg, e);
+        }
+    }
+
+    private static Long getChunkIOStats(String attr, String chunkName, Connection conn)
+        throws SQLException
+    {
+        Statement stmt = null;
+        ResultSet rs = null;
+        try
+        {
+            String field = "writes";
+            if (attr.equals("ChunkIOPageWrites"))
+                field = "pageswritten";
+            else if (attr.equals("ChunkIOPageReads"))
+                field = "pagesread";
+            else if (attr.equals("ChunkIOReads"))
+                field = "reads";
+            else //attr.equals("ChunkIOWrites")
+                field = "writes";
+            stmt = conn.createStatement();
+            List sql_list = getChunkIOStatsSQL(field, attr, chunkName, stmt);
+            stmt.execute((String)sql_list.get(0));
+            stmt.execute((String)sql_list.get(1));
+            rs = stmt.executeQuery((String)sql_list.get(2));
+            if (rs.next())
+              return new Long(rs.getLong(field));
+        }
+        finally
+        {
+            try
+            {
+                if (rs != null) 
+                    rs.close();
+                if (stmt != null)
+                {
+                    stmt.execute("drop table A");
+                    stmt.execute("drop table B");
+                    stmt.close();
+                }
+            } catch (SQLException e) { }
+        }
+        throw new SQLException();
+    }
+
+    private static List getChunkIOStatsSQL(String field, String attr,
+                                           String chunkName, Statement stmt)
+    {
+        List rtn = new ArrayList();
+
+        String sql = //-- Collect chunk IO stats into temp table A
+               "select name dbspace, chknum, \"Primary\" chktype, reads, "+
+               "writes, pagesread, pageswritten "+
+               "from syschktab c, sysdbstab d "+
+               "where c.dbsnum = d.dbsnum "+
+               "union all "+
+               "select name[1,10] dbspace, chknum, \"Mirror\" chktype, reads,"+
+               "writes, pagesread, pageswritten "+
+               "from sysmchktab c, sysdbstab d "+
+               "where c.dbsnum = d.dbsnum "+
+               "into temp A\n\n";
+        rtn.add(sql);
+        sql =  //-- Collect total IO stats into temp table B
+               "select sum(reads) total_reads, sum(writes) total_writes, "+
+               "sum(pagesread) total_pgreads,sum(pageswritten) total_pgwrites "+
+               "from A "+
+               "into temp B\n\n";
+        rtn.add(sql);
+        sql =  //-- Report showing each chunks percent of total IO
+               "select "+field+" from A, B "+
+               "where dbspace = '"+chunkName+"'";
+        rtn.add(sql);
+        return rtn;
+    }
+
     protected String getQuery(Metric metric)
     {
-        String queryVal = metric.getAttributeName();
-        String query = (String)myQueries.get(queryVal);
-        String inst = metric.getObjectProperties().getProperty(PROP_DBNAME);
+        String queryVal = metric.getAttributeName(),
+               query    = (String)myQueries.get(queryVal),
+               attr     = metric.getAttributeName(),
+               objectName = metric.getObjectName();
 
-        // Backwards compat
-        if (inst == null)
-            inst = metric.getProperties().getProperty(PROP_TABLENAME);
+        boolean isAvail = attr.equals(AVAIL_ATTR);
 
-        return query;
+        if (objectName.indexOf("Type=Server") != -1)
+            return (String)myQueries.get(attr);
+
+        else if (objectName.indexOf("Type=Table") != -1)
+        {
+            String dbname = metric.getObjectProperty(PROP_DBNAME),
+                   table  = metric.getObjectProperty(PROP_TABLE),
+                   sql    = myQueries.get(attr).toString().
+                                      replaceAll("%dbname%", dbname).
+                                      replaceAll("%table%", table);
+            return sql;
+        }
+        else if (objectName.indexOf("Type=DBSpace") != -1)
+        {
+            String chunk = metric.getObjectProperty(PROP_CHUNK),
+                   sql   = myQueries.get(attr).toString().
+                                     replaceAll("%chunk%", chunk);
+            return sql;
+        }
+        return null;
+    }
+
+    private String getCurrentSessionStatsSQL(String field)
+    {
+        return "select sum("+field+") "+
+               "from syssesprof, syssessions "+
+               "where syssesprof.sid = syssessions.sid";
+    }
+
+    private String getTableLockStatsSQL(String field)
+    {
+        return "select "+field+" "+
+               "from sysptprof "+
+               "where tabname = '%table%' and dbsname = '%dbname%'";
     }
 }
