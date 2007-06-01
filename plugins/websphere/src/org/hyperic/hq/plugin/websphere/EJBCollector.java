25965d06436251f9b2d249b69318ab66d8567f12
dougm <dougm@572de364-d518-0410-a359-afb9c36ce0ec>
2007-06-01 15:36:50 +0000

    stub service collectors

---
+++ plugins/websphere/src/org/hyperic/hq/plugin/websphere/EJBCollector.java
@@ -0,0 +1,8 @@
+package org.hyperic.hq.plugin.websphere;
+
+public class EJBCollector extends WebsphereCollector {
+
+    public void collect() {
+
+    }
+}
---
+++ plugins/websphere/src/org/hyperic/hq/plugin/websphere/WebappCollector.java
@@ -0,0 +1,8 @@
+package org.hyperic.hq.plugin.websphere;
+
+public class WebappCollector extends WebsphereCollector {
+
+    public void collect() {
+
+    }
+}
