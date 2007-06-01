cc2d1a5f977ce5f97c5635366b83ab1cd2870ad2
dougm <dougm@572de364-d518-0410-a359-afb9c36ce0ec>
2007-06-01 16:19:51 +0000

    change Application to use collector

--- plugins/websphere/src/org/hyperic/hq/plugin/websphere/ApplicationCollector.java
+++ plugins/websphere/src/org/hyperic/hq/plugin/websphere/ApplicationCollector.java
@@ -1,8 +1,35 @@
 package org.hyperic.hq.plugin.websphere;
 
+import javax.management.ObjectName;
+
+import org.hyperic.hq.product.PluginException;
+
+import com.ibm.websphere.management.AdminClient;
+
 public class ApplicationCollector extends WebsphereCollector {
 
-    public void collect() {
+    private ObjectName name;
 
+    protected void init(AdminClient mServer) throws PluginException {
+        super.init(mServer);
+
+        this.name =
+            newObjectNamePattern("type=J2EEApplication," +
+                                 "name=" + getModuleName() + "," +
+                                 getProcessAttributes());
+        
+        this.name = resolve(mServer, this.name);
+    }
+
+    public void collect() {
+        Object state =
+            getAttribute(getMBeanServer(), this.name, "state");
+
+        if (state == null) {
+            setAvailability(false);
+        }
+        else {
+            setAvailability(true);
+        }
     }
 }
