a3e6f1f8f9a24b4f6a493084542d6c7b5ad7dfa4
dougm <dougm@572de364-d518-0410-a359-afb9c36ce0ec>
2007-06-01 16:52:42 +0000

    change EJB to use collector

--- plugins/websphere/src/org/hyperic/hq/plugin/websphere/EJBCollector.java
+++ plugins/websphere/src/org/hyperic/hq/plugin/websphere/EJBCollector.java
@@ -1,8 +1,43 @@
 package org.hyperic.hq.plugin.websphere;
 
+import javax.management.ObjectName;
+
+import org.hyperic.hq.product.PluginException;
+
+import com.ibm.websphere.management.AdminClient;
+
 public class EJBCollector extends WebsphereCollector {
+    private ObjectName name;
+
+    protected void init(AdminClient mServer) throws PluginException {
+        super.init(mServer);
+
+        String module = getModuleName();
+        int ix = module.indexOf('#');
+        if (ix == -1) {
+            throw new PluginException("Malformed ejb name '" + module + "'");
+        }
+        String app = module.substring(0, ix);
+        String ejb = module.substring(ix+1);
+
+        this.name =
+            newObjectNamePattern("j2eeType=EJBModule," +
+                                 "J2EEApplication=" + app + "," +
+                                 "name=" + ejb + "," +
+                                 getProcessAttributes());
+        
+        this.name = resolve(mServer, this.name);
+    }
 
     public void collect() {
+        Object ejbs =
+            getAttribute(getMBeanServer(), this.name, "ejbs");
 
+        if (ejbs == null) {
+            setAvailability(false);
+        }
+        else {
+            setAvailability(true);
+        }
     }
 }
