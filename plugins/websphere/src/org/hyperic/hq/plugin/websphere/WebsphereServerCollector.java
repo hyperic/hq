4b655399c71afa543a30a3d42ddf0ebe8c06d652
dougm <dougm@572de364-d518-0410-a359-afb9c36ce0ec>
2007-06-04 23:59:20 +0000

    use super.name

--- plugins/websphere/src/org/hyperic/hq/plugin/websphere/WebsphereServerCollector.java
+++ plugins/websphere/src/org/hyperic/hq/plugin/websphere/WebsphereServerCollector.java
@@ -2,7 +2,6 @@ package org.hyperic.hq.plugin.websphere;
 
 import java.util.Map;
 
-import javax.management.ObjectName;
 import javax.management.j2ee.statistics.Stats;
 
 import org.hyperic.hq.product.PluginException;
@@ -11,18 +10,16 @@ import com.ibm.websphere.management.AdminClient;
 
 public class WebsphereServerCollector extends WebsphereCollector {
 
-    private ObjectName jvm;
-
     protected void init(AdminClient mServer) throws PluginException {
         super.init(mServer);
 
-        this.jvm =
+        this.name =
             newObjectNamePattern("name=JVM," +
                                  "type=JVM," +
                                  "j2eeType=JVM," +
                                   getServerAttributes());
 
-        this.jvm = resolve(mServer, this.jvm);
+        this.name = resolve(mServer, this.name);
     }
 
     public void collect() {
@@ -36,7 +33,7 @@ public class WebsphereServerCollector extends WebsphereCollector {
         Map values = getResult().getValues();
 
         Stats jvmStats =
-            (Stats)getStats(mServer, this.jvm);
+            (Stats)getStats(mServer, this.name);
 
         if (jvmStats != null) {
             double total = getStatCount(jvmStats, "HeapSize");
