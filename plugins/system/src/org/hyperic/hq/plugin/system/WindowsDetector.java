04207247f605edfc394cbdb9fc82204e7d4e266f
dougm <dougm@572de364-d518-0410-a359-afb9c36ce0ec>
2007-06-06 18:27:58 +0000

    [HHQ-919] Add auto-discovery support for Windows service based plugins (merged from trunk)

--- plugins/system/src/org/hyperic/hq/plugin/system/WindowsDetector.java
+++ plugins/system/src/org/hyperic/hq/plugin/system/WindowsDetector.java
@@ -26,18 +26,23 @@
 package org.hyperic.hq.plugin.system;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
 import org.hyperic.sigar.win32.Service;
 import org.hyperic.sigar.win32.ServiceConfig;
 import org.hyperic.sigar.win32.Win32Exception;
+import org.hyperic.util.StringUtil;
 import org.hyperic.util.config.ConfigResponse;
 import org.hyperic.util.config.EncodingException;
 
 import org.hyperic.hq.appdef.shared.AIServiceValue;
+import org.hyperic.hq.product.MeasurementPlugin;
 import org.hyperic.hq.product.PluginException;
+import org.hyperic.hq.product.ProductPluginManager;
 
 public class WindowsDetector
     extends SystemServerDetector {
@@ -57,57 +62,182 @@ public class WindowsDetector
         return null;
     }
 
-    /**
-     * Windows services are created manually.
-     */
-    protected ArrayList getSystemServiceValues(Sigar sigar, ConfigResponse serverConfig)
+    private boolean setCustomProperty(String type, ConfigResponse cprops,
+                                      String key, String val) {
+
+        //XXX code below does not work for the builtin
+        //service type because the cprops are defined by
+        //the SystemPlugin/ProductPlugin rather than hq-plugin.xml
+        if (type.equals(SystemPlugin.SVC_NAME)) {
+            cprops.setValue(key, val);
+            return true;
+        }
+
+        ProductPluginManager manager =
+            (ProductPluginManager)getManager().getParent();
+        MeasurementPlugin plugin =
+            manager.getMeasurementPlugin(type);
+
+        if (plugin == null) {
+            return false;
+        }
+
+        if (plugin.getCustomPropertiesSchema().getOption(key) == null) {
+            return false;
+        }
+        else {
+            cprops.setValue(key, val);
+            return true;
+        }
+    }
+
+    private ServiceConfig setServiceInventoryProperties(String serviceName,
+                                                        AIServiceValue svc,
+                                                        boolean exists) {
+
+        ConfigResponse cprops = new ConfigResponse();     
+        ServiceConfig config;
+        Service service = null;
+        try {
+            service = new Service(serviceName);
+            config = service.getConfig();
+        } catch (Win32Exception e) {
+            String msg =
+                "Error getting config for service=" +
+                serviceName + ": " + e.getMessage();
+            if (exists) {
+                log.error(msg);
+            }
+            else {
+                log.debug(serviceName + " does not exist");
+            }
+            return null;
+        } finally {
+            if (service != null){
+                service.close();
+            }
+        }
+
+        if (!exists) {
+            if (config.getStartType() == ServiceConfig.START_DISABLED) {
+                log.debug("Skipping " + serviceName +
+                          ", start type=" + config.getStartTypeString());
+                return null;
+            }
+        }
+
+        String desc = config.getDescription();
+        if (desc == null) {
+            desc = config.getDisplayName();
+        }
+        svc.setDescription(desc);
+
+        String type = svc.getServiceTypeName();
+        setCustomProperty(type, cprops,
+                          "path", config.getPath());
+        setCustomProperty(type, cprops,
+                          "startupType", config.getStartTypeString());
+        setCustomProperty(type, cprops,
+                          "displayName", config.getDisplayName());
+        try {
+            svc.setCustomProperties(cprops.encode());
+        } catch (EncodingException e) {
+            log.error("Error encoding cprops: " + e.getMessage());
+            return null;
+        }
+
+        return config;
+    }
+
+    private AIServiceValue findWindowsService(String type, String serviceName) {
+        AIServiceValue svc = createSystemService(type, serviceName);
+        ServiceConfig config =
+            setServiceInventoryProperties(serviceName, svc, false);
+        if (config == null) {
+            return null;
+        }
+        svc.setName(config.getDisplayName());
+        log.debug("Found service " + svc.getName() +
+                  " - " + svc.getDescription());
+        ConfigResponse productConfig = new ConfigResponse();
+        productConfig.setValue(SystemPlugin.PROP_SVC, serviceName);
+        try {
+            svc.setProductConfig(productConfig.encode());
+            svc.setMeasurementConfig(new ConfigResponse().encode());
+            //XXX control config
+        } catch (EncodingException e) {
+            log.error("Error encoding config: " + e.getMessage());
+            return null;
+        }
+    
+        return svc;
+    }
+
+    private void discoverWindowsServices(String names, ArrayList services)
+        throws Win32Exception {
+
+        List serviceNames;
+        if ("true".equals(names)) {
+            serviceNames = Service.getServiceNames();
+        }
+        else {
+            serviceNames = StringUtil.explode(names, ",");
+        }
+
+        for (int i=0; i<serviceNames.size(); i++) {
+            String name = (String)serviceNames.get(i);
+            AIServiceValue svc = 
+                findWindowsService(SystemPlugin.SVC_NAME, name);
+            if (svc != null) {
+                services.add(svc);
+            }
+        }
+    }
+
+    protected ArrayList getSystemServiceValues(Sigar sigar,
+                                               ConfigResponse serverConfig)
         throws SigarException {
 
         String type = SystemPlugin.SVC_NAME;
-        String prop = SystemPlugin.PROP_SVC;
-
         List serviceConfigs = getServiceConfigs(type);
     
         ArrayList services = new ArrayList();
-    
+
+        //set cprops for manually create resources
         for (int i=0; i<serviceConfigs.size(); i++) {
             ConfigResponse serviceConfig = 
                 (ConfigResponse)serviceConfigs.get(i);
-            ConfigResponse cprops = new ConfigResponse();
 
             String name =
                 serviceConfig.getValue(SystemPlugin.PROP_RESOURCE_NAME);
             String serviceName =
-                serviceConfig.getValue(prop);
-        
-            AIServiceValue svc = createSystemService(type, name);
+                serviceConfig.getValue(SystemPlugin.PROP_SVC);
+            AIServiceValue svc =
+                createSystemService(type, name);
 
-            Service service = null;
-            try {
-                service = new Service(serviceName);
-                ServiceConfig config = service.getConfig();
-                String desc = config.getDescription();
-                if (desc == null) {
-                    desc = config.getDisplayName();
-                }
-                svc.setDescription(desc);
-                cprops.setValue("path", config.getPath());
-                cprops.setValue("startupType", config.getStartTypeString());
-                cprops.setValue("displayName", config.getDisplayName());
-                svc.setCustomProperties(cprops.encode());
-            } catch (Win32Exception e) {
-                String msg =
-                    "Error getting config for service=" +
-                    serviceName + ": " + e.getMessage();
-                log.error(msg);
-            } catch (EncodingException e) {
-                log.error("Error encoding cprops: " + e.getMessage());
-            } finally {
-                if (service != null) {
-                    service.close();
-                }
-            }
+            setServiceInventoryProperties(serviceName, svc, true);
+
             services.add(svc);
+        }
+
+        //auto-discovery of services (plugin defined types)
+        Map plugins = getServiceInventoryPlugins();
+        for (Iterator it = plugins.keySet().iterator(); it.hasNext();) {
+            type = (String)it.next();
+            String name = getTypeProperty(type, SystemPlugin.PROP_SVC);
+            log.debug("Looking for " + type + " service=" + name);
+
+            AIServiceValue svc = findWindowsService(type, name);
+            if (svc != null) {
+                services.add(svc);
+            }
+        }
+
+        //auto-discover of any generic windows service
+        String windowsServices =
+            getManager().getProperty("windows.services.discover");
+        if (windowsServices != null) {
+            discoverWindowsServices(windowsServices, services);
         }
 
         return services;
