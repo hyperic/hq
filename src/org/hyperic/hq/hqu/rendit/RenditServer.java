0fef0dbc75e1fa363b05a44aece67cdd3a0e6da4
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-06-13 22:30:12 +0000

    Servlet dir no more

--- src/org/hyperic/hq/hqu/rendit/RenditServer.java
+++ src/org/hyperic/hq/hqu/rendit/RenditServer.java
@@ -34,13 +34,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hyperic.hq.authz.server.session.AuthzSubject;
 import org.hyperic.hq.hqu.UIPluginDescriptor;
 import org.hyperic.hq.hqu.server.session.UIPluginManagerEJBImpl;
 
