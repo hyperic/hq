446cd4aea50bd9904d60e7926566c6f22eae7fb4
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-06-26 23:39:00 +0000

    Add dox for findAlerts(), simplified findAlerts(), and an getAcknowledgedBy for Alerts which actually looks up the user, since we don't have a solid relationship between authzsubject and alerts

--- src/org/hyperic/hq/hqu/rendit_sys/helpers/AlertHelper.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/helpers/AlertHelper.groovy
@@ -3,6 +3,7 @@ package org.hyperic.hq.hqu.rendit.helpers
 import org.hyperic.hibernate.PageInfo
 import org.hyperic.hq.authz.server.session.AuthzSubject
 import org.hyperic.hq.events.server.session.AlertManagerEJBImpl
+import org.hyperic.hq.events.AlertSeverity
 
 class AlertHelper extends BaseHelper {
     private alertMan = AlertManagerEJBImpl.one
@@ -11,7 +12,31 @@ class AlertHelper extends BaseHelper {
         super(user)
     }
 
-    def findAlerts(int priority, long timeRange, long endTime, PageInfo pInfo) {
-        alertMan.findAlerts(user.id, priority, timeRange, endTime, pInfo);
+    /**
+     * Find Alerts within a specified timerange, greater or equal to a 
+     * given priority.
+     *
+     * @param severity  The minimum severity for the returned alerts
+     * @param timeRange The range (in millis) prior to endTime which 
+     *                  the resulting alerts will be contained in
+     * @param endTime   Millis since the epoch specifying the end of the time
+     *                  range that alerts will be returned for
+     * @param pInfo     PageInfo that contains an AlertSortField for its
+     *                  sort parameters
+     */
+    def findAlerts(AlertSeverity severity, long timeRange, long endTime, 
+                   PageInfo pInfo) 
+    {
+        alertMan.findAlerts(user.id, severity.code, timeRange, endTime, pInfo)
+    }
+    
+    /**
+     * Finds all recent alerts with at least the specified severity level.
+     *
+     * @see findAlerts(AlertSeverity, long, long, PageInfo)
+     */
+    def findAlerts(AlertSeverity severity, PageInfo pInfo) {
+        long millis = System.currentTimeMillis()
+        alertMan.findAlerts(user.id, severity.code, millis, millis, pInfo)
     }
 }
--- src/org/hyperic/hq/hqu/rendit_sys/metaclass/AlertCategory.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/metaclass/AlertCategory.groovy
@@ -1,10 +1,20 @@
 package org.hyperic.hq.hqu.rendit.metaclass
 
+import org.hyperic.hq.authz.server.session.AuthzSubject;
+import org.hyperic.hq.authz.server.session.AuthzSubjectManagerEJBImpl
 import org.hyperic.hq.events.server.session.Alert
 
 class AlertCategory {
     static String urlFor(Alert a) {
         def d = a.alertDefinition
         "/alerts/Alerts.do?mode=viewAlert&eid=${d.appdefEntityId}&a=${a.id}"
+    }
+    
+    static AuthzSubject getAcknowledgedBy(Alert a) {
+        def id = a.ackedBy
+        if (id == null)
+            return null
+            
+        AuthzSubjectManagerEJBImpl.one.getSubjectById(id.toInteger())
     }
 }
