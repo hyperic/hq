9b6da3468f16bb056409fe12bb80a24e9752cd4b
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-06-26 23:39:34 +0000

    Add ackedBy column, cleanup

--- ui_plugins/alertcenter/app/AlertController.groovy
+++ ui_plugins/alertcenter/app/AlertController.groovy
@@ -1,11 +1,11 @@
 import org.hyperic.hq.hqu.rendit.BaseController
 
 import java.text.DateFormat
+import org.hyperic.hq.common.YesOrNo
+import org.hyperic.hq.events.AlertSeverity
 import org.hyperic.hq.events.EventConstants
-import org.hyperic.hq.events.server.session.Alert
 import org.hyperic.hq.events.server.session.AlertSortField
 import org.hyperic.hq.hqu.rendit.html.DojoUtil
-import org.hyperic.hq.common.YesOrNo
 
 class AlertController 
 	extends BaseController
@@ -14,28 +14,22 @@ class AlertController
         DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT)
 
     private final TABLE_SCHEMA = [
-        getData: {pageInfo ->
-            alertHelper.findAlerts(0, System.currentTimeMillis(),
-                                   System.currentTimeMillis(), pageInfo)
-        },
+        getData: {pageInfo -> alertHelper.findAlerts(AlertSeverity.LOW, pageInfo)},
         defaultSort: AlertSortField.DATE,
-        defaultSortOrder: 1,  // ascending
+        defaultSortOrder: 0,  // descending
         rowId: {it.id},
-        styleClass: {
-            it.fixed ? null : "#fa8672"
-        },
+        styleClass: {it.fixed ? null : "alertHighlight"},
         columns: [
             [field:AlertSortField.DATE, 
              label:{df.format(it.timestamp)}],
             [field:AlertSortField.DEFINITION,
-             label:{
-                linkTo(it.alertDefinition.name, [resource:it])
-            }],
+             label:{linkTo(it.alertDefinition.name, [resource:it]) }],
             [field:AlertSortField.RESOURCE,
              label:{it.alertDefinition.resource.name}],
             [field:AlertSortField.FIXED,
-             label:{it.fixed ? YesOrNo.YES.value.capitalize() : 
-                               YesOrNo.NO.value.capitalize()}],
+             label:{YesOrNo.valueFor(it.fixed).value.capitalize()}],
+            [field:AlertSortField.ACKED_BY,
+             label:{it.acknowledgedBy?.fullName}],
             [field:AlertSortField.SEVERITY,
              label:{EventConstants.getPriority(it.alertDefinition.priority)}]
         ]
@@ -50,9 +44,7 @@ class AlertController
     }
     
     def data(params) {
-        log.info "Params = ${params}"
-        def json = DojoUtil.processTableRequest(TABLE_SCHEMA, params)        
-        log.info "Result: ${json}"                     
+        def json = DojoUtil.processTableRequest(TABLE_SCHEMA, params)
 		render(inline:"/* ${json} */", contentType:'text/json-comment-filtered')
     }
 }
