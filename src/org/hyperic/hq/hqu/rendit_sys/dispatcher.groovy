e34e8709a3e7f4764b52327699986fe744ccf6ea
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-06-27 02:51:17 +0000

    Add ResourceCategory

--- src/org/hyperic/hq/hqu/rendit_sys/dispatcher.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/dispatcher.groovy
@@ -5,6 +5,7 @@ import org.hyperic.hq.hqu.rendit.PluginLoadException
 import org.hyperic.hq.hqu.rendit.metaclass.AuthzSubjectCategory
 import org.hyperic.hq.hqu.rendit.metaclass.AlertCategory
 import org.hyperic.hq.hqu.rendit.metaclass.MapCategory
+import org.hyperic.hq.hqu.rendit.metaclass.ResourceCategory
 import org.hyperic.hq.hqu.rendit.metaclass.StringCategory
 import org.hyperic.hq.hqu.UIPluginDescriptor
 
@@ -23,7 +24,7 @@ class Dispatcher {
     final int API_MINOR = 1
     
     private final CATEGORIES = [AuthzSubjectCategory, AlertCategory,
-                                MapCategory, StringCategory]  
+                                MapCategory, ResourceCategory, StringCategory]  
     
     private Log log = LogFactory.getLog(Dispatcher.class);
 
---
+++ src/org/hyperic/hq/hqu/rendit_sys/metaclass/ResourceCategory.groovy
@@ -0,0 +1,38 @@
+package org.hyperic.hq.hqu.rendit.metaclass
+
+import org.hyperic.hq.authz.server.session.Resource
+import org.hyperic.hq.authz.shared.AuthzConstants
+import org.hyperic.hq.appdef.shared.AppdefEntityID
+
+class ResourceCategory {
+    /**
+     * Creates a URL for the resource.  This should typically only be called
+     * via HtmlUtil.linkTo (or from a controller).  
+     *
+     * Resources have the following contexts:
+     *  'alert' : Returns a link to the resource's alert definitions
+     *  - Otherwise, simply returns a link to the resource's default page.
+     */
+    static String urlFor(Resource r, String context) {
+        if (context == 'alert') {
+            return "/alerts/Config.do?mode=list&eid=${r.entityID}"
+        } 
+        return "/Resource.do?eid=${r.entityID}"
+    }
+    
+    static AppdefEntityID getEntityID(Resource r) {
+        def typeId = r.resourceType.id
+        
+        if (typeId == AuthzConstants.authzPlatform) {
+            return AppdefEntityID.newPlatformID(r.instanceId)
+        } else if (typeId == AuthzConstants.authzServer) {
+            return AppdefEntityID.newServerID(r.instanceId)
+        } else if (typeId == AuthzConstants.authzService) {
+            return AppdefEntityID.newServiceID(r.instanceId)
+        } else if (typeId == AuthzConstants.authzGroup) {
+            return AppdefEntityID.newGroupID(r.instanceId)
+        } else {
+            throw RuntimeException("Resource [${r}] is not an appdef object")
+        }
+    }
+}
