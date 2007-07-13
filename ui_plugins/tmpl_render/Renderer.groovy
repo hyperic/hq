077cbd6f663646dada06006accb12c07a968950f
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-07-13 17:16:49 +0000

    Use patched SimpleTemplateEngine, which allows us to specify a classloader

--- ui_plugins/tmpl_render/Renderer.groovy
+++ ui_plugins/tmpl_render/Renderer.groovy
@@ -1,5 +1,6 @@
 import groovy.text.SimpleTemplateEngine
 
+
 /**
  * This plugin provides basic .gsp template rendering support.  It is
  * primarily used through the RenditServer.renderTemplate() method
@@ -8,7 +9,9 @@ class Renderer {
     def render(File gspFile, Map params, Writer output) {
         gspFile.withReader { reader ->
 			def eng = new SimpleTemplateEngine()
-			def template = eng.createTemplate(reader)
+		    def template = 
+		        eng.createTemplate(Thread.currentThread().contextClassLoader,
+		                           reader)
 			template.make(params).writeTo(output)
 		}
 		output.flush()
