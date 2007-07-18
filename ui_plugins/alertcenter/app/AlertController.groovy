71fec4f03113168f2cd57f88714e5d8141be74a5
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-07-18 23:09:48 +0000

    Add group alert support

--- ui_plugins/alertcenter/app/AlertController.groovy
+++ ui_plugins/alertcenter/app/AlertController.groovy
@@ -5,6 +5,7 @@ import org.hyperic.hq.common.YesOrNo
 import org.hyperic.hq.events.AlertSeverity
 import org.hyperic.hq.events.EventConstants
 import org.hyperic.hq.events.server.session.AlertSortField
+import org.hyperic.hq.galerts.server.session.GalertLogSortField
 import org.hyperic.hq.hqu.rendit.html.DojoUtil
 
 class AlertController 
@@ -50,16 +51,55 @@ class AlertController
         ]
     ]
     
+    private final GALERT_TABLE_SCHEMA = [
+        getData: {pageInfo -> alertHelper.findGroupAlerts(AlertSeverity.LOW, pageInfo)},
+        defaultSort: GalertLogSortField.DATE,
+        defaultSortOrder: 0,  // descending
+        rowId: {it.id},
+        styleClass: {it.fixed ? null : "alertHighlight"},
+        columns: [
+            [field:GalertLogSortField.DATE, 
+             label:{df.format(it.timestamp)}],
+            [field:GalertLogSortField.DEFINITION,
+             label:{linkTo(it.alertDef.name, [resource:it]) }],
+            [field:GalertLogSortField.GROUP,
+             label:{linkTo(it.alertDef.group.name,
+                    [resource:it.alertDef.group])}],
+            [field:GalertLogSortField.FIXED,
+             label:{YesOrNo.valueFor(it.fixed).value.capitalize()}],
+            [field:GalertLogSortField.ACKED_BY,
+             label:{
+                 def by = it.ackedBy
+                 by == null ? "" : by.fullName }],
+            [field:GalertLogSortField.SEVERITY,
+             label:{
+                 def s = it.alertDef.severity
+                 def imgUrl = urlFor(asset:'images') + 
+                 "/${SEVERITY_MAP[s]}-severity.gif"
+                 """<img src="${imgUrl}" width="16" height="16" border="0" 
+                         class="severityIcon">""" +
+                     it.alertDef.severity.value
+              }
+             ],
+         ]
+    ]
+    
     def AlertController() {
         setTemplate('standard')  // in views/templates/standard.gsp 
     }
     
     def index = { params ->
-    	render(locals:[alertSchema:TABLE_SCHEMA])
+    	render(locals:[alertSchema:TABLE_SCHEMA, 
+    	               galertSchema:GALERT_TABLE_SCHEMA])
     }
     
     def data(params) {
         def json = DojoUtil.processTableRequest(TABLE_SCHEMA, params)
+		render(inline:"/* ${json} */", contentType:'text/json-comment-filtered')
+    }
+    
+    def groupData(params) {
+        def json = DojoUtil.processTableRequest(GALERT_TABLE_SCHEMA, params)
 		render(inline:"/* ${json} */", contentType:'text/json-comment-filtered')
     }
 }
--- ui_plugins/alertcenter/views/alert/index.gsp
+++ ui_plugins/alertcenter/views/alert/index.gsp
@@ -1,4 +1,28 @@
-<div id="alert">
-  <%= dojoTable(id:'content', url:urlFor(action:'data'),
+<script type="text/javascript">
+function swapTables(sel) {
+    if (sel=="0")  {
+        dojo.html.show('alertsTable');
+        dojo.html.hide("groupalertsTable")
+    }
+    if (sel=="1")  {
+        dojo.html.hide('alertsTable');
+        dojo.html.show("groupalertsTable")
+    }
+}
+</script>
+
+<div class="BlockTitle"> ${l.SelectTypeMsg}:
+  <select onchange=swapTables(options[selectedIndex].value)>
+    <option value='0' selected="yes">${l.ClassicAlerts}</option>
+    <option value='1'>${l.GroupAlerts}</option>
+  </select>
+</div>
+
+<div id="alertsTable">
+  <%= dojoTable(id:'Alerts', url:urlFor(action:'data'),
                 schema:alertSchema, numRows:15) %>
-</div> 
+</div>
+<div id="groupalertsTable" style="display:none;">
+  <%= dojoTable(id:'GroupAlerts', url:urlFor(action:'groupData'),
+                schema:galertSchema, numRows:15) %>
+</div>                
