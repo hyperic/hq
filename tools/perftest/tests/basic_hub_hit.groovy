546a568721085bcab8c704719009143fb4c92885
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-08-09 00:00:29 +0000

    Profile individual hub pages as well as concurrency

--- tools/perftest/tests/basic_hub_hit.groovy
+++ tools/perftest/tests/basic_hub_hit.groovy
@@ -1,11 +1,32 @@
 import org.hyperic.hq.perftest.WebTask
 
-def basicTask = new WebTask('My basic task') { hq ->
-    hq.jumpTo('hub', platforms : [page_size : 30])
-    hq.jumpTo('hub', platforms : [page_size : 'unlimited'])
+def getAllPlatformsTask = new WebTask('Get all platforms') { hq ->
+    hq.jumpTo('hub', [platforms : [page_size : 'unlimited']])
 }
 
-execute('Simple Test Run', [
-    [ task : basicTask, max_threads : 3, num_times : 5 ],                            
+def getAllServersTask = new WebTask('Get all servers') { hq ->
+    hq.jumpTo('hub', [servers : [page_size : 'unlimited']])
+}
+
+def getAllServicesTask = new WebTask('Get all services') { hq ->
+    hq.jumpTo('hub', [services : [page_size : 'unlimited']])
+}
+
+execute('Pound Platforms Hub', [
+    [ task : getAllPlatformsTask, max_threads : 1, num_times : 20 ]
+])
+
+execute('Pound Servers Hub', [
+    [ task : getAllServersTask, max_threads : 1, num_times : 20 ]
+])
+
+execute('Pound Services Hub', [
+    [ task : getAllServicesTask, max_threads : 1, num_times : 20 ]
+])
+
+execute('Low Concurrency Hub Hit', [
+    [ task : getAllPlatformsTask, max_threads : 3, num_times : 5 ],                            
+    [ task : getAllServersTask,   max_threads : 3, num_times : 5 ],                            
+    [ task : getAllServicesTask,  max_threads : 3, num_times : 5 ],                            
 ])
 
