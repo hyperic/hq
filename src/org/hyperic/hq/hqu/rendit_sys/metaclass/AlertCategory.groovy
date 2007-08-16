e2f11fef903f38d94c9ec2e592402e8009b434b9
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-08-16 00:48:45 +0000

    Add getResourceType

--- src/org/hyperic/hq/hqu/rendit_sys/metaclass/AlertCategory.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/metaclass/AlertCategory.groovy
@@ -1,5 +1,10 @@
 package org.hyperic.hq.hqu.rendit.metaclass
 
+import org.hyperic.hq.appdef.shared.AppdefEntityConstants
+import org.hyperic.hq.appdef.server.session.AppdefResourceType
+import org.hyperic.hq.appdef.server.session.PlatformManagerEJBImpl
+import org.hyperic.hq.appdef.server.session.ServerManagerEJBImpl
+import org.hyperic.hq.appdef.server.session.ServiceManagerEJBImpl
 import org.hyperic.hq.authz.server.session.AuthzSubject
 import org.hyperic.hq.authz.server.session.AuthzSubjectManagerEJBImpl
 import org.hyperic.hq.events.server.session.Alert
@@ -17,6 +22,22 @@ class AlertCategory {
         "/alerts/Alerts.do?mode=viewAlert&eid=${d.appdefID}&a=${a.id}"
     }
 
+    /**
+     * @deprecated  This method should be deprecated, since we don't want
+     *              people to rely on Appdef type entities instead of Resources
+     */
+    static AppdefResourceType getResourceType(AlertDefinition d) {
+        if (d.appdefType == AppdefEntityConstants.APPDEF_TYPE_PLATFORM) {
+            return PlatformManagerEJBImpl.one.findPlatformTypeById(d.appdefId)
+        } else if (d.appdefType == AppdefEntityConstants.APPDEF_TYPE_SERVER) {
+            return ServerManagerEJBImpl.one.findServerType(d.appdefId)
+        } else if (d.appdefType == AppdefEntityConstants.APPDEF_TYPE_SERVICE) {
+            return ServiceManagerEJBImpl.one.findServiceType(d.appdefId)
+        } else {
+            throw new RuntimeException("Unhandled appdef type: ${d.appdefType}")
+        }
+    }
+    
     static String urlFor(AlertDefinition d, String context) {
         "/alerts/Config.do?mode=viewDefinition&eid=${d.appdefEntityId}&ad=${d.id}"        
     }
