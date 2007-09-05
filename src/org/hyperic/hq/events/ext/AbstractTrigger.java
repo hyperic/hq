429e2f49044a3330552ba2c42422649b5ed49843
tkeeney <tkeeney@572de364-d518-0410-a359-afb9c36ce0ec>
2007-09-05 18:47:29 +0000

    [HHQ-915] If an escalation is in progress, then we do not want to execute the classic alerts.

--- src/org/hyperic/hq/events/ext/AbstractTrigger.java
+++ src/org/hyperic/hq/events/ext/AbstractTrigger.java
@@ -185,12 +185,10 @@ public abstract class AbstractTrigger implements TriggerInterface {
                 new ClassicEscalatableCreator(alertDef, event);
             
             // Now start escalation
-            boolean started = EscalationManagerEJBImpl.getOne()
-                                        .startEscalation(alertDef, creator);
-            
-            // If there is no escalation or it wasn't started, then execute 
-            // the classic escalations.
-            if (!started) {
+            if (alertDef.getEscalation() != null) {
+                EscalationManagerEJBImpl.getOne().startEscalation(alertDef,
+                                                                  creator); 
+            } else {
                 creator.createEscalatable();
             }
 
