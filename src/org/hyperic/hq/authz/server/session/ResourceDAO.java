5ef1a8ff38823cf124a8693127804a77b80b6fd4
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2007-09-06 20:26:50 +0000

    [HQ-910] Flush session to ensure that the resource is removed from the mapping table first before removing from EAM_RESOURCE [merge from HEAD]

--- src/org/hyperic/hq/authz/server/session/ResourceDAO.java
+++ src/org/hyperic/hq/authz/server/session/ResourceDAO.java
@@ -95,6 +95,8 @@ public class ResourceDAO
             rg.removeResource(entity);
         }
         entity.getResourceGroups().clear();
+        
+        getSession().flush();
         super.remove(entity);
     }
 
