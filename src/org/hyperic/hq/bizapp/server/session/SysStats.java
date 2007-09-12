73591ea51e9f58e9e842489f87f08ffeab27a2b9
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-09-12 01:03:46 +0000

    Include HQ DB statistics

--- src/org/hyperic/hq/bizapp/server/session/SysStats.java
+++ src/org/hyperic/hq/bizapp/server/session/SysStats.java
@@ -25,16 +25,37 @@
 
 package org.hyperic.hq.bizapp.server.session;
 
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hyperic.hibernate.Util;
 import org.hyperic.sigar.CpuInfo;
 import org.hyperic.sigar.Sigar;
 
 class SysStats {
     private static final Log _log = LogFactory.getLog(SysStats.class);
 
+    static Properties getDBStats() {
+        Properties props = new Properties();
+        DatabaseMetaData md;
+        
+        try {
+            md = Util.getConnection().getMetaData();
+            props.setProperty("hq.db.product.name", md.getDatabaseProductName());
+            props.setProperty("hq.db.product.ver", md.getDatabaseProductVersion());
+            props.setProperty("hq.db.driver.name", md.getDriverName());
+            props.setProperty("hq.db.driver.ver", md.getDriverVersion());
+        } catch(SQLException e) {
+            _log.warn("Error get db stats");
+            return props;
+        }
+        
+        return props;
+    }
+    
     static Properties getCpuMemStats() {
         Sigar sigar = null;
         Properties props = new Properties();
