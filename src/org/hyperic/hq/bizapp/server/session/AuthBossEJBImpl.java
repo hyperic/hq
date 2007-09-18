44f7d1494929b0a8ca8b719034004df68faa4df0
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-09-18 23:52:44 +0000

    Remove more unused code.

--- src/org/hyperic/hq/bizapp/server/session/AuthBossEJBImpl.java
+++ src/org/hyperic/hq/bizapp/server/session/AuthBossEJBImpl.java
@@ -69,7 +69,7 @@ public class AuthBossEJBImpl extends BizappSessionEJB implements SessionBean {
                ConfigPropertyException 
     {
         try {
-            return getAuthManager().login(username, password);
+            return getAuthManager().getSessionId(username, password);
         } catch (AccessLocalException e) {
             throw new LoginException(e.getMessage());
         }
