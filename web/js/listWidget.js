9f36bf5036eef7bb6f874ed14566062f1a54b83c
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2007-10-06 05:45:38 +0000

    [HHQ-605] Change wording to remove or disable

--- web/js/listWidget.js
+++ web/js/listWidget.js
@@ -51,7 +51,7 @@ function ReplaceButton(divId, tdId, tdState, imageId, btnFunction) {
 		newInput.setAttribute("src", imgPath);
 		newInput.setAttribute("name", inputName);
         if (btnFunction == 'delete' || btnFunction == 'remove') {
-            newInput.setAttribute("onclick", "return confirm('Are you sure you want to delete selections?');");
+            newInput.setAttribute("onclick", "return confirm('Are you sure you want to remove or disable selections?');");
         }
 		newDiv.appendChild(newInput);
 	}
