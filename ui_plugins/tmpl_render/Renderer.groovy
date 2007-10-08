2e4cbfe0c591d242c18a54e59a66939431948fbc
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-10-08 21:41:13 +0000

    HQ-975.  Fix to work with new groovy APIs

--- ui_plugins/tmpl_render/Renderer.groovy
+++ ui_plugins/tmpl_render/Renderer.groovy
@@ -8,10 +8,9 @@ import groovy.text.SimpleTemplateEngine
 class Renderer {
     def render(File gspFile, Map params, Writer output) {
         gspFile.withReader { reader ->
-			def eng = new SimpleTemplateEngine()
-		    def template = 
-		        eng.createTemplate(Thread.currentThread().contextClassLoader,
-		                           reader)
+			def eng = new SimpleTemplateEngine(Thread.currentThread().contextClassLoader)
+		    def template = eng.createTemplate(reader) 
+	
 			template.make(params).writeTo(output)
 		}
 		output.flush()
