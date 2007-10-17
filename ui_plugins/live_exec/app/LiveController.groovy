b131b56d6a5b4367af939cdcfad5cd969d8aba18
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-10-17 01:24:42 +0000

    Add first-cut at some liveData-fu

--- ui_plugins/live_exec/app/LiveController.groovy
+++ ui_plugins/live_exec/app/LiveController.groovy
@@ -1,5 +1,11 @@
 import org.hyperic.hq.hqu.rendit.BaseController
 
+import org.hyperic.hq.hqu.rendit.html.HtmlUtil
+import org.json.JSONObject
+import org.hyperic.hq.livedata.shared.LiveDataCommand
+import org.hyperic.util.config.ConfigResponse
+import org.hyperic.hq.livedata.server.session.LiveDataManagerEJBImpl
+
 class LiveController 
 	extends BaseController
 {
@@ -8,6 +14,20 @@ class LiveController
     }
     
     def index(params) {
-    	render(locals:[ platforms : resourceHelper.findAllPlatforms() ])
+        def cmds = viewedResource.getLiveDataCommands(user).sort()
+        cmds.add(0, '---')
+    	render(locals:[ commands:cmds, eid:viewedResource.entityID ])
+    }
+    
+    def invoke(params) {
+        log.info "Invoking!  Yay!"
+        def cmd = new LiveDataCommand(viewedResource.entityID, 
+                                      params.getOne('cmd'), 
+                                      new ConfigResponse())
+        def res = LiveDataManagerEJBImpl.one.getData(user, cmd).objectResult
+        log.info "Res is ${res}"
+        
+        def json = [result: HtmlUtil.escapeHtml(res)] as JSONObject
+		render(inline:"/* ${json} */", contentType:'text/json-comment-filtered')
     }
 }
--- ui_plugins/live_exec/views/live/index.gsp
+++ ui_plugins/live_exec/views/live/index.gsp
@@ -1,7 +1,45 @@
-<p>
-  <select>
-  <% for (p in platforms) { %>
-    <option value="${p.id}">${h p.name}</option>
+<script type="text/javascript">
+function selectCommand(cmd) {
+  if (cmd == '---') {
+    dojo.html.hide('goButtonCont');
+  } else {  
+    dojo.html.show('goButtonCont');
+  }
+}
+
+function go() {
+  var cmdSelect = dojo.byId('commandSelect');
+  var cmd = cmdSelect.options[cmdSelect.selectedIndex].value;
+  var url = '<%= urlFor(action:'invoke') %>' + '?cmd=' + cmd;
+  url = url + '&eid=<%= eid %>'
+  dojo.io.bind({
+    url: url,
+    method: "get",
+    mimetype: "text/json-comment-filtered",
+    load: function(type, data, evt) {
+      dojo.byId('result').innerHTML = data.result;
+    },
+    error: function(err) {
+      alert('error!');
+    },
+  });
+}
+
+</script>
+
+<div>
+  Execute Command
+  <select id="commandSelect" 
+          onchange="selectCommand(options[selectedIndex].value)">
+  <% for (c in commands) { %>
+    <option value="${c}">${h c}</option>
   <% } %>
   </select>
-</p>
+</div>
+
+<div id="goButtonCont">
+  <button onclick="go()">Go!</button>
+</div>
+
+<div id="result">
+</div>
