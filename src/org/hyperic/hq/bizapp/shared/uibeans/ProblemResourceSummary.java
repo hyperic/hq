e2eaa92336aa2968daa453a32fa4a29559701f34
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2007-10-18 17:17:25 +0000

    Use AppdefEntityValue objects rather than full filled AppdefResourceValue objects

--- src/org/hyperic/hq/bizapp/shared/uibeans/ProblemResourceSummary.java
+++ src/org/hyperic/hq/bizapp/shared/uibeans/ProblemResourceSummary.java
@@ -28,82 +28,84 @@ package org.hyperic.hq.bizapp.shared.uibeans;
 import java.io.Serializable;
 
 import org.hyperic.hq.appdef.shared.AppdefEntityID;
-import org.hyperic.hq.appdef.shared.AppdefResourceValue;
+import org.hyperic.hq.appdef.shared.AppdefEntityNotFoundException;
+import org.hyperic.hq.appdef.shared.AppdefEntityValue;
+import org.hyperic.hq.authz.shared.PermissionException;
 import org.hyperic.hq.measurement.ext.ProblemResourceInfo;
 
 /**
  * 
- * This bean is for end user representions of problem resources
+ * This bean is for end user representations of problem resources
  *
  */
 public class ProblemResourceSummary implements Serializable {
-    private String entityName = null;
-    private ProblemResourceInfo prInfo = null;
-    private double availability = Double.NaN;
+    private String _entityName = null;
+    private ProblemResourceInfo _prInfo = null;
+    private double _availability = Double.NaN;
     
-    public ProblemResourceSummary(AppdefResourceValue entity,
-                                  ProblemResourceInfo problem) {
-        if (! entity.getEntityId().equals(problem.getEntityId())){
+    public ProblemResourceSummary(AppdefEntityValue entity,
+                                  ProblemResourceInfo problem)
+        throws AppdefEntityNotFoundException, PermissionException {
+        if (! entity.getID().equals(problem.getEntityId())){
             throw new IllegalArgumentException("resource id " +
-                entity.getEntityId() + " is not the one with problems, " +
+                entity.getID() + " is not the one with problems, " +
                 problem.getEntityId());
         }
         
-        this.entityName = entity.getName();
-        this.prInfo = problem;
+        _entityName = entity.getName();
+        _prInfo = problem;
     }
 
     public Integer getAlertCount() {
-        if (prInfo.isAlertCountSet())
-            return new Integer(prInfo.getAlertCount());
+        if (_prInfo.isAlertCountSet())
+            return new Integer(_prInfo.getAlertCount());
         
         return new Integer(0);
     }
 
     public Integer getOobCount() {
-        if (prInfo.isOobCountSet())
-            return new Integer(prInfo.getOobCount());
+        if (_prInfo.isOobCountSet())
+            return new Integer(_prInfo.getOobCount());
         
         return new Integer(0);
     }
 
     public Long getEarliest() {
-        return new Long(prInfo.getEarliest());
+        return new Long(_prInfo.getEarliest());
     }
 
     public Long getLatest() {
-        return new Long(prInfo.getLatest());
+        return new Long(_prInfo.getLatest());
     }
 
     public AppdefEntityID getEntityId() {
-        return prInfo.getEntityId();
+        return _prInfo.getEntityId();
     }
 
     public String getEntityName() {
-        return entityName;
+        return _entityName;
     }
 
     public int getResourceId() {
-        return prInfo.getEntityId().getID();
+        return _prInfo.getEntityId().getID();
     }
 
     public int getResourceType() {
-        return prInfo.getEntityId().getType();
+        return _prInfo.getEntityId().getType();
     }
 
     public double getAvailability() {
-        return availability;
+        return _availability;
     }
     
     public void setAvailability(double availability) {
-        this.availability = availability;
+        _availability = availability;
     }
     
     public void addProblemInfo(ProblemResourceInfo info) {
-        this.prInfo.setAlertCount(this.prInfo.getAlertCount() +
-                                  info.getAlertCount());
-        this.prInfo.setOobCount(this.prInfo.getOobCount() + info.getOobCount());
-        this.prInfo.setEarliest(Math.min(this.prInfo.getEarliest(),
-                                         info.getEarliest()));
+        _prInfo.setAlertCount(_prInfo.getAlertCount() + info.getAlertCount());
+        _prInfo.setOobCount(_prInfo.getOobCount() + info.getOobCount());
+        _prInfo.setEarliest(Math.min(_prInfo.getEarliest(),
+                                     info.getEarliest()));
     }
 }
