e2c91e2b2618cea6ccfbecb0534c2796ae78e810
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-10-22 20:42:19 +0000

    Don't add audit center to masthead

--- ui_plugins/auditcenter/init.groovy
+++ ui_plugins/auditcenter/init.groovy
@@ -6,4 +6,4 @@ plugin.apiMinor = 1
 
 // Enable if you want to dump the translated scripts to stdout
 plugin.dumpScripts = false
-plugin.addMastheadView('/audit/index.hqu', 'Audit Center', 'tracker')
+// plugin.addMastheadView('/audit/index.hqu', 'Audit Center', 'tracker')
