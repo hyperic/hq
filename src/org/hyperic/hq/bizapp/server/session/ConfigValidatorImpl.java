eae3c5681432adf7e412e69ef73f3c7bcec7fb8f
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2007-10-24 18:23:34 +0000

    RawMeasurementManager API change to take AuthzSubject

--- src/org/hyperic/hq/bizapp/server/session/ConfigValidatorImpl.java
+++ src/org/hyperic/hq/bizapp/server/session/ConfigValidatorImpl.java
@@ -6,7 +6,7 @@
  * normal use of the program, and does *not* fall under the heading of
  * "derived work".
  * 
- * Copyright (C) [2004, 2005, 2006], Hyperic, Inc.
+ * Copyright (C) [2004-2007], Hyperic, Inc.
  * This file is part of HQ.
  * 
  * HQ is free software; you can redistribute it and/or modify
@@ -92,7 +92,7 @@ public class ConfigValidatorImpl
                                                  ProductPlugin.TYPE_MEASUREMENT,
                                                  ids[i], true);
                 
-                rmMan.checkConfiguration(subj, ids[i], responses[i]);
+                rmMan.checkConfiguration(subject, ids[i], responses[i]);
             } catch(ConfigFetchException exc){
                 responses[i] = null;
             } 
