20e4bc098e283f98af73f4db87087ee3ce726899
tkeeney <tkeeney@572de364-d518-0410-a359-afb9c36ce0ec>
2007-11-12 18:14:17 +0000

    Cleaned up imports.

--- src/org/hyperic/hq/events/server/session/AlertDefinitionManagerEJBImpl.java
+++ src/org/hyperic/hq/events/server/session/AlertDefinitionManagerEJBImpl.java
@@ -26,7 +26,6 @@
 package org.hyperic.hq.events.server.session;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
