838b3d40d5d13b5b3ec56a9697680c7f6a5c7d3c
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2007-11-14 23:56:12 +0000

    Fix up aiApprove script to process the queue in smaller batches to avoid needing to sync the entire queue before processing the first platform.  Also add a session flush after each approval.

--- etc/gconsoleTemplates/aiApprove.groovy
+++ etc/gconsoleTemplates/aiApprove.groovy
@@ -10,34 +10,47 @@ import org.hyperic.hq.authz.shared.AuthzSubjectManagerLocal
 import org.hyperic.hq.authz.shared.AuthzSubjectValue
 import org.hyperic.util.pager.PageControl
 import org.hyperic.util.pager.PageList
+import org.hyperic.hibernate.Util
 
 AuthzSubjectManagerLocal subMan = AuthzSubjectManagerEJBImpl.one
 AIQueueManagerLocal aiMan = AIQueueManagerEJBImpl.one
 
 AuthzSubjectValue overlord = subMan.overlord
 
-PageList list = aiMan.getQueue(overlord, true, true, PageControl.PAGE_ALL)
+int totalProcessed = 0;
+PageControl chunk = new PageControl(0, 5);
 
-list.each { plat ->
-    List platformIds = new ArrayList()
-    List serverIds = new ArrayList()
-    List ipIds = new ArrayList()
+while (true) {
 
-    platformIds.add(plat.id)
-    AIServerValue[] servers = plat.AIServerValues
-    servers.each { server ->
-        serverIds.add(server.id)
+    PageList list = aiMan.getQueue(overlord, true, true, chunk)
+
+    if (list.size() == 0)
+      break;
+
+    list.each { plat ->
+        List platformIds = new ArrayList()
+        List serverIds = new ArrayList()
+        List ipIds = new ArrayList()
+
+        platformIds.add(plat.id)
+        AIServerValue[] servers = plat.AIServerValues
+        servers.each { server ->
+            serverIds.add(server.id)
+        }
+
+        AIIpValue[] ips = plat.AIIpValues
+        ips.each { ip ->
+            ipIds.add(ip.id)
+        }
+
+        System.out.println("Processing platform " + plat.id)
+        aiMan.processQueue(overlord, platformIds, serverIds,
+                           ipIds, AIQueueConstants.Q_DECISION_APPROVE)
+        totalProcessed++;
+        System.out.println("Done")
+        Util.flushCurrentSession()
     }
-    
-    AIIpValue[] ips = plat.AIIpValues
-    ips.each { ip ->
-        ipIds.add(ip.id)  
-    }
 
-    System.out.println("Processing platform " + plat.id)
-    aiMan.processQueue(overlord, platformIds, serverIds, 
-                       ipIds, AIQueueConstants.Q_DECISION_APPROVE)
-    System.out.println("Done")
 }
 
-return "Processed " + list.size() + " platforms"
+return "Processed " + totalProcessed + " platforms"
