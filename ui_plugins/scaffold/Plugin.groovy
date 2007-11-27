0084950f9fc1cd2794fd4d5a2ed045dec70dfd45
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-11-27 17:54:08 +0000

    Add comments, descriptions about attach points

--- ui_plugins/scaffold/Plugin.groovy
+++ ui_plugins/scaffold/Plugin.groovy
@@ -2,5 +2,20 @@ import org.hyperic.hq.hqu.rendit.HQUPlugin
 
 class Plugin extends HQUPlugin {
     Plugin() {
+        /*
+         * Uncomment the following to add a link to the plugin in the
+         * Administration section.class
+         *
+         * The params are:
+         *   autoAttach:  Attach this view automatically
+         *   path:        The internal request to make to render the view
+         *                (translates to http://localhost:7080/hqu/console/index.hqu)
+         *                This should be a full request to a controller in /app
+         *                ex:  If /app/BankController.groovy had a method 'debit'
+         *                    '/bank/debit.hqu'
+         *    
+         *   name:        A description of the view, only used in the database.
+         */
+        // addAdminView(true, '/console/index.hqu', 'Groovy Console')
     }
 }
