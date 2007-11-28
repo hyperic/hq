e26b77282b2d111c7e04f6102da32458a9855890
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2007-11-28 02:10:24 +0000

    Put the # of resources down next to the menu item

--- ui_plugins/systemsdown/Plugin.groovy
+++ ui_plugins/systemsdown/Plugin.groovy
@@ -1,7 +1,23 @@
+import org.hyperic.hq.measurement.server.session.DerivedMeasurementManagerEJBImpl as DMM
+import org.hyperic.hq.authz.server.session.AuthzSubject
+import org.hyperic.hq.authz.server.session.Resource
+import org.hyperic.hq.hqu.server.session.Attachment
 import org.hyperic.hq.hqu.rendit.HQUPlugin
+import org.hyperic.hq.hqu.AttachmentDescriptor
+import org.hyperic.hq.hqu.SimpleAttachmentDescriptor
 
 class Plugin extends HQUPlugin {
     Plugin() {
         addMastheadView(true, '/systemsdown/index.hqu', 'Currently Down Resources', 'resource')    
     }
+    
+    AttachmentDescriptor getAttachmentDescriptor(Attachment a, Resource r,
+                                                 AuthzSubject u) 
+    { 
+        def l = DMM.one.numUnavailEntities
+        new SimpleAttachmentDescriptor(a, 
+                                       descriptor.getProperty('plugin.helpTag'), 
+                                       "${description}  ($l)")
+    }
+    
 }
