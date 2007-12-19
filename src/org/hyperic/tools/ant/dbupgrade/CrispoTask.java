f7ee2775545bccd8c1c6f498044e60eff6d6d5a9
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2007-12-19 06:30:05 +0000

    [HHQ-1366] Fix migration of configurations > 4K characters

--- src/org/hyperic/tools/ant/dbupgrade/CrispoTask.java
+++ src/org/hyperic/tools/ant/dbupgrade/CrispoTask.java
@@ -15,10 +15,13 @@ public class CrispoTask extends SchemaSpecTask {
 
     private static final String CRISPO_TABLE      = "EAM_CRISPO";
     private static final String CRISPO_OPT_TABLE  = "EAM_CRISPO_OPT";
+    private static final String CRISPO_ARR_TABLE  = "EAM_CRISPO_ARRAY";
     
     private static final String CRISPO_ID_SEQ     = "EAM_CRISPO_ID_SEQ";
     private static final String CRISPO_OPT_ID_SEQ = "EAM_CRISPO_OPT_ID_SEQ";
     
+    private static final String ARRAY_DELIMITER = "|";
+
     public int createCrispo(Dialect d, ConfigResponse cr)
         throws SQLException
     {
@@ -59,21 +62,51 @@ public class CrispoTask extends SchemaSpecTask {
         Statement stmt = null;
         ResultSet rs = null;
         int id;
+        String sql;
 
         try {
-            String sql = d.getSequenceNextValString(CRISPO_OPT_ID_SEQ);
+            sql = d.getSequenceNextValString(CRISPO_OPT_ID_SEQ);
             stmt = getConnection().createStatement();
             rs = stmt.executeQuery(sql);
             rs.next();
             id = rs.getInt(1);
 
-            sql = "insert into " + CRISPO_OPT_TABLE
-                    + " (id, version_col, propkey, val, "
-                    + "crispo_id) VALUES (" + id + ", 1, '" + key + "', "
-                    + "'" + val + "', " + crispoId + ")";
+            DBUtil.closeResultSet(LOGCTX, rs);  // Done with the result set
 
-            System.out.println("executed query: " + sql);
+            // See if the value is too long
+            String[] elem = null;
+            if (val.length() > 4000) {
+                elem = val.split("\\" + ARRAY_DELIMITER);
+            }
+            
+            if (elem != null && elem.length > 1) {
+                sql = "insert into " + CRISPO_OPT_TABLE
+                        + " (id, version_col, propkey, crispo_id) "
+                        + "VALUES (" + id + ", 1, '" + key + "', "
+                        + crispoId + ")";
+            } else {
+                if (elem != null)                    // No split, narrow string
+                    val = val.substring(0, 3999);
+
+                elem = null;
+                sql = "insert into " + CRISPO_OPT_TABLE
+                        + " (id, version_col, propkey, val, crispo_id) "
+                        + "VALUES (" + id + ", 1, '" + key + "', '" + val
+                        + "', " + crispoId + ")";
+            }
+
+            log("executing query: " + sql);
             stmt.executeUpdate(sql);
+            
+            if (elem != null) {
+                for (int i = 0; i < elem.length; i++) {
+                    sql = "insert into " + CRISPO_ARR_TABLE
+                            + " (opt_id, val, idx) VALUES (" + id + ", '"
+                            + elem[i] + "', " + i + ")";
+                    log("executing query: " + sql);
+                    stmt.executeUpdate(sql);
+                }
+            }
         } finally {
             DBUtil.closeJDBCObjects(LOGCTX, null, stmt, rs);
         }
