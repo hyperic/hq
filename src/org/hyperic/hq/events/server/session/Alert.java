7ee5a9c75e12b08ddc38ffcd6ff438514bd88207
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2007-12-19 06:43:06 +0000

    [HQ-1036] Simplify the formula-based fields to one, instead of two

--- src/org/hyperic/hq/events/server/session/Alert.java
+++ src/org/hyperic/hq/events/server/session/Alert.java
@@ -34,6 +34,7 @@ import java.util.List;
 import org.hyperic.dao.DAOFactory;
 import org.hyperic.hibernate.PersistedObject;
 import org.hyperic.hq.authz.server.session.AuthzSubject;
+import org.hyperic.hq.escalation.server.session.EscalationState;
 import org.hyperic.hq.escalation.server.session.PerformsEscalations;
 import org.hyperic.hq.events.AlertDefinitionInterface;
 import org.hyperic.hq.events.AlertInterface;
@@ -50,9 +51,8 @@ public class Alert
     private AlertDefinition _alertDefinition;
     private Collection      _actionLog    = new ArrayList();
     private Collection      _conditionLog = new ArrayList();
-    private Long            _stateId;
-    private Long            _ackedBy;
     private AlertValue      _alertVal;
+    private Boolean         _ackable;
 
     protected Alert() {
     }
@@ -161,26 +161,18 @@ public class Alert
         _conditionLog.remove(acl);
     }
     
-    protected void setAckedBy(Long ackedBy) {
-        _ackedBy = ackedBy;
-    }
-    
-    protected Long getAckedBy() { 
-        return _ackedBy;
-    }
-    
-    protected void setStateId(Long stateId) {
-        _stateId = stateId;
-    }
-    
-    protected Long getStateId() {
-        return _stateId;
+    protected void setAckable(Boolean ackable) {
+        _ackable = ackable;
     }
     
     public boolean isAckable() {
-        return getStateId() != null && getAckedBy() == null;
+        return _ackable != null && _ackable.booleanValue();
     }
     
+    protected void invalidate() {
+        set_version_(new Long(get_version_() + 1));     // Invalidate caches
+    }
+
     public AlertValue getAlertValue() {
         if (_alertVal == null) 
             _alertVal = new AlertValue();
