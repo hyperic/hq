9f08fd4a8ed6cde4e236c5ce16ae64cd78675ec7
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2008-01-10 18:08:14 +0000

    Update to work with newer HQU code

--- ui_plugins/plugin_mon/app/ManageController.groovy
+++ ui_plugins/plugin_mon/app/ManageController.groovy
@@ -1,46 +1,75 @@
+import org.hyperic.hq.hqu.rendit.html.DojoUtil
 import org.hyperic.hq.hqu.rendit.BaseController
 import org.hyperic.hq.hqu.server.session.UIPluginManagerEJBImpl
 
 class ManageController 
 	extends BaseController
 {
+    private pMan = UIPluginManagerEJBImpl.one
+    
     def ManageController() {
-        setTemplate('standard')  // in views/templates/standard.gsp 
-        addBeforeFilter({ 
-            if (!user.isSuperUser()) {
-                render(inline: "Unauthorized")
-                return true
-            }
-            return false
-        })
+        onlyAllowSuperUsers()
+        setTemplate('standard')  // in views/templates/standard.gsp
+        setJSONMethods(['pluginData'])
     }
     
-    private def getPMan() {
-        UIPluginManagerEJBImpl.one
+    def index(params) {
+    	render(locals:[pluginSchema: pluginSchema])
     }
     
-    def index = { params ->
-    	render(locals:[plugins : pMan.findAll()])
+    def pluginData(params) {
+        DojoUtil.processTableRequest(pluginSchema, params)
     }
     
-    def deletePlugin = { params ->
+    private getPluginSchema() {
+        [getData: {pageInfo, params ->
+            pMan.findAll()
+        },
+        rowId: {it -> 3},
+        defaultSort: null,
+        defaultSortOrder: 0,  // descending
+        styleClass: {},
+        columns: [
+            [field: [getValue: {localeBundle.Name},
+                     description:'name', sortable:false], 
+             width: '330%',
+             label: {it.name}],
+            [field: [getValue: {localeBundle.Version},
+                     description:'version', sortable:false], 
+             width: '330%',
+             label: {it.name}],
+             /*
+            [field: [getValue: {},
+                     description:'name', 
+                     sortable:false], 
+             width: '340%',
+             label: {
+                buttonTo text:'Delete', action:'deletePlugin',
+                         id:it, htmlId:it.id, 
+                         confirm:'Are you sure?'
+            }]
+            */
+        ]]
+    }
+    
+    def deletePlugin(params) {
 	    def plugin = pMan.findPluginById(new Integer(params.getOne('id')))
 	    pMan.deletePlugin(plugin)
     	redirectTo(action : 'index')
     }
     
-    def showPlugin = { params ->
+    def showPlugin(params) {
         def plugin = pMan.findPluginById(new Integer(params.getOne('id')))
         render(locals:[plugin : plugin])
     }
     
-    def attach = { params ->
+    def attach(params) {
 		def view  = pMan.findViewById(new Integer(params.getOne('id')))
 		pMan.attachView(view, view.prototype)
 		redirectTo(action : 'showPlugin', id : view.plugin)
     }
 
-    def detach = { params ->
+    def detach(params) {
 	    def attachment = 
 	        pMan.findAttachmentById(new Integer(params.getOne('id')))
 	    pMan.detach(attachment)
--- ui_plugins/plugin_mon/views/manage/index.gsp
+++ ui_plugins/plugin_mon/views/manage/index.gsp
@@ -1,21 +1,6 @@
 <h3>UI Plugin Management</h3>
-<table>
-<thead>
-  <tr>
-    <th>${l.Name}</th>
-    <th>${l.Version}</th>
-    <th>${l.Description}</th>
-  </tr>
-</thead>
-<tbody>
-<% for (p in plugins) { %>
-  <tr>
-    <td><%= linkTo p.name, [action:'showPlugin', id:p] %></td>    
-    <td><%= h p.pluginVersion %></td>    
-    <td><%= h p.description %></td>
-    <td><%= buttonTo 'Delete', [action:'deletePlugin', id:p,
-                                confirm:'Are you sure?'] %></td>
-  </tr>
-<% } %>
-</tbody>
-</table>
+
+
+<%= dojoTable(id:'pluginTable', title:l.Plugins,
+              refresh:10, url:urlFor(action:'pluginData'),
+              schema:pluginSchema, numRows:500, pageControls:false) %>
