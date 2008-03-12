107cb4bd91c14aef0634a43d25dd8a9777d86f90
tkeeney <tkeeney@572de364-d518-0410-a359-afb9c36ce0ec>
2008-03-12 23:29:41 +0000

    Added unit tests to check package deployment/undeployment.

--- src/org/hyperic/util/unittest/server/TestServerLifecycleManagement.java
+++ src/org/hyperic/util/unittest/server/TestServerLifecycleManagement.java
@@ -26,6 +26,7 @@
 package org.hyperic.util.unittest.server;
 
 
+
 /**
  * A test case tasked with testing the server lifecycle management operations 
  * provided in the super class.
@@ -35,11 +36,64 @@ public class TestServerLifecycleManagement extends BaseServerTestCase {
     public TestServerLifecycleManagement(String name) {
         super(name);
     }
+                
+    /**
+     * Test starting the jboss server without deploying the HQ application.
+     * 
+     * @throws Exception
+     */
+    public void testStartServerNoHQDeployment() throws Exception {
+        startServer();
+    }
     
+    /**
+     * Test starting the jboss server and deploying the HQ application.
+     * 
+     * @throws Exception
+     */
+    public void testStartServerDeployHQ() throws Exception {
+        startServer();
+        deployHQ(false);
+    }
     
-    public void testStartServer() throws Exception {
-        this.startServer();
-        this.stopServer();
+    /**
+     * Test starting the jboss server and deploying the HQEE application.
+     * 
+     * @throws Exception
+     */
+    public void testStartServerDeployHQEE() throws Exception {
+        startServer();
+        deployHQ(true);
+    }
+    
+    /**
+     * Test deploying the HQEE application without starting the jboss server. 
+     * The server should be started before the deployment.
+     * 
+     * @throws Exception
+     */
+    public void testDeployHQEEWithoutStartingFirst() throws Exception {
+        deployHQ(true);
+    }
+    
+    /**
+     * Test deploying then undeploying the HQ application.
+     * 
+     * @throws Exception
+     */
+    public void testDeployAndUndeployHQ() throws Exception {
+        deployHQ(false);
+        undeployHQ();
+    }
+    
+    /**
+     * Test deploying then undeploying the HQEE application.
+     * 
+     * @throws Exception
+     */
+    public void testDeployAndUndeployHQEE() throws Exception {
+        deployHQ(true);
+        undeployHQ();
     }
 
 }
