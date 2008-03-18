fe308c6b5bd7a58c2552884cbf7376292c8a8b70
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2008-03-18 01:16:12 +0000

    Allow for mass list and approval through the HQU mass plugin. [HHQ-1805] [merge from trunk]

---
+++ ui_plugins/mass/examples/autodiscoveryList.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# List platforms currently in the auto-inventory queue.
+# 
+# This script takes 1 optional fqdn argument.  This can be either an exact fqdn or a regex.
+#
+# Examples:
+#
+# List the platform in the queue with the FQDN www.hyperic.com
+# ./autoinventoryList.sh -dfqdn="www.hyperic.com"
+#
+# List all platforms
+# ./autoinventoryList.sh -dfqdn="regex:.*"
+#
+# List all platforms that end with the domain hyperic.com
+# ./autoinventoryList.sh -dfqdn="regex:.*hyperic.com" # Approve all platforms that match *.hyperic.com
+#
+
+curl http://hqadmin:hqadmin@localhost:7080/hqu/mass/autodiscovery/list.hqu $@
