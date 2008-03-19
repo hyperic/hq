81c30fbea39419a01a5c1a40b4e9e6331a707ede
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2008-03-19 03:31:15 +0000

    Add group utility methods (isCompatible(), and getCompatableType()) which allow us to easily print out the compat groups resourceType in mass group/list

--- src/org/hyperic/hq/hqu/rendit_sys/metaclass/ResourceGroupCategory.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/metaclass/ResourceGroupCategory.groovy
@@ -1,12 +1,16 @@
 package org.hyperic.hq.hqu.rendit.metaclass
 
 import org.hyperic.hq.authz.server.session.ResourceGroup
+import org.hyperic.hq.authz.server.session.Resource
+import org.hyperic.hq.authz.server.session.ResourceManagerEJBImpl as RsrcMan
 import org.hyperic.hq.authz.server.session.AuthzSubject
 import org.hyperic.hq.appdef.shared.AppdefEntityConstants
 import org.hyperic.hq.authz.server.session.ResourceGroupManagerEJBImpl as GroupMan
+import org.hyperic.hq.authz.shared.AuthzConstants
 
 class ResourceGroupCategory {
     private static groupMan = GroupMan.one
+    private static rsrcMan  = RsrcMan.one
     
     static String urlFor(ResourceGroup r, String context) {
         "/Resource.do?eid=${AppdefEntityConstants.APPDEF_TYPE_GROUP}:${r.id}"
@@ -16,5 +20,37 @@ class ResourceGroupCategory {
                              Collection resources)
     {
         groupMan.setResources(user, group, resources)
+    }
+
+    /**
+     * Returns true if the ResourceGroup is compatible (i.e. only contains
+     * resources of a homogenous type)
+     */
+    static boolean isCompatible(ResourceGroup g) {
+        g.groupType == AppdefEntityConstants.APPDEF_TYPE_GROUP_COMPAT_PS ||
+        g.groupType == AppdefEntityConstants.APPDEF_TYPE_GROUP_COMPAT_SVC
+    }
+    
+    /**
+     * Gets the Resource prototype that the group is compatible wth.
+     */
+    static Resource getCompatibleType(ResourceGroup g) {
+        if (!isCompatible(g)) {
+            throw new IllegalArgumentException("Attempted to get compatible " +
+                                               "type on non-compatible group")
+        }
+        
+        def resourceTypeId
+        if (g.groupEntType == AppdefEntityConstants.APPDEF_TYPE_PLATFORM) {
+            resourceTypeId = AuthzConstants.authzPlatformProto
+        } else if (g.groupEntType == AppdefEntityConstants.APPDEF_TYPE_SERVER) {
+            resourceTypeId = AuthzConstants.authzServerProto
+        } else if (g.groupEntType == AppdefEntityConstants.APPDEF_TYPE_SERVICE) {
+            resourceTypeId = AuthzConstants.authzServiceProto
+        } else {
+            assert "Unknown group ent type ${g.groupEntType} for compat groups"
+        }
+            
+        rsrcMan.findResourcePojoByInstanceId(resourceTypeId, g.groupEntResType)
     }
 }
