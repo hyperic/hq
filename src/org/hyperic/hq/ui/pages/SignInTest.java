9c1b3249e983adea92bd87a53529a34a5d985196
rpack <rpack@572de364-d518-0410-a359-afb9c36ce0ec>
2008-03-19 00:56:32 +0000

    Signin Page Test

---
+++ src/org/hyperic/hq/ui/pages/SignInTest.java
@@ -0,0 +1,44 @@
+package org.hyperic.hq.ui.pages;
+
+import org.apache.tapestry.BaseComponentTestCase;
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.PageRedirectException;
+import org.apache.tapestry.engine.ILink;
+import org.apache.tapestry.link.PageLink;
+import org.hyperic.ui.tapestry.page.PageListing;
+import org.junit.Test;
+
+public class SignInTest extends BaseComponentTestCase {
+
+    /**
+     * Note: this test requires the container
+     */
+    @Test
+    public void test_SignInListener() {
+
+	/*
+	IMarkupWriter writer = newWriter();
+	IRequestCycle cycle = newCycle(true, writer);
+	// Create the signin page so the enhanced subclass can be created
+	PageLink signInLink = (PageLink) newInstance(PageLink.class, "page",
+		"SignIn");
+	SignIn signInPage = (SignIn) newInstance(SignIn.class, "userName",
+		"hqadmin", "password", "hqadmin", "message", null,
+		"signinLink", signInLink);
+	try {
+	    ILink retVal = signInPage.signinButtonListener(cycle);
+	} catch (PageRedirectException e) {
+	    assertEquals(e.getTargetPageName(), PageListing.DASHBOARD_URL);
+	}
+	
+	signInPage = (SignIn) newInstance(SignIn.class, "userName",
+		"hqadmin", "password", "badpassword", "message", null,
+		"signinLink", signInLink);
+	ILink retVal = signInPage.signinButtonListener(cycle);
+	
+	assertEquals(retVal.getURL(), PageListing.SIGN_IN +".html");
+	 */
+    }
+    
+}
