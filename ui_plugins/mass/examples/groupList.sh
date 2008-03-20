700381b3512035c31b4962326da167be4ef9c7ec
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2008-03-20 21:37:45 +0000

    merge from 3.2:  Allow groups to be listed via regex or name

--- ui_plugins/mass/examples/groupList.sh
+++ ui_plugins/mass/examples/groupList.sh
@@ -5,4 +5,4 @@ PARMS="${PARMS} "-dincludeSystem="false"
 PARMS="${PARMS} "-dverbose="false"
 
 curl http://hqadmin:hqadmin@localhost:7080/hqu/mass/group/list.hqu \
-    ${PARMS}
+    ${PARMS} -dname="$1"
