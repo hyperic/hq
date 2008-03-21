b08981197be3e74f7c515baaedec4b2771aa68d5
scottmf <scottmf@572de364-d518-0410-a359-afb9c36ce0ec>
2008-03-21 19:55:21 +0000

    [HQ-1186] modified coldfusion plugin to work with 6.1.x

--- plugins/coldfusion/src/org/hyperic/hq/plugin/coldfusion/ColdfusionServerDetector.java
+++ plugins/coldfusion/src/org/hyperic/hq/plugin/coldfusion/ColdfusionServerDetector.java
@@ -6,7 +6,7 @@
  * normal use of the program, and does *not* fall under the heading of
  * "derived work".
  *
- * Copyright (C) [2004, 2005, 2006], Hyperic, Inc.
+ * Copyright (C) [2004-2008], Hyperic, Inc.
  * This file is part of HQ.
  * 
  * HQ is free software; you can redistribute it and/or modify
@@ -28,95 +28,131 @@ package org.hyperic.hq.plugin.coldfusion;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Iterator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.hyperic.hq.product.AutoServerDetector;
-import org.hyperic.hq.product.FileServerDetector;
-import org.hyperic.hq.product.MeasurementPlugin;
-import org.hyperic.hq.product.Metric;
-import org.hyperic.hq.product.MetricUnreachableException;
-import org.hyperic.hq.product.MetricInvalidException;
-import org.hyperic.hq.product.MetricNotFoundException;
-import org.hyperic.hq.product.MetricValue;
 import org.hyperic.hq.product.PluginException;
 import org.hyperic.hq.product.ServerDetector;
 import org.hyperic.hq.product.ServerResource;
-import org.hyperic.hq.product.ServiceResource;
-import org.hyperic.hq.product.ProductPlugin;
+import org.hyperic.hq.product.Win32ControlPlugin;
 
 import org.hyperic.util.config.ConfigResponse;
-import org.hyperic.util.config.ConfigSchema;
-import org.hyperic.util.config.SchemaBuilder;
 
-import org.hyperic.sigar.win32.RegistryKey;
+import org.hyperic.sigar.win32.Service;
+import org.hyperic.sigar.win32.ServiceConfig;
+import org.hyperic.sigar.win32.Win32Exception;
 
-public class ColdfusionServerDetector
-    extends ServerDetector
-    implements AutoServerDetector
-{
-    private static Log myLog =
-        LogFactory.getLog(ColdfusionServerDetector.class);
+public class ColdfusionServerDetector extends ServerDetector implements
+    AutoServerDetector {
+    private static Log _log = LogFactory.getLog(ColdfusionServerDetector.class);
 
     private static final String SERVER_NAME = "Coldfusion";
+    private static final String VERSION_6 = "6.x";
     private static final String VERSION_7 = "7.x";
-    private static final String JAVA_PROCESS = "java";
-    private static final String CFMX7_PROCESS = "cfmx7";
-    private static final String PROCESS_PATTERN =
-        CFMX7_PROCESS+"|"+JAVA_PROCESS;
-    //this PTQL query matches the PROCESS_NAME and returns the parent process id
-    private static final String PTQL_QUERY = 
-        "State.Name.re="+PROCESS_PATTERN+",State.Name.Pne=$1,"+
-        "Args.*.ct=jrun.jar";
-//        "Args.*.ct=jrun.jar,Args.*.ct="+SERVER_NAME.toLowerCase();
+    private static final String FILE_DIV = System.getProperty("file.separator");
 
     public List getServerResources(ConfigResponse platformConfig)
-        throws PluginException
-    {
+        throws PluginException {
+        if (isWin32()) {
+            return findWindowsServerResources(platformConfig);
+        } else {
+            List servers = new ArrayList();
+            String version = getTypeInfo().getVersion();
+            String ptql = getTypeProperty("process.query");
+            List paths = getServerProcessList(version, ptql);
+            for (int i = 0; i < paths.size(); i++) {
+                String dir = (String) paths.get(i);
+                List found = getServerList(dir, version);
+                if (!found.isEmpty()) {
+                    servers.addAll(found);
+                }
+            }
+            return servers;
+        }
+    }
+
+    private List findWindowsServerResources(ConfigResponse platformConfig) {
+        String versionFile = getTypeProperty("version.file");
+        List configs;
+        try {
+            configs = Service
+                .getServiceConfigs("Macromedia JRun CFusion Server");
+        } catch (Win32Exception e) {
+            return null;
+        }
+        if (configs.size() == 0) {
+            return null;
+        }
+
         List servers = new ArrayList();
-        List paths = getServerProcessList();
-        for (int i=0; i<paths.size(); i++)
-        {
-            String dir = (String)paths.get(i);
-            List found = getServerList(dir);
-            if (!found.isEmpty())
-                servers.addAll(found);
+        for (int i = 0; i < configs.size(); i++) {
+            ServiceConfig serviceConfig = (ServiceConfig) configs.get(i);
+            String name = serviceConfig.getName();
+            File dir = new File(serviceConfig.getExe()).getParentFile();
+            File dll = new File(dir, "lib" + FILE_DIV + versionFile);
+            if (!dll.exists()) {
+                continue;
+            }
+            dir = dir.getParentFile(); // strip "Binn"
+
+            ServerResource server = createServerResource(
+                dir.getAbsolutePath(), name);
+            servers.add(server);
         }
         return servers;
     }
 
-    private static List getServerProcessList()
-    {
+    private ServerResource createServerResource(String installpath,
+        String name) {
+        ServerResource server = createServerResource(installpath);
+
+        String instance = name;
+        server.setName(server.getName() + " " + instance);
+
+        ConfigResponse config = new ConfigResponse();
+        config.setValue(Win32ControlPlugin.PROP_SERVICENAME, name);
+        server.setProductConfig(config);
+        server.setMeasurementConfig();
+        server.setControlConfig();
+
+        return server;
+    }
+
+    private static List getServerProcessList(String version, String ptql)
+        throws PluginException {
         List servers = new ArrayList();
-        long[] pids = getPids(PTQL_QUERY);
-        for (int i=0; i<pids.length; i++)
-        {
+        long[] pids = getPids(ptql);
+        for (int i = 0; i < pids.length; i++) {
             String exe = getProcExe(pids[i]);
             if (exe == null)
                 continue;
             File binary = new File(exe);
-            if (!binary.isAbsolute())
-                continue;
-            servers.add(binary.getAbsolutePath());
+            if (!binary.isAbsolute()) {
+                String path = getProcCwd(pids[i]);
+                if (path != null) {
+                    servers.add(path + "/" + binary);
+                }
+                throw new PluginException("plugin cannot determine process "
+                    + binary + "'s absolute path.  Please check permissions.");
+            } else {
+                servers.add(binary.getAbsolutePath());
+            }
         }
         return servers;
     }
 
-    public List getServerList(String path) throws PluginException
-    {
+    private List getServerList(String path, String version)
+        throws PluginException {
         List servers = new ArrayList();
         String installpath = getParentDir(path, 3);
+        if (version.equals(VERSION_6)) {
+            installpath = getParentDir(path, 2);
+        }
 
         ConfigResponse productConfig = new ConfigResponse();
         productConfig.setValue("installpath", installpath);
-
-        String version = VERSION_7;
-        // Only check the binaries if they match the path we expect
-        if (path.indexOf(JAVA_PROCESS) == -1 &&
-            path.indexOf(CFMX7_PROCESS) == -1)
-            return servers;
 
         ServerResource server = createServerResource(installpath);
         // Set custom properties
@@ -126,7 +162,7 @@ public class ColdfusionServerDetector
         setProductConfig(server, productConfig);
         // sets a default Measurement Config property with no values
         server.setMeasurementConfig();
-        server.setName(getPlatformName()+" "+SERVER_NAME+" "+version);
+        server.setName(getPlatformName() + " " + SERVER_NAME + " " + version);
         servers.add(server);
 
         return servers;
