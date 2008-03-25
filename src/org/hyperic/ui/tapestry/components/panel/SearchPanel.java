25fc17dd7ec9578ed132706ad1316cc5551e6b08
rpack <rpack@572de364-d518-0410-a359-afb9c36ce0ec>
2008-03-25 00:29:07 +0000

    The working version of the searchpanel

--- src/org/hyperic/ui/tapestry/components/panel/SearchPanel.java
+++ src/org/hyperic/ui/tapestry/components/panel/SearchPanel.java
@@ -9,8 +9,11 @@ import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.IScript;
 import org.apache.tapestry.PageRenderSupport;
 import org.apache.tapestry.TapestryUtils;
+import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.InjectScript;
 import org.apache.tapestry.annotations.Parameter;
+import org.apache.tapestry.engine.IEngineService;
+import org.apache.tapestry.engine.ILink;
 
 public abstract class SearchPanel extends BaseComponent{
     
@@ -22,19 +25,44 @@ public abstract class SearchPanel extends BaseComponent{
     @Parameter(name = "enableHotkeys", defaultValue = "ognl:true")
     public abstract boolean getEnableHotkeys();
     public abstract void setEnableHotkeys(boolean enable);
-    
+
+    /**
+     * The JSON object that specifies 
+     * @return the hotkey combination
+     */
+    @Parameter(name = "hotKey", defaultValue = "literal:{keyCode: 83, ctrl: true}")
+    public abstract String getHotkey();
+    public abstract void setHotkey(String hotKey);
+
     @InjectScript("SearchPanel.script")
     public abstract IScript getScript();
+    
+    @InjectObject("infrastructure:searchService")
+    public abstract IEngineService getSearchService();
+   
+    /**
+     * Get the URL for the search service
+     * Used for SearchPanel.script for the search XHR
+     * @return the URL.toString()
+     */
+    public String getSearchServiceURL(){
+        ILink link = getSearchService().getLink(false, null);
+        return link.getURL();
+    }
     
     public void renderComponent(IMarkupWriter writer, IRequestCycle cycle){
         super.renderComponent(writer, cycle);
         if (!cycle.isRewinding()) {
-	    if (getEnableHotkeys()) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		PageRenderSupport pageRenderSupport = TapestryUtils
-			.getPageRenderSupport(cycle, this);
-		getScript().execute(this, cycle, pageRenderSupport, map);
-	    }
+            if (getEnableHotkeys()) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                map.put("enableHotkeys", getEnableHotkeys());
+                map.put("keyCombination", getHotkey());
+                map.put("searchURL", getSearchServiceURL());
+                map.put("resourcePageURL", "Resource.do");
+                PageRenderSupport pageRenderSupport = TapestryUtils
+                .getPageRenderSupport(cycle, this);
+                getScript().execute(this, cycle, pageRenderSupport, map);
+            }
         }
     }
 }
