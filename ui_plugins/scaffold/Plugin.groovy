0c77055af474b7872f5f09f6d183ad318f5c3477
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2008-04-02 06:59:22 +0000

    Add documentation for resourceType attachments

--- ui_plugins/scaffold/Plugin.groovy
+++ ui_plugins/scaffold/Plugin.groovy
@@ -8,10 +8,12 @@ class Plugin extends HQUPlugin {
          * The following can be un-commented to have the plugin's view rendered in HQ.
          *
          * description:  The brief name of the view (e.g.: "Fast Executor")
-         * attachType:   one of ['masthead', 'admin']
+         * attachType:   one of ['masthead', 'admin', 'resource']
          * controller:   The controller to invoke when the view is to be generated
          * action:       The method within 'controller' to invoke
          * category:     (optional)  If set, specifies either 'tracker' or 'resource' menu
+         * resourceType: (if attachType == resource), specifies an list of 
+         *               type names to attach to (ex. ['MacOSX', 'Linux'])
          */
         /*
         addView(description:  'A Groovy HQU-@PLUGIN_NAME@',
