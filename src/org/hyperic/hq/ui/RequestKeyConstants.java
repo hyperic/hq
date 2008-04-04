afa830510d61b05486c67e2e3873f4e254585d46
rpack <rpack@572de364-d518-0410-a359-afb9c36ce0ec>
2008-04-04 20:16:14 +0000

    Constants for Tapestry Page Params and CSS classnames

---
+++ src/org/hyperic/hq/ui/RequestKeyConstants.java
@@ -0,0 +1,15 @@
+package org.hyperic.hq.ui;
+
+public class RequestKeyConstants extends Constants {
+
+    /*
+     * The Attachment id for a HQU plugin that used by the plugin page - user
+     * facing query param
+     */
+    public static final String PLUGIN_ID_PARAM = "id";
+
+    /* 
+     * Used for the HQU plugin body rendering by the hqu/Attach component 
+     */
+    public static final String HQU_PLUGIN_ID_PARAM = "attachId";
+}
---
+++ src/org/hyperic/hq/ui/pages/CSSConstants.java
@@ -0,0 +1,11 @@
+package org.hyperic.hq.ui.pages;
+
+public class CSSConstants {
+
+    public static final String PLATFORM = "platformIcon";
+    public static final String SERVER = "serverIcon";
+    public static final String SERVICE = "serviceIcon";
+    public static final String GROUP = "groupIcon";
+    public static final String APPLICATION = "applicationIcon";
+
+}
