7b36bbaf416c17c7af59a7f4a484953d5a744e36
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2008-04-10 00:25:31 +0000

    Simplify query to not have to know about fields, Hibernate will figure out the primary key

--- src/org/hyperic/hq/authz/server/session/RoleDAO.java
+++ src/org/hyperic/hq/authz/server/session/RoleDAO.java
@@ -6,7 +6,7 @@
  * normal use of the program, and does *not* fall under the heading of
  * "derived work".
  * 
- * Copyright (C) [2004, 2005, 2006], Hyperic, Inc.
+ * Copyright (C) [2004-2008], Hyperic, Inc.
  * This file is part of HQ.
  * 
  * HQ is free software; you can redistribute it and/or modify
@@ -34,7 +34,6 @@ import org.hyperic.hq.authz.server.session.ResourceGroup.ResourceGroupCreateInfo
 import org.hyperic.hq.authz.shared.AuthzConstants;
 import org.hyperic.hq.authz.shared.GroupCreationException;
 import org.hyperic.hq.authz.shared.ResourceGroupValue;
-import org.hyperic.hq.authz.shared.ResourceValue;
 import org.hyperic.hq.authz.shared.RoleValue;
 import org.hyperic.hq.common.SystemException;
 import org.hyperic.hq.dao.HibernateDAO;
@@ -194,7 +193,7 @@ public class RoleDAO extends HibernateDAO {
         return getSession()
             .createQuery("select distinct r from Role r, AuthzSubject s " +
                          "where r.system = ? and s.id = ? and " +
-                         "r.id not in (select id from s.roles) " +
+                         "r not in (select r2 from s.roles r2) " +
                          "order by r.sortName " +
                          (asc ? "asc" : "desc"))
             .setBoolean(0, system)
