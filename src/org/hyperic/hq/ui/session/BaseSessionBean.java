c0957feffaab44043ab38640708ab306459e18d5
rpack <rpack@572de364-d518-0410-a359-afb9c36ce0ec>
2008-04-10 17:53:18 +0000

    Modified to remove container managed params

--- src/org/hyperic/hq/ui/session/BaseSessionBean.java
+++ src/org/hyperic/hq/ui/session/BaseSessionBean.java
@@ -1,8 +1,17 @@
 package org.hyperic.hq.ui.session;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.hyperic.hq.ui.WebUser;
 
 public interface BaseSessionBean {
+    
+    /**
+     * @deprecated ditch this interface method and impls when struts is removed 
+     * for legacy support only 
+     */
+    public abstract void init(HttpServletRequest r, ServletContext c);
 
     public abstract WebUser getWebUser();
 
--- src/org/hyperic/hq/ui/session/BaseSessionBeanImpl.java
+++ src/org/hyperic/hq/ui/session/BaseSessionBeanImpl.java
@@ -6,30 +6,43 @@ import javax.servlet.http.HttpServletRequest;
 import org.hyperic.hq.ui.Constants;
 import org.hyperic.hq.ui.WebUser;
 
-final class BaseSessionBeanImpl implements BaseSessionBean{
+public final class BaseSessionBeanImpl implements BaseSessionBean {
 
-    //Used for bean setup
-    private ServletContext context;
-    private HttpServletRequest request;
-    
+    // Used for bean setup
+    private ServletContext _context;
+    private HttpServletRequest _request;
+
     private WebUser webUser;
     private Integer sessionId;
 
     public BaseSessionBeanImpl() {
-        webUser = (WebUser) getRequest().getSession().getAttribute(Constants.WEBUSER_SES_ATTR);
-        sessionId = webUser.getSessionId();
+    }
+
+    /**
+     * @deprecated see interface for reason 
+     */
+    public void init(HttpServletRequest request, ServletContext ctx) {
+        if (request == null || ctx == null)
+            throw new NullPointerException("init params cannot be null");
+        _context = ctx;
+        _request = request;
     }
 
     public WebUser getWebUser() {
+        if (webUser == null)
+            webUser = (WebUser) _request.getSession().getAttribute(
+                    Constants.WEBUSER_SES_ATTR);
         return webUser;
     }
 
     public void setWebUser(WebUser webUser) {
-        request.getSession().setAttribute(Constants.WEBUSER_SES_ATTR, webUser);
+        _request.getSession().setAttribute(Constants.WEBUSER_SES_ATTR, webUser);
         this.webUser = webUser;
     }
 
     public Integer getSessionId() {
+        if(sessionId == null)
+            sessionId = getWebUser().getSessionId();
         return sessionId;
     }
 
@@ -37,22 +50,4 @@ final class BaseSessionBeanImpl implements BaseSessionBean{
         this.sessionId = sessionId;
     }
 
-    /*
-     * These are called by Hivemind upon object creation
-     */
-    private ServletContext getServletContext() {
-        return context;
-    }
-
-    public void setServletContext(ServletContext context) {
-        this.context = context;
-    }
-
-    private HttpServletRequest getRequest() {
-        return request;
-    }
-
-    public void setRequest(HttpServletRequest request) {
-        this.request = request;
-    }
 }
