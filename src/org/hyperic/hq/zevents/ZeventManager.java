7ed37dd883b69a6732d03a1a6ee9269f939516b4
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2008-04-15 21:02:56 +0000

    HHQ-1961.  Get listener timeout outside of _listenerLock synch block, since getting the timeout will lock INIT_LOCK (and cause a deadlock)

--- src/org/hyperic/hq/zevents/ZeventManager.java
+++ src/org/hyperic/hq/zevents/ZeventManager.java
@@ -535,6 +535,7 @@ public class ZeventManager {
         }
 
         ThreadWatchdog dog = HQApp.getInstance().getWatchdog();
+        long timeout = getListenerTimeout();
         for (Iterator i=listenerBatches.entrySet().iterator(); i.hasNext(); ) {
             Map.Entry ent = (Map.Entry)i.next();
             ZeventListener listener = (ZeventListener)ent.getKey();
@@ -543,8 +544,7 @@ public class ZeventManager {
             synchronized (_listenerLock) {
                 InterruptToken t = null;
                 try {
-                    t = dog.interruptMeIn(getListenerTimeout(), 
-                                          TimeUnit.SECONDS,
+                    t = dog.interruptMeIn(timeout, TimeUnit.SECONDS,
                                           "Processing listener events");
                     listener.processEvents(Collections.unmodifiableList(batch));
                 } catch(RuntimeException e) {
