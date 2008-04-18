5e1dad1abb719e58145d3c9c6b444cbc2963e372
tkeeney <tkeeney@572de364-d518-0410-a359-afb9c36ce0ec>
2008-04-18 00:00:37 +0000

    The end goal is for all agent-server communication to execute through this new transport. Moved the utility classes from EE to .ORG since these classes will be useful to both EE and .ORG versions of the transport.

---
+++ src/org/hyperic/hq/transport/util/HQInvokerLocator.java
@@ -0,0 +1,133 @@
+/*
+ * NOTE: This copyright does *not* cover user programs that use HQ
+ * program services by normal system calls through the application
+ * program interfaces provided as part of the Hyperic Plug-in Development
+ * Kit or the Hyperic Client Development Kit - this is merely considered
+ * normal use of the program, and does *not* fall under the heading of
+ * "derived work".
+ *
+ * Copyright (C) [2004-2008], Hyperic, Inc.
+ * This file is part of HQ.
+ *
+ * HQ is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
+
+package org.hyperic.hq.transport.util;
+
+import java.util.Map;
+
+import org.jboss.remoting.InvokerLocator;
+
+
+/**
+ * The invoker locator containing configuration specific to the HQ transport.
+ */
+public class HQInvokerLocator extends InvokerLocator {
+    
+    private final String _agentToken;
+    
+    private boolean _oneWay;
+    
+    private boolean _guaranteed;
+    
+    private AsynchronousInvoker _invoker;
+    
+    /**
+     * Creates an instance.
+     *
+     * @param protocol The protocol.
+     * @param host The remote host.
+     * @param port The port on the remote host.
+     * @param path The invoker locator path or <code>null</code>.
+     * @param parameters The invoker locator parameters or <code>null</code>.
+     * @param agentToken The agent token.
+     */
+    public HQInvokerLocator(String protocol, 
+                            String host, 
+                            int port, 
+                            String path, 
+                            Map parameters, 
+                            String agentToken) {
+        super(protocol, host, port, path, parameters);
+        _agentToken = agentToken;
+    }
+    
+    /**
+     * Set the asynchronous invoker.
+     * 
+     * @param invoker The asynchronous invoker.
+     * @throws NullPointerException if the asychronous invoker is <code>null</code>.
+     */
+    public void setAsynchronousInvoker(AsynchronousInvoker invoker) {
+        if (invoker == null) {
+            throw new NullPointerException("async invoker is null");
+        }
+        
+        _invoker = invoker;
+    }
+    
+    /**
+     * @return The asynchronous invoker or <code>null</code>.
+     */
+    public AsynchronousInvoker getAsynchronousInvoker() {
+        return _invoker;
+    }
+    
+    /**
+     * @return The agent token.
+     */
+    public String getAgentToken() {
+        return _agentToken;
+    }
+    
+    /**
+     * Set invocations as one-way meaning that the client invoker will 
+     * not block on the remote invocation. This also means the client 
+     * application should not expect a return value.
+     */
+    public void setOneWay() {
+        _oneWay = true;
+    }
+    
+    /**
+     * @return <code>true</code> if the invocation is one-way.
+     */
+    public boolean isOneWay() {
+        return _oneWay;
+    }
+    
+    /**
+     * if invocations are already set as one-way, delivery may be specified 
+     * as guaranteed.
+     * 
+     * @throws IllegalStateException if invocations are not already set as 
+     *                               {@link #setOneWay() one-way}.
+     */
+    public void setDeliveryGuaranteed() {
+        if (!isOneWay()) {
+            throw new IllegalStateException("guranteed delivery is only applicable " +
+            		                        "for one-way invocation");
+        }
+        
+        _guaranteed = true;
+    }
+    
+    /**
+     * @return <code>true</code> if delivery is guaranteed.
+     */
+    public boolean isDeliveryGuaranteed() {
+        return _guaranteed;
+    }
+
+}
---
+++ src/org/hyperic/hq/transport/util/TransportUtils.java
@@ -0,0 +1,113 @@
+/*
+ * NOTE: This copyright does *not* cover user programs that use HQ
+ * program services by normal system calls through the application
+ * program interfaces provided as part of the Hyperic Plug-in Development
+ * Kit or the Hyperic Client Development Kit - this is merely considered
+ * normal use of the program, and does *not* fall under the heading of
+ * "derived work".
+ *
+ * Copyright (C) [2004-2008], Hyperic, Inc.
+ * This file is part of HQ.
+ *
+ * HQ is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
+
+package org.hyperic.hq.transport.util;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jboss.remoting.Client;
+import org.jboss.remoting.InvocationRequest;
+
+/**
+ * Provides utility methods for the transport layer.
+ */
+public class TransportUtils {
+    
+    /**
+     * Private constructor for utility class.
+     */
+    private TransportUtils() {}
+    
+    /**
+     * Determine if the invocation request represents a one-way (asynchronous) 
+     * invocation.
+     * 
+     * @param invocation The invocation request.
+     * @return <code>true</code> if this is a one-way invocation.
+     */
+    public static boolean isOneWayInvocation(InvocationRequest invocation) {
+        Map requestPayload = invocation.getRequestPayload();
+        
+        if (requestPayload != null) {
+            String value = (String)requestPayload.get(Client.ONEWAY_FLAG);
+            return Boolean.valueOf(value).booleanValue();
+        } else {
+            return false;            
+        }
+    }
+    
+    /**
+     * Set the invocation request as one-way (asynchronous).
+     * 
+     * @param invocation The invocation request.
+     */
+    public static void setOneWayInvocation(InvocationRequest invocation) {
+        Map requestPayload = invocation.getRequestPayload();
+        
+        if (requestPayload == null) {
+            requestPayload = new HashMap();
+            invocation.setRequestPayload(requestPayload);
+        }
+        
+        requestPayload.put(Client.ONEWAY_FLAG, Boolean.TRUE.toString());        
+    }
+    
+    /**
+     * Return the HTTP transport name.
+     * 
+     * @param encrypted <code>true</code> if the transport is encrypted.
+     * @return The HTTP transport name.
+     */
+    public static String getHttpTransport(boolean encrypted) {
+        if (encrypted) {
+            return "https";
+        } else {
+            return "http";
+        }
+    }
+    
+    /**
+     * Assert that the interface only contains operations with void return types.
+     * 
+     * @param serviceInterface The interface to inspect.
+     * @throws IllegalArgumentException if a non-void return type operation is found.
+     */
+    public static void assertVoidReturnTypes(Class serviceInterface) {
+        Method[] methods = serviceInterface.getMethods();
+        
+        for (int i = 0; i < methods.length; i++) {
+            Method method = methods[i];
+            
+            if (!method.getReturnType().equals(Void.TYPE)) {
+                throw new IllegalArgumentException(
+                        "found a non-void return type operation: "+method+
+                        " in "+serviceInterface);
+            }
+        }
+    }
+    
+}
