a565352aacfa8e47b6b4e137604db4700ef8348f
jtravis <jtravis@572de364-d518-0410-a359-afb9c36ce0ec>
2008-04-21 22:21:06 +0000

    Add more info about JSON methods

--- ui_plugins/scaffold/app/SampleController.groovy
+++ ui_plugins/scaffold/app/SampleController.groovy
@@ -14,7 +14,9 @@ class @CONTROLLER_NAME@Controller
         // The name of the currently-executed action dictates which .gsp file 
         // to render (in this case, index.gsp).
         //
-        // If you want to render AJAX, read RenderFrame.groovy for parameters.
+        // If you want to render AJAX, read RenderFrame.groovy for parameters
+        // or return a Map from this method and in init(), call:
+        //     setJSONMethods(['myJSONMethod', 'anotherJSONMethod'])
     	render(locals:[ plugin :  getPlugin(),
     	                userName: user.name])  
     }
