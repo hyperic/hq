76722ebcc2a3144a3365802273b4f9674672ed1e
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2008-04-29 16:01:23 +0000

    Add AlertDefinitionCategory to allow definition.setEsclation() from HQU.

---
+++ src/org/hyperic/hq/hqu/rendit_sys/metaclass/AlertDefinitionCategory.groovy
@@ -0,0 +1,16 @@
+package org.hyperic.hq.hqu.rendit.metaclass
+
+import org.hyperic.hq.events.server.session.AlertDefinitionManagerEJBImpl
+import org.hyperic.hq.events.server.session.AlertDefinition
+import org.hyperic.hq.authz.server.session.AuthzSubject
+import org.hyperic.hq.escalation.server.session.Escalation
+
+class AlertDefinitionCategory {
+
+    static void setEscalation(AlertDefinition d, AuthzSubject s,
+                              Escalation e) {
+        AlertDefinitionManagerEJBImpl.one.setEscalation(s.authzSubjectValue,
+                                                        d.getId(),
+                                                        e.getId())
+    }
+}
