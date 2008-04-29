1964de41afac49a99d7d80388adc418223534ea1
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2008-04-29 00:19:33 +0000

    Simpler to just let AppdefEntityID to parse the values

--- src/org/hyperic/hq/bizapp/server/trigger/conditional/ConfigChangedTrigger.java
+++ src/org/hyperic/hq/bizapp/server/trigger/conditional/ConfigChangedTrigger.java
@@ -6,7 +6,7 @@
  * normal use of the program, and does *not* fall under the heading of
  * "derived work".
  *
- * Copyright (C) [2004, 2005, 2006], Hyperic, Inc.
+ * Copyright (C) [2004-2008], Hyperic, Inc.
  * This file is part of HQ.
  *
  * HQ is free software; you can redistribute it and/or modify
@@ -129,8 +129,7 @@ public class ConfigChangedTrigger
         }
 
         try {
-            id = new AppdefEntityID(Integer.parseInt(sType),
-                                    Integer.parseInt(sID));
+            id = new AppdefEntityID(sType + ":" + sID);
         } catch(NumberFormatException exc){
             throw new InvalidTriggerDataException(
                 "Instance type: " + sType + " or id: " + sID +
