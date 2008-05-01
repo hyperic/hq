174cc8f5d3a369b804a7328c62fb94114db08a34
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2008-05-01 17:41:23 +0000

    Add APIs to AuthzSubjectCategory to get and set user preferences.

--- src/org/hyperic/hq/hqu/rendit_sys/metaclass/AuthzSubjectCategory.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/metaclass/AuthzSubjectCategory.groovy
@@ -1,13 +1,39 @@
 package org.hyperic.hq.hqu.rendit.metaclass
 
-import org.hyperic.hq.authz.shared.PermissionManager
 import org.hyperic.hq.authz.shared.PermissionManagerFactory
 import org.hyperic.hq.authz.server.session.AuthzSubject
+import org.hyperic.hq.authz.server.session.AuthzSubjectManagerEJBImpl
+import org.hyperic.hq.authz.shared.AuthzSubjectManagerLocal
+import org.hyperic.util.config.ConfigResponse
 
 class AuthzSubjectCategory {
+
+    static AuthzSubjectManagerLocal subMan = AuthzSubjectManagerEJBImpl.one
+
+    /**
+     * Check if the current user has administration permission
+     */
     static boolean isSuperUser(AuthzSubject subject) {
 		PermissionManagerFactory
 			.getInstance()
 			.hasAdminPermission(subject.id)
+    }
+
+    /**
+     * Get a user preference by key
+     */
+    static String getPreference(AuthzSubject subject, String key,
+                                String defaultValue) {
+        ConfigResponse cr = subMan.getUserPrefs(subject, subject.id)
+        cr.getValue(key, defaultValue)
+    }
+
+    /**
+     * Set a user preference
+     */
+    static void setPreference(AuthzSubject subject, String key, String val) {
+        ConfigResponse cr = subMan.getUserPrefs(subject, subject.id)
+        cr.setValue(key, val)
+        subMan.setUserPrefs(subject, subject.id, cr)
     }
 }
