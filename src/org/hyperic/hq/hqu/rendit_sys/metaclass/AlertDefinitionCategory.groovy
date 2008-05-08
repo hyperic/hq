60d4f21fea3868d5325c7f1f0fccfd7a9015660d
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2008-05-08 23:04:52 +0000

    Add ability to unset escalation

--- src/org/hyperic/hq/hqu/rendit_sys/metaclass/AlertDefinitionCategory.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/metaclass/AlertDefinitionCategory.groovy
@@ -1,14 +1,20 @@
 package org.hyperic.hq.hqu.rendit.metaclass
 
-import org.hyperic.hq.events.server.session.AlertDefinitionManagerEJBImpl
+import org.hyperic.hq.events.server.session.AlertDefinitionManagerEJBImpl as AdefMan
 import org.hyperic.hq.events.server.session.AlertDefinition
 import org.hyperic.hq.authz.server.session.AuthzSubject
 import org.hyperic.hq.escalation.server.session.Escalation
+import org.hyperic.hq.escalation.server.session.EscalationManagerEJBImpl as EscMan
+import org.hyperic.hq.events.server.session.ClassicEscalationAlertType
 
 class AlertDefinitionCategory {
 
     static void setEscalation(AlertDefinition d, AuthzSubject s,
                               Escalation e) {
-        AlertDefinitionManagerEJBImpl.one.setEscalation(s, d.getId(), e.getId())
+        AdefMan.one.setEscalation(s, d.getId(), e.getId())
+    }
+
+    static void unsetEscalation(AlertDefinition d, AuthzSubject s) {
+        EscMan.one.setEscalation(ClassicEscalationAlertType.CLASSIC, d.getId(), null)
     }
 }
