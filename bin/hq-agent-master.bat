b83b230e22d4d0d6740673902a33ebbc232643c7
gviedma <gviedma@572de364-d518-0410-a359-afb9c36ce0ec>
2008-05-09 23:03:51 +0000

    Changes to support new agent directory structure

---
+++ bin/hq-agent-master.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+
+if "%OS%"=="Windows_NT" goto nt
+echo This script only works with NT-based versions of Windows.
+goto :eof
+
+:nt
+rem
+rem Find the application home.
+rem
+rem %~dp0 is location of current script under NT
+set _AGENTHOME=%~dp0..\
+
+set ROLLBACK_PROPS=%_AGENTHOME%conf\rollback.properties
+set AGENT_BUNDLE_PROP=set.HQ_AGENT_BUNDLE
+
+rem look for the agent bundle property in the rollback properties file
+rem and invoke the bundle hq-agent.bat script
+for /F "delims== tokens=1*" %%i in (%ROLLBACK_PROPS%) do (
+  if "%%i"=="%AGENT_BUNDLE_PROP%" (
+    if not EXIST %_AGENTHOME%bundles\%%j\bin\hq-agent.bat (
+      echo Failed to find bundle script %_AGENTHOME%bundles\%%j\bin\hq-agent.bat.
+    ) else (
+      call %_AGENTHOME%bundles\%%j\bin\hq-agent.bat %*
+    )
+  )
+)
---
+++ bin/hq-agent-master.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# find the current path to master executable
+FINDNAME=$0 
+while [ -h $FINDNAME ] ; do FINDNAME=`ls -ld $FINDNAME | awk '{print $NF}'` ; done 
+RUNDIR=`echo $FINDNAME | sed -e 's@/[^/]*$@@'` 
+unset FINDNAME
+
+# cd to top level agent home
+if test -d $RUNDIR; then
+  cd $RUNDIR/..
+else
+  cd ..
+fi
+
+ROLLBACK_PROPERTIES=conf/rollback.properties
+PROP_NAME=set.HQ_AGENT_BUNDLE
+
+# resolve the HQ Agent Bundle property
+AGENT_BUNDLE=`grep $PROP_NAME $ROLLBACK_PROPERTIES | awk -F= '{print $2}'`
+
+if test -d "./bundles/$AGENT_BUNDLE"; then
+  # pass on the command to the bundle
+  #echo "Invoking agent bundle $AGENT_BUNDLE"
+  ./bundles/$AGENT_BUNDLE/bin/hq-agent.sh "$@"
+else
+  echo "Bundle $AGENT_BUNDLE does not exist!"
+fi
