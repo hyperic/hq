7dc794df33e6c058e08ad49b20d802f26071f2e1
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2008-05-10 05:43:43 +0000

    Add method to delete resources [merge from HEAD]

--- src/org/hyperic/hq/hqu/rendit_sys/metaclass/ResourceCategory.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/metaclass/ResourceCategory.groovy
@@ -17,6 +17,7 @@ import org.hyperic.hq.appdef.server.session.PlatformManagerEJBImpl as PlatMan
 import org.hyperic.hq.appdef.server.session.ServerManagerEJBImpl as ServerMan
 import org.hyperic.hq.appdef.server.session.ServiceManagerEJBImpl as ServiceMan
 import org.hyperic.hq.measurement.server.session.DerivedMeasurementManagerEJBImpl as DMMan
+import org.hyperic.hq.bizapp.server.session.AppdefBossEJBImpl as AppdefBoss
 import org.hyperic.hq.events.server.session.AlertDefinitionManagerEJBImpl as AlertMan
 import org.hyperic.hq.livedata.server.session.LiveDataManagerEJBImpl
 import org.hyperic.hq.livedata.shared.LiveDataCommand
@@ -24,6 +25,7 @@ import org.hyperic.hq.livedata.shared.LiveDataResult
 import org.hyperic.util.config.ConfigResponse
 import org.hyperic.hq.hqu.rendit.util.ResourceConfig
 import org.hyperic.hq.hqu.rendit.helpers.ResourceHelper
+import org.hyperic.hq.auth.shared.SessionManager
 
 /**
  * This class provides tonnes of abstractions over the Appdef layer.
@@ -397,6 +399,21 @@ class ResourceCategory {
             throw new IllegalArgumentException("Resource prototype [" + 
                                                "${proto.name} not available " + 
                                                "to createInstance()")
+        }
+    }
+
+    static void remove(Resource r, AuthzSubject user) {
+        def boss = AppdefBoss.one
+        def mgr = SessionManager.instance
+        def sessionId = mgr.put(user)
+        if (r.isPlatform()) {
+            boss.removePlatform(sessionId, r.instanceId)
+        }
+        else if (r.isServer()) {
+            boss.removeServer(sessionId, r.instanceId)
+        }
+        else if (r.isService()) {
+            boss.removeService(sessionId, r.instanceId)
         }
     }
 
