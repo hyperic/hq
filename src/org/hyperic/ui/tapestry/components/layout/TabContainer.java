634c4c6db06d0e14f802c65ec3d3244cc9fa1f2e
rpack <rpack@572de364-d518-0410-a359-afb9c36ce0ec>
2008-05-19 17:31:25 +0000

    Removed dojo tabs

--- src/org/hyperic/ui/tapestry/components/layout/TabContainer.java
+++ src/org/hyperic/ui/tapestry/components/layout/TabContainer.java
@@ -1,6 +1,7 @@
 package org.hyperic.ui.tapestry.components.layout;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -9,35 +10,65 @@ import org.apache.tapestry.IRequestCycle;
 import org.apache.tapestry.IScript;
 import org.apache.tapestry.PageRenderSupport;
 import org.apache.tapestry.TapestryUtils;
+import org.apache.tapestry.annotations.InjectComponent;
 import org.apache.tapestry.annotations.InjectScript;
 import org.apache.tapestry.annotations.Parameter;
+import org.apache.tapestry.components.Block;
+import org.apache.tapestry.link.AbstractLinkComponent;
 import org.hyperic.ui.tapestry.components.BaseComponent;
 
-public abstract class TabContainer extends BaseComponent{
+public abstract class TabContainer extends BaseComponent {
 
     @InjectScript("TabContainer.script")
     public abstract IScript getScript();
-    
-    @Parameter(name="conaterStyle", defaultValue="literal:width:100%;height:100%;margin-right:5px")
+
+    @InjectComponent("link")
+    public abstract AbstractLinkComponent getLinkComponent();
+
+    @Parameter(name = "conaterStyle", defaultValue = "literal:width:100%;height:100%;margin-right:5px")
     public abstract String getContainerStyle();
 
-    @Parameter(name="tabs")
+    @Parameter(name = "conatinerId")
+    public abstract String getContainerId();
+
+    @Parameter(name = "tabs")
     public abstract List<Tab> getTabs();
-    
-    @Parameter(name="selectedId")
+
+    @Parameter(name = "selectedId")
     public abstract String getSelectedId();
+    public abstract void setSelectedId(String selectedId);
     
+    @Parameter(name = "tabClass", defaultValue="literal:tabButton")
+    public abstract String getTabClass();
+
     public abstract Tab getTab();
     public abstract void setTab(Tab tab);
-    
+
     public void renderComponent(IMarkupWriter writer, IRequestCycle cycle) {
-        if (!cycle.isRewinding()) {
-            Map map = new HashMap();
-            PageRenderSupport pageRenderSupport = TapestryUtils
-                    .getPageRenderSupport(cycle, this);
-            getScript().execute(this, cycle, pageRenderSupport, map);
-        }
         super.renderComponent(writer, cycle);
     }
     
+    /**
+     * 
+     * @param selectedId the currently selected tab id
+     */
+    public void tabClicked(String selectedId) {
+        setSelectedId(selectedId);
+    }
+
+    /**
+     * 
+     * @return the currently selected block. If no block is selected, select the
+     *         first
+     */
+    public Block getSelectedBlock() {
+        Iterator<Tab> i = getTabs().iterator();
+        while (i.hasNext()) {
+            Tab t = i.next();
+            if (t.getId().equals(getSelectedId())) {
+                return t.getBlock();
+            }
+        }
+        return getTabs().get(0).getBlock();
+    }
 }
