f9bc75c344d090058a75796da62aaf4ed9fe41cc
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2008-06-27 20:24:04 +0000

    Remove source code limitation of 500 characters for log events. Increase log_trackspool to 4k, truncate at 4000 characters. [HHQ-2066]

--- src/org/hyperic/hq/product/TrackEvent.java
+++ src/org/hyperic/hq/product/TrackEvent.java
@@ -43,7 +43,7 @@ public class TrackEvent implements java.io.Serializable {
     //maxlen as defined in schema (sql/events/EventLog.hbm.xml)
     //events are also encoded and stored on disk with a max size of 1024
     //see AgentDListProvider.RECSIZE
-    public static final int MESSAGE_MAXLEN = 500;
+    public static final int MESSAGE_MAXLEN = 4000;
     public static final int SOURCE_MAXLEN  = 100;
 
     private AppdefEntityID id;  // The appdef id.
