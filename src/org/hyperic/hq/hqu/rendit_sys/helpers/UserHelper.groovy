03da750065ad56534228a653b205d1fcb2306882
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2008-07-09 18:30:31 +0000

    Sync UserHelper with trunk, w/ AuthzSubject -> AuthzSubjectValue differences.

--- src/org/hyperic/hq/hqu/rendit_sys/helpers/UserHelper.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/helpers/UserHelper.groovy
@@ -1,12 +1,18 @@
 package org.hyperic.hq.hqu.rendit.helpers
 
-import org.hyperic.hq.auth.server.session.AuthManagerEJBImpl
+import java.util.List
+
+import org.hyperic.hq.auth.server.session.AuthManagerEJBImpl as AuthMan
 import org.hyperic.hq.authz.server.session.AuthzSubject
 import org.hyperic.hq.authz.server.session.AuthzSubjectManagerEJBImpl as SubjectMan
 import org.hyperic.hq.authz.shared.AuthzSubjectValue
 
+/**
+ * The UserHelper can be used to find Users in the HQ system.
+ */
 class UserHelper extends BaseHelper {
     private subjectMan = SubjectMan.one
+    private authMan = AuthMan.one
     private AuthzSubjectValue userValue
 
     UserHelper(AuthzSubject user) {
@@ -16,9 +22,9 @@ class UserHelper extends BaseHelper {
 
     /**
      * Find all users
-     * @return a List of {@link AuthzSubject}s
+     * @return a List of {@link AuthzSubjectValues}s
      */
-    public getAllUsers() {
+    public List getAllUsers() {
         subjectMan.getAllSubjects(userValue, [], null).collect {
             subjectMan.findSubjectById(it.id)
         }
@@ -26,44 +32,78 @@ class UserHelper extends BaseHelper {
 
     /**
      * Find a user by name
-     * @return a {@link AuthzSubject}s
+     * @return a {@link AuthzSubjectValue}
      */
-    public findUser(name) {
+    public findUser(String name) {
         subjectMan.findSubjectByName(name)
+    }
+
+    /**
+     * Get a user by id.
+     * @return The {@link AuthzSubject} for this id, or null if the id does
+     * not exist.
+     */
+    public getUser(Integer id) {
+        subjectMan.getSubjectById(id)
     }
 
     /**
      * Create a user
      * @return a {@link AuthzSubject}s
      */
-    public createUser(userName, active, dsn, dept, email, first, last, phone,
-                      sms, html) { 
+    public createUser(String userName, boolean active, String dsn,
+                      String dept, String email, String first,
+                      String last, String phone, String sms,
+                      boolean html) {
         subjectMan.createSubject(userValue, userName, active, dsn, dept, email,
                                  first, last, phone, sms, html)
     }
 
     /**
-     * Update a user
+     * Create a user with the given password.
      * @return a {@link AuthzSubject}s
      */
-    public updateUser(found, active, dsn, dept, email, first, last, phone, sms,
-                      html) {
+    public createUser(String userName, String pass, boolean active,
+                      String dsn, String dept, String email,
+                      String first, String last, String phone,
+                      String sms, boolean html) {
+        def user = subjectMan.createSubject(userValue, userName, active, dsn,
+                                            dept, email, first, last, phone,
+                                            sms, html)
+        authMan.addUser(userValue, userName, pass)
+        user
+    }
+
+    /**
+     * Update a user
+     * @param found The {@link AuthzSubject} to update.
+     */
+    public void updateUser(found, boolean active, String dsn,
+                           String dept, String email, String first,
+                           String last, String phone, String sms, boolean html) {
         subjectMan.updateSubject(user, found, active, dsn, dept, email, first,
                                  last, phone, sms, html)
     }
      
     /**
-     * Update a user's password
+     * Update a user's password hash.
      */
-    public updateUserPassword(subject, password) {
-        AuthManagerEJBImpl.one.changePasswordHash(userValue, subject.name,
-                                                  password)
+    public void updateUserPassword(String subject, String hash) {
+        authMan.changePasswordHash(userValue, subject.name, hash)
+    }
+
+    /**
+     * Change the password for a user
+     * @param subject The {@link AuthzSubject} to change the password for
+     */
+    public void changeUserPassword(subject, String password) {
+        authMan.changePassword(userValue, subject.name, password)
     }
 
     /**
      * Remove a user from database
      */
-    public removeUser(uid) {
-        subjectMan.removeSubject(userValue, uid)
+    public void removeUser(Integer id) {
+        subjectMan.removeSubject(userValue, id)
     }
  }
