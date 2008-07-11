94e2720008d2902b57e48e21d9a6c62c9124a3a5
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2008-07-11 19:15:57 +0000

    Add role finders.

--- src/org/hyperic/hq/hqu/rendit_sys/helpers/RoleHelper.groovy
+++ src/org/hyperic/hq/hqu/rendit_sys/helpers/RoleHelper.groovy
@@ -2,6 +2,7 @@ package org.hyperic.hq.hqu.rendit.helpers
 
 import org.hyperic.hq.authz.server.session.RoleManagerEJBImpl as RoleMan
 import org.hyperic.hq.authz.server.session.AuthzSubject
+import org.hyperic.hq.authz.server.session.Role
 
 class RoleHelper extends BaseHelper {
 
@@ -13,8 +14,29 @@ class RoleHelper extends BaseHelper {
 
     /**
      * Find all Roles
+     *
+     * @return A Collection of Roles in the system.
      */
     public Collection getAllRoles() {
-        return roleMan.allRoles
+        roleMan.allRoles
+    }
+
+    /**
+     * Find a Role by name.
+     * @param name The role name to search for.
+     * @return The Role with the given name, or null if that role does not
+     * exist.
+     */
+    public Role findRoleByName(String name) {
+        roleMan.findRoleByName(name)
+    }
+
+    /**
+     * Find a Role by id.
+     * @param id The role id to search for.
+     * @return The Role with the given id, or null if that role does not exist.
+     */
+    public Role findRoleById(int id) {
+        roleMan.findRoleById(id)
     }
 }
