cf40831472c8fe0cd9a76bdd730d7a1fcbcfbf40
rmorgan <rmorgan@572de364-d518-0410-a359-afb9c36ce0ec>
2008-07-18 03:15:12 +0000

    Revert inadvertent checkin. [merge from trunk]

--- src/org/hyperic/hq/authz/server/session/RoleManagerEJBImpl.java
+++ src/org/hyperic/hq/authz/server/session/RoleManagerEJBImpl.java
@@ -732,7 +732,7 @@ public class RoleManagerEJBImpl extends AuthzSession implements SessionBean {
 
         return value;
     }
-se
+
     /**
      * Get the Resource entity associated with this Role.
      * @param role This role.
