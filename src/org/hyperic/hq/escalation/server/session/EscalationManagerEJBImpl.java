6f140cc7abd9f82c73e2719819ebd926175b251b
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2008-07-31 22:50:27 +0000

    Don't loop unless there are actions

--- src/org/hyperic/hq/escalation/server/session/EscalationManagerEJBImpl.java
+++ src/org/hyperic/hq/escalation/server/session/EscalationManagerEJBImpl.java
@@ -467,7 +467,7 @@ public class EscalationManagerEJBImpl
         //        we allow this to proceed
         _log.debug("Executing state[" + s.getId() + "]");
         if (actionIdx >= e.getActions().size()) {
-            if (e.isRepeat()) {
+            if (e.isRepeat() && e.getActions().size() > 0) {
                 actionIdx = 0;          // Loop back
             }
             else {
