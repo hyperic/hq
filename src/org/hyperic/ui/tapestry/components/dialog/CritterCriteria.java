3d3eb81a1672eacc846e0cd361344a5d2063824a
rpack <rpack@572de364-d518-0410-a359-afb9c36ce0ec>
2008-08-12 05:47:36 +0000

    Added

---
+++ src/org/hyperic/ui/tapestry/components/dialog/CritterCriteria.java
@@ -0,0 +1,27 @@
+package org.hyperic.ui.tapestry.components.dialog;
+
+public class CritterCriteria {
+
+    private Object _selectedType;
+    private String _stringValue;
+
+    public CritterCriteria() {
+    }
+
+    public Object getSelectedType() {
+        return _selectedType;
+    }
+
+    public void setSelectedType(Object selectedType) {
+        _selectedType = selectedType;
+    }
+
+    public String getStringValue() {
+        return _stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        _stringValue = stringValue;
+    }
+
+}
