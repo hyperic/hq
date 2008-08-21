119f21a31b9a324380b4f1223645230f48d6ac8d
scottmf <scottmf@572de364-d518-0410-a359-afb9c36ce0ec>
2008-08-21 22:09:01 +0000

    [HQ-1389] fixed NullPointerException

--- src/org/hyperic/hq/appdef/server/session/PagerProcessor_aiplatform.java
+++ src/org/hyperic/hq/appdef/server/session/PagerProcessor_aiplatform.java
@@ -62,9 +62,13 @@ public class PagerProcessor_aiplatform implements PagerProcessorExt {
             // XXX scottmf, this needs to be refactored to not pass Objects 
             // around and hide the backend implementation of DAOs from the 
             // front end.  Unfortunately it won't be in 3.x, definitely 4.0
-            return AIQSynchronizer.getAIQPlatform(
-                new AIPlatformDAO(DAOFactory.getDAOFactory()), value)
-                .getAIPlatformValue();
+            aiplatform = AIQSynchronizer.getAIQPlatform(
+                new AIPlatformDAO(DAOFactory.getDAOFactory()), value);
+            if (aiplatform != null) {
+                return aiplatform.getAIPlatformValue();
+            } else {
+                return null;
+            }
         }
         return o;
     }
