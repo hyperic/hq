acd5f477fad03789c0ce3cf498481d23295d7840
rpack <rpack@572de364-d518-0410-a359-afb9c36ce0ec>
2008-08-21 21:12:03 +0000

    Inherit the properties from Tapestry's exception presenter

--- src/org/hyperic/hq/ui/presenters/ExceptionPresenterImpl.java
+++ src/org/hyperic/hq/ui/presenters/ExceptionPresenterImpl.java
@@ -1,12 +1,18 @@
 package org.hyperic.hq.ui.presenters;
 
 import org.apache.tapestry.IRequestCycle;
-import org.apache.tapestry.error.ExceptionPresenter;
+import org.apache.tapestry.web.WebRequest;
 
-public class ExceptionPresenterImpl implements ExceptionPresenter {
+public class ExceptionPresenterImpl extends org.apache.tapestry.error.ExceptionPresenterImpl {
+
+    private WebRequest _request;
 
     public void presentException(IRequestCycle cycle, Throwable t) {
 
+    }
+
+    public void setWebRequest(WebRequest request) {
+        _request = request;
     }
 
 }
