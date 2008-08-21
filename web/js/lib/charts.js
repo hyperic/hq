0af38adb3b6eee80904d0b78d30c8d88299098a8
astroganov <astroganov@572de364-d518-0410-a359-afb9c36ce0ec>
2008-08-21 21:12:38 +0000

    moved formatDate out from charts.js into lib.js; updated to latest formatDate code from cloudstatus

--- web/js/lib/charts.js
+++ web/js/lib/charts.js
@@ -9175,46 +9175,4 @@ String.prototype.padR = function(width,pad)
     if (length < 1) this.substr(0,width);
  
     return (this + String.repeat(pad,length)).substr(0,width);
-} 
-
-// WTF??????????
-Date.prototype.formatDate = function(format)
-{
-    var date = this;
-    if (!format)
-      format="MM/dd/yyyy";               
- 
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();    
- 
-    format = format.replace("MM",month.toString().padL(2,"0"));        
- 
-    if (format.indexOf("yyyy") > -1)
-        format = format.replace("yyyy",year.toString());
-    else if (format.indexOf("yy") > -1)
-        format = format.replace("yy",year.toString().substr(2,2));
- 
-    format = format.replace("dd",date.getDate().toString().padL(2,"0"));
- 
-    var hours = date.getHours();       
-    if (format.indexOf("t") > -1)
-    {
-       if (hours > 11)
-        format = format.replace("t","pm")
-       else
-        format = format.replace("t","am")
-    }
-    if (format.indexOf("HH") > -1)
-        format = format.replace("HH",hours.toString().padL(2,"0"));
-    if (format.indexOf("hh") > -1) {
-        if (hours > 12) hours -= 12;
-        if (hours == 0) hours = 12;
-        format = format.replace("hh",hours.toString().padL(2,"0"));        
-    }
-    if (format.indexOf("mm") > -1)
-       format = format.replace("mm",date.getMinutes().toString().padL(2,"0"));
-    if (format.indexOf("ss") > -1)
-       format = format.replace("ss",date.getSeconds().toString().padL(2,"0"));
-    return format;
 }
-
--- web/js/lib/lib.js
+++ web/js/lib/lib.js
@@ -2569,3 +2569,55 @@ function setField(field, value){
         field.value = value;
     }
 }
+
+Date.prototype.formatDate = function(format)
+{
+    var date = this;
+    var short_months = ['Jan','Feb','Mar','Apr','May','Jun', 'Jul','Aug','Sep','Oct','Nov','Dec'];
+    var months = ['January','February','March','April','May','June', 'July','August','September','October','November','December'];
+    if (!format)
+      format="MM/dd/yyyy";               
+ 
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();    
+ 
+    format = format.replace("MM",month.toString().padL(2,"0"));        
+ 
+    format = format.replace("M",month.toString());
+ 
+    if (format.indexOf("yyyy") > -1)
+        format = format.replace("yyyy",year.toString());
+    else if (format.indexOf("yy") > -1)
+        format = format.replace("yy",year.toString().substr(2,2));
+ 
+    format = format.replace("dd",date.getDate().toString().padL(2,"0"));
+
+    format = format.replace("d",date.getDate().toString());
+ 
+    format = format.replace("b",short_months[date.getMonth()]);
+
+    format = format.replace("B",months[date.getMonth()]);
+
+    format = format.replace("z",dojo.date.getTimezoneName(date));
+
+    var hours = date.getHours();       
+    if (format.indexOf("t") > -1)
+    {
+       if (hours > 11)
+        format = format.replace("t","pm")
+       else
+        format = format.replace("t","am")
+    }
+    if (format.indexOf("HH") > -1)
+        format = format.replace("HH",hours.toString().padL(2,"0"));
+    if (format.indexOf("hh") > -1) {
+        if (hours > 12) hours -= 12;
+        if (hours == 0) hours = 12;
+        format = format.replace("hh",hours.toString().padL(2,"0"));        
+    }
+    if (format.indexOf("mm") > -1)
+       format = format.replace("mm",date.getMinutes().toString().padL(2,"0"));
+    if (format.indexOf("ss") > -1)
+       format = format.replace("ss",date.getSeconds().toString().padL(2,"0"));
+    return format;
+}
