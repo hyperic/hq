8aabe989ae97c1d4d2b9de3c8ecd12d3ac39a2e4
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2008-09-19 02:44:18 +0000

    Don't need to set flag again

--- src/org/hyperic/hq/autoinventory/server/session/RuntimeReportProcessor.java
+++ src/org/hyperic/hq/autoinventory/server/session/RuntimeReportProcessor.java
@@ -366,8 +366,6 @@ public class RuntimeReportProcessor {
                 ServerValue foundAppdefServer
                     = AIConversionUtil.convertAIServerToServer(aiserver, 
                                                                _serverMgr);
-            
-                foundAppdefServer.setWasAutodiscovered(true);
 
                 serverTypePK = foundAppdefServer.getServerType().getId();
                 
