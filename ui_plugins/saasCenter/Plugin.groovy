31dae1ef40f38ced37fd75c58940149d8f5cbe80
rpack <rpack@572de364-d518-0410-a359-afb9c36ce0ec>
2008-10-29 17:53:19 +0000

    Conditionalize the attachement

--- ui_plugins/saasCenter/Plugin.groovy
+++ ui_plugins/saasCenter/Plugin.groovy
@@ -1,10 +1,24 @@
 import org.hyperic.hq.hqu.rendit.HQUPlugin
 
+import org.hyperic.hq.authz.server.session.AuthzSubject
+import org.hyperic.hq.authz.server.session.Resource
+import org.hyperic.hq.authz.server.session.ResourceManagerEJBImpl as rme
+import org.hyperic.hq.hqu.AttachmentDescriptor
+import org.hyperic.hq.hqu.server.session.Attachment
+
 class Plugin extends HQUPlugin {
     
     Plugin() {
         addMastheadView(true, '/saascenter/index.hqu', 'SaaS Center', 'resource')    
     }
     
+    AttachmentDescriptor getAttachmentDescriptor(Attachment a, Resource r,
+            AuthzSubject u) 
+    {
+        if (rme.one.resourcesExistOfType('AWS')) {        
+            return super.getAttachmentDescriptor(a, r, u)
+        } else {
+            return null
+        }
+    }
 }    
-
