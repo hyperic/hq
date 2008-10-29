96599cfe591c258e13044e85d483f109697e309d
rpack <rpack@572de364-d518-0410-a359-afb9c36ce0ec>
2008-10-29 16:54:26 +0000

    Attach to the "Resource" menu

--- ui_plugins/saasCenter/Plugin.groovy
+++ ui_plugins/saasCenter/Plugin.groovy
@@ -1,15 +1,10 @@
 import org.hyperic.hq.hqu.rendit.HQUPlugin
 
-import saascenterController
-
 class Plugin extends HQUPlugin {
-    void initialize(File pluginDir) {
-        super.initialize(pluginDir)
-        addView(description:  'A Groovy HQU-saasCenter',
-                attachType:   'masthead', 
-                controller:   SaascenterController,
-                action:       'index', 
-                category:     'tracker')
+    
+    Plugin() {
+        addMastheadView(true, '/saascenter/index.hqu', 'SaaS Center', 'resource')    
     }
-}
+    
+}    
 
