970ab9f49614452405f6a0d2249d34d8ed50a0fa
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2008-12-09 03:40:56 +0000

    HHQ-2586.  Reviewed by rmorgan.  Cache handles to templates, since each template we create results in a new class getting thrown into the ClassLoader, which in turn well never get cleaned up (merge from HEAD)

--- ui_plugins/tmpl_render/Renderer.groovy
+++ ui_plugins/tmpl_render/Renderer.groovy
@@ -1,4 +1,7 @@
 import groovy.text.SimpleTemplateEngine
+import groovy.text.Template
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 
 
 /**
@@ -6,12 +9,37 @@ import groovy.text.SimpleTemplateEngine
  * primarily used through the RenditServer.renderTemplate() method
  */
 class Renderer {
-    def render(File gspFile, Map params, Writer output) {
-        gspFile.withReader { reader ->
-            def eng = new SimpleTemplateEngine(Thread.currentThread().contextClassLoader)
-            def template = eng.createTemplate(reader) 
-            template.make(params).writeTo(output)
+    // We cache the templates here in a static context, because each
+    // creation of a template results in a new class -- and that's
+    // bad if classloaders aren't being cleaned up correctly.
+    private static final Map<String, Map> _templates = [:]
+    private final Log _log = LogFactory.getLog(Renderer.class.name)
+
+    private Template getTemplate(File gspFile) {
+        if (!gspFile.isFile()) {
+            throw new RuntimeException("File [${gspFile.absolutePath}] not readable or not found");
         }
+        
+        long mtime = gspFile.lastModified()
+        synchronized(_templates) {
+            Map tmplMap = _templates.get(gspFile)
+            
+            if (!tmplMap || tmplMap.mtime != mtime) {
+                _log.info("Creating template for [${gspFile.absolutePath}]")
+                gspFile.withReader { reader ->
+                    def eng = new SimpleTemplateEngine(Thread.currentThread().contextClassLoader)
+                    Template t = eng.createTemplate(reader)
+                    _templates[gspFile] = [mtime: mtime, template : t]
+                    return t
+                }
+            } else {
+                return tmplMap.template
+            }
+        }
+    }
+
+    def render(File gspFile, Map params, Writer output) {
+        getTemplate(gspFile).make(params).writeTo(output)
         output.flush()
     }
 }
