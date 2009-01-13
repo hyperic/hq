97ae8ca502c872390d358b00fa07911208d4b618
clee <clee@572de364-d518-0410-a359-afb9c36ce0ec>
2009-01-13 22:50:28 +0000

    [HQ-1556] Propagate escalation change to child alert definition (merge from HEAD)

--- src/org/hyperic/hq/events/server/session/AlertDefinitionDAO.java
+++ src/org/hyperic/hq/events/server/session/AlertDefinitionDAO.java
@@ -432,4 +432,13 @@ public class AlertDefinitionDAO extends HibernateDAO {
             .uniqueResult();
     }
 
+    int setChildrenEscalation(AlertDefinition def, Escalation esc) {
+        return createQuery("update AlertDefinition set escalation = :esc, " +
+                           "mtime = :mtime where parent = :def")
+            .setParameter("esc", esc)
+            .setLong("mtime", System.currentTimeMillis())
+            .setParameter("def", def)
+            .executeUpdate();
+    }
+
 }
