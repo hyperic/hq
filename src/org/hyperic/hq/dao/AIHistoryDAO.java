f7843766e3955ced9ffae7d1bbd05bab9b7836d9
jhickey <jhickey@572de364-d518-0410-a359-afb9c36ce0ec>
2009-06-29 21:21:17 +0000

    HHQ-3195 Not really but testing CI/Fisheye integration.  Dummy change.

--- src/org/hyperic/hq/dao/AIHistoryDAO.java
+++ src/org/hyperic/hq/dao/AIHistoryDAO.java
@@ -31,6 +31,7 @@ import org.hyperic.hq.autoinventory.ScanConfigurationCore;
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA.
+ * Testing
  */
 
 public class AIHistoryDAO extends HibernateDAO
