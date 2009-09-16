40d58bd873bd980b59d581b5d59b470d57f5be83
dcrutchf <dcrutchf@572de364-d518-0410-a359-afb9c36ce0ec>
2009-09-16 22:06:53 +0000

    adding serial version uid

--- src/org/hyperic/util/config/InstallConfigOption.java
+++ src/org/hyperic/util/config/InstallConfigOption.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InstallConfigOption extends ConfigOption implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
     private List _values = new ArrayList(); // Values the enum holds
     
     /**
