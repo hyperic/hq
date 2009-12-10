10dea79ce7c26397bb2d9fe36d69902af548976a
Jennifer Hickey <jennifer.hickey@springsource.com>
2009-12-10 10:59:56 -0800

    HE-262 Added registration of JBossMailService MBean to EmailManagerImpl

--- hq-web/src/main/java/org/hyperic/hq/bizapp/server/session/EmailManagerImpl.java
+++ hq-web/src/main/java/org/hyperic/hq/bizapp/server/session/EmailManagerImpl.java
@@ -25,32 +25,71 @@
 
 package org.hyperic.hq.bizapp.server.session;
 
+import javax.annotation.PostConstruct;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hyperic.hq.appdef.shared.AppdefEntityID;
 import org.hyperic.hq.bizapp.server.action.email.EmailFilter;
 import org.hyperic.hq.bizapp.server.action.email.EmailRecipient;
 import org.hyperic.hq.bizapp.shared.EmailManager;
 import org.hyperic.hq.context.Bootstrap;
+import org.jboss.mail.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * This SessionEJB is used to ensure that EmailFilter does not fail since it
- * requires an associated session.
- * Class uses transaction type NotSupported so that callers don't get hung up on
- * Email since it is an I/O operation.
+ * requires an associated session. Class uses transaction type NotSupported so
+ * that callers don't get hung up on Email since it is an I/O operation.
  */
 @Service
 @Transactional
 // Really? TODO
 public class EmailManagerImpl implements EmailManager {
+
+    private final Log log = LogFactory.getLog(EmailManagerImpl.class.getName());
+    private MBeanServer server;
+
+    @Autowired
+    public EmailManagerImpl(MBeanServer server) {
+        this.server = server;
+    }
+
+    @PostConstruct
+    public void initJBossMailServer() {
+        MailService mailService = new MailService();
+        mailService.setJNDIName("java:/SpiderMail");
+        mailService.setUser("EAM Application");
+        mailService.setPassword("password");
+        try {
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(
+                getClass().getClassLoader().getResourceAsStream("jboss-mail-config.xml"));
+            Element config = (Element) document.getFirstChild();
+            mailService.setConfiguration(config);
+        } catch (Exception e) {
+            log.error("Error parsing JBoss mail service configuration.  Unable to create mail service.", e);
+            return;
+        }
+        try {
+            server.registerMBean(mailService, new ObjectName("jboss:service=SpiderMail"));
+        } catch (Exception e) {
+            log.error("Error registering JBoss mail service MBean", e);
+        }
+
+    }
+
     /**
      */
-    public void sendAlert(EmailFilter filter, AppdefEntityID appEnt,
-                          EmailRecipient[] addresses, String subject,
-                          String[] body, String[] htmlBody, int priority,
-                          boolean filterNotifications) {
-        filter.sendAlert(appEnt, addresses, subject, body, htmlBody, priority,
-                         filterNotifications);
+    public void sendAlert(EmailFilter filter, AppdefEntityID appEnt, EmailRecipient[] addresses, String subject,
+                          String[] body, String[] htmlBody, int priority, boolean filterNotifications) {
+        filter.sendAlert(appEnt, addresses, subject, body, htmlBody, priority, filterNotifications);
     }
 
     public static EmailManager getOne() {
