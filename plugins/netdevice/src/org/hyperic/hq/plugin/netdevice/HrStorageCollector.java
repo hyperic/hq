a216b901fdd0de0a417cd1b8405346f53a2cd21f
trader <trader@572de364-d518-0410-a359-afb9c36ce0ec>
2010-01-06 20:10:23 +0000

    Same as revision 9853, reformatted with project settings

--- plugins/netdevice/src/org/hyperic/hq/plugin/netdevice/HrStorageCollector.java
+++ plugins/netdevice/src/org/hyperic/hq/plugin/netdevice/HrStorageCollector.java
@@ -1,29 +1,18 @@
 /*
- * 'HrStorageCollector.java'
- *
- *
- * NOTE: This copyright does *not* cover user programs that use HQ
- * program services by normal system calls through the application
- * program interfaces provided as part of the Hyperic Plug-in Development
- * Kit or the Hyperic Client Development Kit - this is merely considered
- * normal use of the program, and does *not* fall under the heading of
- * "derived work".
- * 
- * Copyright (C) [2004, 2005, 2006, 2007, 2008, 2009], Hyperic, Inc.
- * This file is part of HQ.
- * 
- * HQ is free software; you can redistribute it and/or modify
- * it under the terms version 2 of the GNU General Public License as
- * published by the Free Software Foundation. This program is distributed
- * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA.
+ * NOTE: This copyright does *not* cover user programs that use HQ program
+ * services by normal system calls through the application program interfaces
+ * provided as part of the Hyperic Plug-in Development Kit or the Hyperic Client
+ * Development Kit - this is merely considered normal use of the program, and
+ * does *not* fall under the heading of "derived work". Copyright (C) [2004,
+ * 2005, 2006], Hyperic, Inc. This file is part of HQ. HQ is free software; you
+ * can redistribute it and/or modify it under the terms version 2 of the GNU
+ * General Public License as published by the Free Software Foundation. This
+ * program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
 package org.hyperic.hq.plugin.netdevice;
@@ -33,63 +22,46 @@ import java.util.Map;
 import org.hyperic.snmp.SNMPSession;
 import org.hyperic.snmp.SNMPValue;
 
-public class HrStorageCollector extends SNMPCollector
+public class HrStorageCollector
+    extends SNMPCollector
 {
-   private static final String SIZE  = "hrStorageSize";
-   private static final String USED  = "hrStorageUsed";
-   private static final String UNITS = "hrStorageAllocationUnits";
 
-   public static class HrUnitsConverter implements ColumnValueConverter
-   {
-      private Map _units;
+    private static final String SIZE = "hrStorageSize";
+    private static final String USED = "hrStorageUsed";
+    private static final String UNITS = "hrStorageAllocationUnits";
 
-      public HrUnitsConverter ( Map units )
-      {
-         _units = units;
-      }
+    public static class HrUnitsConverter implements ColumnValueConverter {
 
-      //
-      // Convert value to bytes...
-      // See HOST-RESOURCES-MIB::hrStorageAllocationUnits
-      //
-      public double convert ( String    index,
-                              SNMPValue value ) throws Exception
-      {
-         double val = value.toLong ( );
+        private Map _units;
 
-            SNMPValue unit = (SNMPValue)_units.get ( index );
+        public HrUnitsConverter(Map units) {
+            _units = units;
+        }
 
-            if ( unit != null )
-            {
-               val *= unit.toLong ( );
+        // convert value to bytes
+        // see HOST-RESOURCES-MIB::hrStorageAllocationUnits
+        public double convert(String index, SNMPValue value) throws Exception {
+            double val = value.toLong();
+            SNMPValue unit = (SNMPValue) _units.get(index);
+            if (unit != null) {
+                val *= unit.toLong();
             }
+            return val;
+        }
+    }
 
-         return val;
-      }
-   }
-
-   public void collect ( )
-   {
-      String columnName = getColumnName ( );
-
-      if ( columnName.equals ( SIZE ) || columnName.equals ( USED ) )
-      {
-         try
-         {
-            SNMPSession session = getSession ( );
-
-            Map units = getIndexedColumn ( session, UNITS, false );
-
-            collectIndexedColumn ( new HrUnitsConverter ( units ) );
-         }
-         catch ( Exception e )
-         {
-            return;
-         }
-      }
-      else
-      {
-         collectIndexedColumn ( );
-      }
-   }
+    public void collect() {
+        String columnName = getColumnName();
+        if (columnName.equals(SIZE) || columnName.equals(USED)) {
+            try {
+                SNMPSession session = getSession();
+                Map units = getIndexedColumn(session, UNITS, false);
+                collectIndexedColumn(new HrUnitsConverter(units));
+            } catch (Exception e) {
+                return;
+            }
+        } else {
+            collectIndexedColumn();
+        }
+    }
 }
