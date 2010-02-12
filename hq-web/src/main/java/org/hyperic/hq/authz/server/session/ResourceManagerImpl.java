970434814a0aaa45a92dd11d0900f0745988ab86
Patrick Nguyen <patrickn@vmware.com>
2010-02-12 09:41:11 -0800

    [HE-401] Fixed conversion/typo issue when the ResourceManager and AppdefBoss were converted from an EJB into a POJO

--- hq-web/src/main/java/org/hyperic/hq/authz/server/session/ResourceManagerImpl.java
+++ hq-web/src/main/java/org/hyperic/hq/authz/server/session/ResourceManagerImpl.java
@@ -710,7 +710,7 @@ public class ResourceManagerImpl implements ResourceManager {
 
         // TODO: Move filtering into QL
         ArrayList<Resource> ordResources = new ArrayList<Resource>(resources.size());
-        for (Resource res : ordResources) {
+        for (Resource res : resources) {
             if (StringUtil.stringDoesNotExist(res.getName(), resourceName)) {
                 continue;
             }
