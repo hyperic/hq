4757b001b831ce913493e2a16f33a363a29f5927
Worker Bee <workerb@qaxen01.(none)>
2010-02-18 09:47:24 -0800

    Updating multiagent to work with evolution

---
+++ tools/agentpork/forkagents.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+. etc/multiagent.properties
+
+for ((i=0;i<$NUM_CLONES;))
+do
+	for ((j=0;j<5;j++))
+	do
+		echo "cloning agent ------------> " $i
+		./create_clone.sh $i
+		i=$(($i + 1))
+		sleep 1
+	done
+	sleep 30
+        for ((j=5;j>0;j--))
+        do
+		k=$(($i - $j))
+                echo "stopping agent ------------> " $k
+                ./stop_clone.sh $k
+                sleep 1
+        done
+	sleep 5
+done
