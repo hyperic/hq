9b0cf5ac12305d9f57ae616e9439a49bf5bd6457
Jennifer Hickey <jennifer.hickey@springsource.com>
2010-03-04 17:12:23 -0800

    HE-505 final tweaks

--- hq-bootstrap/src/main/java/org/hyperic/bootstrap/HQServer.java
+++ hq-bootstrap/src/main/java/org/hyperic/bootstrap/HQServer.java
@@ -2,9 +2,7 @@ package org.hyperic.bootstrap;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
