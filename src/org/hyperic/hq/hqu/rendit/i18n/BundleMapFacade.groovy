d403d4eaa38f37fc7aea5d506e33696acf6277b9
dcrutchf <dcrutchf@572de364-d518-0410-a359-afb9c36ce0ec>
2010-03-17 23:07:21 +0000

    Added a method for parameterized i18n strings

--- src/org/hyperic/hq/hqu/rendit/i18n/BundleMapFacade.groovy
+++ src/org/hyperic/hq/hqu/rendit/i18n/BundleMapFacade.groovy
@@ -1,5 +1,7 @@
 package org.hyperic.hq.hqu.rendit.i18n
 
+import java.text.MessageFormat;
+
 /**
  * This class wraps a resource bundle and implements methods which make it
  * usable with the subscript operator (getAt) and also as object
@@ -7,14 +9,18 @@ package org.hyperic.hq.hqu.rendit.i18n
  */
 class BundleMapFacade {
     private ResourceBundle bundle
-    
+    private MessageFormat formatter
+	
     BundleMapFacade(bundle) {
         this.bundle = bundle
+		this.formatter = new MessageFormat("");
+		
+		this.formatter.setLocale(bundle.locale);
     }
     
     def getAt(String o) {
         try {
-            return bundle.getString(o)
+            return this.bundle.getString(o)
         } catch(MissingResourceException e) {
             return "MISSING i18n KEY: ${o.toString()}"
         }
@@ -23,4 +29,14 @@ class BundleMapFacade {
     def getProperty(String propName) {
         getAt(propName)
     }
+	
+	def getFormattedMessage(String key, Object... arguments) {
+		try {
+			this.formatter.applyPattern(this.bundle.getString(key));
+			
+		    return this.formatter.format(arguments);
+		} catch(MissingResourceException e) {
+		    return "MISSING i18n KEY: ${o.toString()}"
+		}
+	}
 }
