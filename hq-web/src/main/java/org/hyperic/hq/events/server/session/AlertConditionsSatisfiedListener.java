79d68295d79978f8eb5fd62fc372f94f186b2015
Jennifer Hickey <jennifer.hickey@springsource.com>
2010-04-09 17:39:24 -0700

    [HQ-1905] [HHQ-3783] [HHQ-3784] fixed deadlock bug while processing escalations Merge

--- hq-web/src/main/java/org/hyperic/hq/events/server/session/AlertConditionsSatisfiedListener.java
+++ hq-web/src/main/java/org/hyperic/hq/events/server/session/AlertConditionsSatisfiedListener.java
@@ -6,12 +6,15 @@ import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hyperic.hq.events.shared.AlertManager;
 import org.hyperic.hq.measurement.server.session.AlertConditionsSatisfiedZEvent;
 import org.hyperic.hq.stats.ConcurrentStatsCollector;
 import org.hyperic.hq.zevents.ZeventEnqueuer;
 import org.hyperic.hq.zevents.ZeventListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Component;
 
 /**
@@ -24,6 +27,8 @@ import org.springframework.stereotype.Component;
 public class AlertConditionsSatisfiedListener implements ZeventListener<AlertConditionsSatisfiedZEvent> {
     private AlertManager alertManager;
     private ZeventEnqueuer zEventManager;
+    private static final int MAX_RETRIES = 3;
+    private final Log log = LogFactory.getLog(AlertConditionsSatisfiedListener.class);
 
     @Autowired
     public AlertConditionsSatisfiedListener(AlertManager alertManager, ZeventEnqueuer zEventManager) {
@@ -43,7 +48,26 @@ public class AlertConditionsSatisfiedListener implements ZeventListener<AlertCon
     public void processEvents(List<AlertConditionsSatisfiedZEvent> events) {
         final long start = System.currentTimeMillis();
         for (AlertConditionsSatisfiedZEvent z : events) {
-            alertManager.fireAlert(z);
+             // HQ-1905 need to retry due to potential StaleStateExceptions
+             for (int i=0; i<MAX_RETRIES; i++) {
+                 try {
+                     alertManager.fireAlert(z);
+                     break;
+                 } catch (OptimisticLockingFailureException e) {
+                     if ((i+1) < MAX_RETRIES) {
+                         String times = (MAX_RETRIES - i == 1) ? "time" : "times";
+                         log.warn("Warning, exception occurred while running fireAlert.  will retry "
+                                                    + (MAX_RETRIES - (i+1)) + " more " + times + ".  errorMsg: " + e);
+                         continue;
+                     } else {
+                         log.error("fireAlert threw an Exception, will not be retried",e);
+                         break;
+                     }
+                 }catch(Throwable t) {
+                     log.error("fireAlert threw an Exception, will not be retried",t);
+                     break;
+                 }
+             }
         }
        
         ConcurrentStatsCollector.getInstance().addStat(
