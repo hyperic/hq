81e3792ac3a27fed83b15add346deba73110af5e
glaullon <administrator@glaullon-mbpro.local>
2010-06-25 10:36:34 -0700

    remove file

--- hq-plugin/gemfire-plugin/src/main/java/com/vmware/springsource/hyperic/plugin/gemfire/GemFireDetector.java
+++ hq-plugin/gemfire-plugin/src/main/java/com/vmware/springsource/hyperic/plugin/gemfire/GemFireDetector.java
@@ -31,8 +31,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hyperic.hq.product.jmx.MxServerDetector;
