82f239c3db80defc8a3615d7d466d88e7ac9c8d6
Jennifer Hickey <jennifer.hickey@springsource.com>
2010-07-19 14:32:55 -0700

    Unit test now passes (minus validation)

--- hq-plugin/vsphere-plugin/src/test/java/org/hyperic/hq/plugin/vsphere/VMAndHostVCenterPlatformDetectorTest.java
+++ hq-plugin/vsphere-plugin/src/test/java/org/hyperic/hq/plugin/vsphere/VMAndHostVCenterPlatformDetectorTest.java
@@ -1,5 +1,6 @@
 package org.hyperic.hq.plugin.vsphere;
 
+import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Properties;
 
@@ -19,13 +20,40 @@ import org.hyperic.hq.hqapi1.types.ResourcePrototypeResponse;
 import org.hyperic.hq.hqapi1.types.ResourcesResponse;
 import org.hyperic.hq.hqapi1.types.ResponseStatus;
 import org.hyperic.hq.hqapi1.types.StatusResponse;
+import org.hyperic.hq.product.PluginException;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.vmware.vim25.AboutInfo;
+import com.vmware.vim25.GuestInfo;
+import com.vmware.vim25.HostConfigInfo;
+import com.vmware.vim25.HostDnsConfig;
+import com.vmware.vim25.HostHardwareSummary;
+import com.vmware.vim25.HostIpConfig;
+import com.vmware.vim25.HostIpRouteConfig;
+import com.vmware.vim25.HostListSummary;
+import com.vmware.vim25.HostNetworkInfo;
 import com.vmware.vim25.HostRuntimeInfo;
+import com.vmware.vim25.HostSystemPowerState;
+import com.vmware.vim25.HostVirtualNic;
+import com.vmware.vim25.HostVirtualNicSpec;
+import com.vmware.vim25.InvalidProperty;
+import com.vmware.vim25.RuntimeFault;
+import com.vmware.vim25.ToolsConfigInfo;
+import com.vmware.vim25.VirtualHardware;
+import com.vmware.vim25.VirtualMachineConfigInfo;
+import com.vmware.vim25.VirtualMachineFileInfo;
+import com.vmware.vim25.VirtualMachinePowerState;
+import com.vmware.vim25.VirtualMachineRuntimeInfo;
 import com.vmware.vim25.mo.HostSystem;
 import com.vmware.vim25.mo.ManagedEntity;
-
+import com.vmware.vim25.mo.ResourcePool;
+import com.vmware.vim25.mo.VirtualMachine;
+/**
+ * Unit test of the {@link VMAndHostVCenterPlatformDetector}
+ * @author jhickey
+ *
+ */
 public class VMAndHostVCenterPlatformDetectorTest {
 
     private HQApi hqApi;
@@ -48,8 +76,16 @@ public class VMAndHostVCenterPlatformDetectorTest {
     private VMAndHostVCenterPlatformDetector detector;
 
     private Properties props = new Properties();
-    
+
     private HostSystem host;
+    
+    private ManagedEntity dataCenter;
+    
+    private ManagedEntity dataCenters;
+    
+    private VirtualMachine vm;
+    
+    private ResourcePool resourcePool;
 
     @Before
     public void setUp() throws Exception {
@@ -63,6 +99,10 @@ public class VMAndHostVCenterPlatformDetectorTest {
         this.resourceEdgeApi = EasyMock.createMock(ResourceEdgeApi.class);
         this.vim = EasyMock.createMock(VSphereUtil.class);
         this.host = EasyMock.createMock(HostSystem.class);
+        this.dataCenter = EasyMock.createMock(ManagedEntity.class);
+        this.dataCenters = EasyMock.createMock(ManagedEntity.class);
+        this.vm = EasyMock.createMock(VirtualMachine.class);
+        this.resourcePool = EasyMock.createMock(ResourcePool.class);
         this.detector = new VMAndHostVCenterPlatformDetector();
     }
 
@@ -96,11 +136,12 @@ public class VMAndHostVCenterPlatformDetectorTest {
         EasyMock.expect(
             resourceApi.getResourcePrototype(VMAndHostVCenterPlatformDetector.HOST_TYPE))
             .andReturn(hostProtoResponse).times(2);
+
+        createHost();
+        createVM();
         
-        EasyMock.expect(vim.find(VSphereUtil.HOST_SYSTEM)).andReturn(new ManagedEntity[] {host});
-        HostRuntimeInfo hostInfo = new HostRuntimeInfo();
-        EasyMock.expect(host.getRuntime()).andReturn(hostInfo);
-        EasyMock.expect(vim.findByUuid(VSphereUtil.HOST_SYSTEM, "myPlat.testEsx")).andThrow(new ManagedEntityNotFoundException("Not Found"));
+        EasyMock.expect(vim.findByUuid(VSphereUtil.HOST_SYSTEM, "myPlat.testEsx")).andThrow(
+            new ManagedEntityNotFoundException("Not Found"));
 
         ResourcePrototypeResponse vmProtoResponse = new ResourcePrototypeResponse();
         vmProtoResponse.setStatus(ResponseStatus.SUCCESS);
@@ -193,12 +234,85 @@ public class VMAndHostVCenterPlatformDetectorTest {
         response.setStatus(ResponseStatus.SUCCESS);
         return response;
     }
+    
+    private void createHost() throws PluginException {
+        EasyMock.expect(vim.find(VSphereUtil.HOST_SYSTEM)).andReturn(new ManagedEntity[] { host });
+        HostRuntimeInfo hostInfo = new HostRuntimeInfo();
+        hostInfo.setPowerState(HostSystemPowerState.poweredOn);
+        EasyMock.expect(host.getRuntime()).andReturn(hostInfo);
+        HostConfigInfo hostConfig = new HostConfigInfo();
+        HostNetworkInfo networkInfo = new HostNetworkInfo();
+        HostIpRouteConfig ipRouteConfig = new HostIpRouteConfig();
+        ipRouteConfig.setDefaultGateway("1.2.3.4");
+        networkInfo.setIpRouteConfig(ipRouteConfig);
+        HostDnsConfig dnsConfig = new HostDnsConfig();
+        
+        networkInfo.setDnsConfig(dnsConfig);
+        HostVirtualNic nic = new HostVirtualNic();
+        
+        HostVirtualNicSpec nicSpec = new HostVirtualNicSpec();
+        nic.setSpec(nicSpec);
+        HostIpConfig ipConfig = new HostIpConfig();
+        ipConfig.setIpAddress("10.0.1.123");
+        ipConfig.setSubnetMask("10.0.1.0");
+
+        nicSpec.setIp(ipConfig);
+        nicSpec.setMac("65.7.8.9");
+        networkInfo.setVnic(new HostVirtualNic[] { nic });
+        AboutInfo aboutInfo = new AboutInfo();
+        aboutInfo.setFullName("ESX Host 4.1");
+        hostConfig.setNetwork(networkInfo);
+        hostConfig.setProduct(aboutInfo);
+        HostListSummary hostListSummary = new HostListSummary();
+        HostHardwareSummary hardware = new HostHardwareSummary();
+        hardware.setUuid("1234-5678-9012-34");
+        hardware.setNumCpuCores((short)4);
+        hardware.setNumCpuPkgs((short)2);
+        hostListSummary.setHardware(hardware);
+        EasyMock.expect(host.getConfig()).andReturn(hostConfig);
+        EasyMock.expect(host.getSummary()).andReturn(hostListSummary);
+        EasyMock.expect(host.getName()).andReturn("esxHost123").times(2);
+        EasyMock.expect(host.getParent()).andReturn(dataCenter);
+        EasyMock.expect(dataCenter.getName()).andReturn("Test DataCenter").times(2);
+        EasyMock.expect(dataCenter.getParent()).andReturn(dataCenters);
+        EasyMock.expect(dataCenters.getName()).andReturn("Datacenters");
+        EasyMock.expect(dataCenters.getParent()).andReturn(null);
+    }
+    
+    private void createVM() throws InvalidProperty, RuntimeFault, RemoteException {
+        EasyMock.expect(host.getVms()).andReturn(new VirtualMachine[]  {vm});
+        VirtualMachineConfigInfo config = new VirtualMachineConfigInfo();
+        config.setTemplate(false);
+        config.setUuid("4206c445-7eb4-a72f-38c8-d865c4c374f2");
+        config.setName("Test VM");
+        config.setGuestFullName("Linux OS");
+        config.setVersion("8.1");
+        VirtualMachineFileInfo fileInfo = new VirtualMachineFileInfo();
+        fileInfo.setVmPathName("/pathto/vm");
+        config.setFiles(fileInfo);
+        VirtualHardware hardware = new VirtualHardware();
+        hardware.setNumCPU(4);
+        hardware.setMemoryMB(1000);
+        config.setHardware(hardware);
+        ToolsConfigInfo toolsConfig = new ToolsConfigInfo();
+        toolsConfig.setToolsVersion(4);
+        config.setTools(toolsConfig);
+        EasyMock.expect(resourcePool.getPropertyByPath("name")).andReturn("Test vApp");
+        VirtualMachineRuntimeInfo runtime = new VirtualMachineRuntimeInfo();
+        runtime.setPowerState(VirtualMachinePowerState.poweredOn);
+        GuestInfo guestInfo = new GuestInfo();
+        guestInfo.setHostName("leela.local");
+        EasyMock.expect(vm.getConfig()).andReturn(config);
+        EasyMock.expect(vm.getRuntime()).andReturn(runtime);
+        EasyMock.expect(vm.getGuest()).andReturn(guestInfo);
+        EasyMock.expect(vm.getResourcePool()).andReturn(resourcePool);
+    }
 
     private void replay() {
-        EasyMock.replay(hqApi, resourceApi, agentApi, resourceEdgeApi, vim, host);
+        EasyMock.replay(hqApi, resourceApi, agentApi, resourceEdgeApi, vim, host, dataCenter, dataCenters, vm, resourcePool);
     }
 
     private void verify() {
-        EasyMock.verify(hqApi, resourceApi, agentApi, resourceEdgeApi, vim, host);
+        EasyMock.verify(hqApi, resourceApi, agentApi, resourceEdgeApi, vim, host, dataCenter, dataCenters, vm, resourcePool);
     }
 }
