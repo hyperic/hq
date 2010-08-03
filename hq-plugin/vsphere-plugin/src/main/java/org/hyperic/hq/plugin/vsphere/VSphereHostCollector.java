ba0d4fa7d539f74392913295065baa31ee203542
Jennifer Hickey <jennifer.hickey@springsource.com>
2010-08-02 17:47:40 -0700

    Merged July 21 log level change

--- hq-plugin/vsphere-plugin/src/main/java/org/hyperic/hq/plugin/vsphere/VSphereHostCollector.java
+++ hq-plugin/vsphere-plugin/src/main/java/org/hyperic/hq/plugin/vsphere/VSphereHostCollector.java
@@ -181,7 +181,7 @@ public class VSphereHostCollector extends VSphereCollector {
         PerfMetricId[] ids = getPerfMetricIds(perfManager, mor);
 
         if (ids == null || ids.length == 0) {
-            _log.warn("No available performance metrics for "
+            _log.info("No available performance metrics for "
                + getType() + "[name=" + getName()
                + "]");
             return;
@@ -199,7 +199,7 @@ public class VSphereHostCollector extends VSphereCollector {
             perfManager.queryPerf(query);
 
         if (values == null) {
-            _log.warn("No performance metrics for "
+            _log.info("No performance metrics for "
                 + getType() + "[name=" + getName()
                 + ", refreshRate=" + refreshRate
                 + ", availablePerfMetric=" + ids.length
