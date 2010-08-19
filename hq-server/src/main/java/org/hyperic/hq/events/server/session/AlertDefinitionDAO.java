4972dc9c894cbfd311c4067fd673bc3257fd9b13
Patrick Nguyen <patrickn@vmware.com>
2010-08-19 15:59:03 -0700

    [HQ-2563] Get "active", instead of "enabled", alert definitions.  Increase the max cache size for AvailabilityDownAlertDefinitionCache.

--- hq-server/src/main/java/org/hyperic/hq/events/server/session/AlertDefinitionDAO.java
+++ hq-server/src/main/java/org/hyperic/hq/events/server/session/AlertDefinitionDAO.java
@@ -1,15 +1,15 @@
 /*
- * NOTE: This copyright does *not* cover user programs that use HQ
+ * NOTE: This copyright does *not* cover user programs that use Hyperic
  * program services by normal system calls through the application
  * program interfaces provided as part of the Hyperic Plug-in Development
  * Kit or the Hyperic Client Development Kit - this is merely considered
  * normal use of the program, and does *not* fall under the heading of
  * "derived work".
  *
- * Copyright (C) [2004-2008], Hyperic, Inc.
- * This file is part of HQ.
+ * Copyright (C) [2004-2010], VMware, Inc.
+ * This file is part of Hyperic.
  *
- * HQ is free software; you can redistribute it and/or modify
+ * Hyperic is free software; you can redistribute it and/or modify
  * it under the terms version 2 of the GNU General Public License as
  * published by the Free Software Foundation. This program is distributed
  * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
@@ -194,7 +194,7 @@ public class AlertDefinitionDAO
             .append("join fetch ad.resource rez ")
             .append("join fetch rez.resourceType ")
             .append("join fetch ad.conditionsBag c ")
-            .append("where ad.enabled = true ")
+            .append("where ad.active = true ")
             .append("and ad.deleted = false ")
             .append("and c.name = 'Availability' ")
             .append("and (ad.parent is null or ad.parent.id != 0) ")
