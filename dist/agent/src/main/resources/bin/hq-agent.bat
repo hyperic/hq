8d53781792bf2f8e13c96e820e0cd13642114200
Patrick Nguyen <patrickn@vmware.com>
2010-09-16 13:34:01 -0700

    [HQ-2621] Fix syntax issue with startup script for agents with built-in JRE
    Former-commit-id: 29a0c63c5262f81cfa7c7736eec7fb90f25c9dca

--- dist/agent/src/main/resources/bin/hq-agent.bat
+++ dist/agent/src/main/resources/bin/hq-agent.bat
@@ -27,7 +27,7 @@ cd %_REALPATH%
 
 if not "%HQ_JAVA_HOME%"=="" goto gothqjava
 
-if EXIST "%AGENT_INSTALL_HOME%\jre" (
+if EXIST "%AGENT_INSTALL_HOME%"\jre (
     set HQ_JAVA_HOME=%AGENT_INSTALL_HOME%\jre
     goto gotjava
 )
