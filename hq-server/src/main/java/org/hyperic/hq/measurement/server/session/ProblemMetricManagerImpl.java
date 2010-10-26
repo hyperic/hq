51a9a98cb79d66ef7f3cbaa0cedb9b391148cd0a
Jennifer Hickey <jennifer.hickey@springsource.com>
2010-10-26 09:54:47 -0700

    Removed Java 6 Overrides until we can compile at 1.6 compliance (to generate eclipse metadata)

--- hq-server/src/main/java/org/hyperic/hq/measurement/server/session/ProblemMetricManagerImpl.java
+++ hq-server/src/main/java/org/hyperic/hq/measurement/server/session/ProblemMetricManagerImpl.java
@@ -41,49 +41,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProblemMetricManagerImpl implements ProblemMetricManager {
 
-    @Override
+    
     public void createProblem(Integer mid, long time, int type, Integer additional) {
     }
 
-    @Override
+    
     public MetricProblem getByIdAndTimestamp(Measurement meas, long timestamp) {
         return null;
     }
 
-    @Override
+    
     public ProblemMetricInfo[] getProblemMetrics(AppdefEntityID aid, long begin, long end) {
         return new ProblemMetricInfo[0];
     }
 
-    @Override
+    
     public ProblemResourceInfo[] getProblemResources(long begin, long end,
                                                      Set<AppdefEntityID> permitted,
                                                      PageControl pc) {
         return new ProblemResourceInfo[0];
     }
 
-    @Override
+    
     public ProblemResourceInfo[] getProblemResourcesByTypeAndInstances(
         int appdefType, int[] instanceIds, long begin, long end, PageControl pc) {
         return new ProblemResourceInfo[0];
     }
 
     @SuppressWarnings("unchecked")
-    @Override
+    
     public Map<Integer, ProblemMetricInfo> getProblemsByTemplate(
         int appdefType, Integer[] eids, long begin, long end) {
         return Collections.emptyMap();
     }
 
-    @Override
+    
     public void processMetricValue(Integer mid, MetricValue mv, int type) {
     }
 
-    @Override
+    
     public void removeProblems(Collection<Integer> mids) {
     }
 
-    @Override
+    
     public void removeProblems(AppdefEntityID entityId) {
     }
 
