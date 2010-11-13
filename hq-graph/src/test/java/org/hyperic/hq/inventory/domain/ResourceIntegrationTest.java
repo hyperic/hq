b09d98d493a92bbb108ffe8f2ddec4a3a3d2834c
Jennifer Hickey <jennifer.hickey@springsource.com>
2010-11-12 17:41:05 -0800

    Fixed integration tests
    Former-commit-id: 6c6fbacf4563b6363a82c8cf0b34e534797eafa3

--- hq-graph/src/test/java/org/hyperic/hq/inventory/domain/ResourceIntegrationTest.java
+++ hq-graph/src/test/java/org/hyperic/hq/inventory/domain/ResourceIntegrationTest.java
@@ -1,13 +1,36 @@
 package org.hyperic.hq.inventory.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 import org.hyperic.hq.inventory.domain.Resource;
 import org.junit.Test;
 
 @RooIntegrationTest(entity = Resource.class)
 public class ResourceIntegrationTest {
+    
+    @Autowired
+    private ResourceDataOnDemand dod;
 
     @Test
     public void testMarkerMethod() {
+    }
+
+    @Test
+    public void testPersist() {
+        org.junit.Assert.assertNotNull(
+            "Data on demand for 'Resource' failed to initialize correctly",
+            dod.getRandomResource());
+        org.hyperic.hq.inventory.domain.Resource obj = dod
+            .getNewTransientResource(Integer.MAX_VALUE);
+        org.junit.Assert.assertNotNull(
+            "Data on demand for 'Resource' failed to provide a new transient entity", obj);
+        // Neo4J assigns an ID with the Resource constructor is called
+        // org.junit.Assert.assertNull("Expected 'Resource' identifier to be null",
+        // obj.getId());
+        // Neo4jEntityManager persist doesn't do anything
+        obj.persist();
+        obj.flush();
+        org.junit.Assert.assertNotNull("Expected 'Resource' identifier to no longer be null",
+            obj.getId());
     }
 }
--- hq-graph/src/test/java/org/hyperic/hq/plugin/domain/ResourceTypeIntegrationTest.java
+++ hq-graph/src/test/java/org/hyperic/hq/plugin/domain/ResourceTypeIntegrationTest.java
@@ -1,13 +1,31 @@
 package org.hyperic.hq.plugin.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 import org.hyperic.hq.plugin.domain.ResourceType;
 import org.junit.Test;
 
 @RooIntegrationTest(entity = ResourceType.class)
 public class ResourceTypeIntegrationTest {
+    
+    @Autowired
+    private ResourceTypeDataOnDemand dod;
 
     @Test
     public void testMarkerMethod() {
+    }
+
+	@Test
+    public void testPersist() {
+        org.junit.Assert.assertNotNull("Data on demand for 'ResourceType' failed to initialize correctly", dod.getRandomResourceType());
+        org.hyperic.hq.plugin.domain.ResourceType obj = dod.getNewTransientResourceType(Integer.MAX_VALUE);
+        org.junit.Assert.assertNotNull("Data on demand for 'ResourceType' failed to provide a new transient entity", obj);
+        // Neo4J assigns an ID with the Resource constructor is called
+        // org.junit.Assert.assertNull("Expected 'Resource' identifier to be null",
+        // obj.getId());
+        //org.junit.Assert.assertNull("Expected 'ResourceType' identifier to be null", obj.getId());
+        obj.persist();
+        obj.flush();
+        org.junit.Assert.assertNotNull("Expected 'ResourceType' identifier to no longer be null", obj.getId());
     }
 }
