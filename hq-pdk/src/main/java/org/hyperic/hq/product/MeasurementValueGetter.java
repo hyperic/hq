2a79def49fc49b3e7b3bd4c31b2ac620b1573f78
Ryan Morgan <rmorgan@hyperic.com>
2010-11-18 16:47:56 -0800

    [HQ-2748] Rework ScheduleThread to use an Executor per-plugin rather than a single thread for all non-collector metrics.
    Former-commit-id: ca7ce4f68129e4d3d45c00ae2f711f3edd082057

--- hq-pdk/src/main/java/org/hyperic/hq/product/MeasurementValueGetter.java
+++ hq-pdk/src/main/java/org/hyperic/hq/product/MeasurementValueGetter.java
@@ -27,6 +27,9 @@
 package org.hyperic.hq.product;
 
 public interface MeasurementValueGetter {
+
+    public GenericPlugin getPlugin(String plugin) throws PluginNotFoundException;
+
     public MetricValue getValue(String name, Metric metric)
     throws PluginException, PluginNotFoundException,
            MetricNotFoundException, MetricUnreachableException;
