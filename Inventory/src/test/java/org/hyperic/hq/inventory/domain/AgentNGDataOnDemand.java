f67c9a56d26c46589b6ff923cd6b0a1f1bb1c1f3
David Crutchfield <dcrutchfield@vmware.com>
2010-12-10 09:57:40 -0800

    Removal of Inventory project
    Former-commit-id: 4c3174123e87d199b40ae21d48119737b62434bb

--- Inventory/src/test/java/org/hyperic/hq/inventory/domain/AgentNGDataOnDemand.java
+++
@@ -1,8 +0,0 @@
-package org.hyperic.hq.inventory.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-import org.hyperic.hq.inventory.domain.AgentNG;
-
-@RooDataOnDemand(entity = AgentNG.class)
-public class AgentNGDataOnDemand {
-}
