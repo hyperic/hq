d1fedf16925ef74675c4a9079245d3547cbfad2f
Jennifer Hickey <jennifer.hickey@springsource.com>
2010-12-13 14:03:53 -0800

    Removed old inventory project
    Former-commit-id: 36182114e13db19a288625c9322f13197f11a9e7

--- Inventory/src/test/java/org/hyperic/hq/inventory/domain/ConfigDataOnDemand.java
+++
@@ -1,8 +0,0 @@
-package org.hyperic.hq.inventory.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-import org.hyperic.hq.inventory.domain.Config;
-
-@RooDataOnDemand(entity = Config.class)
-public class ConfigDataOnDemand {
-}
--- Inventory/src/test/java/org/hyperic/hq/inventory/domain/IpNGDataOnDemand.java
+++
@@ -1,8 +0,0 @@
-package org.hyperic.hq.inventory.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-import org.hyperic.hq.inventory.domain.IpNG;
-
-@RooDataOnDemand(entity = IpNG.class)
-public class IpNGDataOnDemand {
-}
