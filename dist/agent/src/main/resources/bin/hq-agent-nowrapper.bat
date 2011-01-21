7dd14d86d18031b52c4f3cfff6f9609634fcf326
Jason Konicki <jkonicki@vmware.com>
2011-01-21 12:12:15 -0500

    HHQ-4538 Fixed the hq-agent-nowrapper.bat startup script.
    Former-commit-id: f07643e0a4b597d65586057b4c61c2998fae1a1b

--- dist/agent/src/main/resources/bin/hq-agent-nowrapper.bat
+++ dist/agent/src/main/resources/bin/hq-agent-nowrapper.bat
@@ -1,14 +1,23 @@
 @echo off
 if "%OS%" == "Windows_NT" setlocal
+setLocal EnableDelayedExpansion
 
-set PDK_LIB=pdk\lib
-set AGENT_LIB=lib
+set PDK_LIB=..\pdk\lib
+set AGENT_LIB=..\lib
+set AGENT_BUNDLE_HOME=..
+set AGENT_INSTALL_HOME=..\..\..
+set AGENT_BUNDLE_HOME_PROP=agent.bundle.home
+set AGENT_INSTALL_HOME_PROP=agent.install.home
+set WRAPPER_PATH=%LD_LIBRARY_PATH%;..\..\..\wrapper\lib
+set CLIENT_CLASSPATH_1=%AGENT_LIB%
+set CLIENT_CLASSPATH_2=
 
 rem # ------------- 
 rem # Shouldn't need to change anything below this
 rem # -------------
 
 SET RUNDIR=%~dp0
+
 cd %RUNDIR%
 if not "%HQ_JAVA_HOME%"=="" (
     set JAVA_HOME=%HQ_JAVA_HOME%
@@ -25,26 +34,30 @@ if not exist "%JAVA_HOME%\bin\java.exe" goto nojavaExe
 :gotjava
 set JAVA=%JAVA_HOME%\bin\java
 
-set CLIENT_CLASSPATH=%AGENT_LIB%\hq-agent-core-${project.version}.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%AGENT_LIB%
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\hq-common-${project.version}.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%AGENT_LIB%\hq-lather-${project.version}.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\ant-1.7.1.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\commons-logging-1.0.4.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\log4j-1.2.14.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\jdom-1.1.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\hq-util-${project.version}.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\sigar-${sigar.version}.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\bcel-5.1.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\hq-pdk-${project.version}.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\jakarta-oro-2.0.7.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\commons-httpclient-3.1.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\commons-codec-1.2.jar
-set CLIENT_CLASSPATH=%CLIENT_CLASSPATH%;%PDK_LIB%\groovy-all-1.5.jar
+
+for /F  %%a in ('dir /b %AGENT_LIB%\*.jar') do (
+  set CLIENT_CLASSPATH_1=!CLIENT_CLASSPATH_1!;%AGENT_LIB%\%%a
+)
+set CLIENT_CLASSPATH_1=!CLIENT_CLASSPATH_1!
+
+for /F  %%a in ('dir /b %PDK_LIB%\*.jar') do (
+  set CLIENT_CLASSPATH_2=!CLIENT_CLASSPATH_2!;%PDK_LIB%\%%a
+)
+set CLIENT_CLASSPATH_2=!CLIENT_CLASSPATH_2!
+
+for /F  %%a in ('dir /b %PDK_LIB%\jdbc\*.jar') do (
+  set CLIENT_CLASSPATH_3=!CLIENT_CLASSPATH_3!;%PDK_LIB%\%%a
+)
+set CLIENT_CLASSPATH_3=!CLIENT_CLASSPATH_3!
+
+for /F  %%a in ('dir /b %PDK_LIB%\mx4j\*.jar') do (
+  set CLIENT_CLASSPATH_4=!CLIENT_CLASSPATH_4!;%PDK_LIB%\%%a
+)
+set CLIENT_CLASSPATH_4=!CLIENT_CLASSPATH_4!
 
 set CLIENT_CLASS=org.hyperic.hq.bizapp.agent.client.AgentClient
 
-set CLIENT_CMD="%JAVA%" -classpath "%CLIENT_CLASSPATH%" %CLIENT_CLASS%
+set CLIENT_CMD="%JAVA%" -Dwrapper.native_library=wrapper -Djava.library.path=%java.library.path%;%WRAPPER_PATH% -D%AGENT_INSTALL_HOME_PROP%="%AGENT_INSTALL_HOME%" -D%AGENT_BUNDLE_HOME_PROP%="%AGENT_BUNDLE_HOME%" -classpath "%CLIENT_CLASSPATH_1%;%CLIENT_CLASSPATH_2%;%CLIENT_CLASSPATH_3%;%CLIENT_CLASSPATH_4%;" %CLIENT_CLASS%
 
 set START_CMD=%CLIENT_CMD% start
 set PING_CMD=%CLIENT_CMD% ping
