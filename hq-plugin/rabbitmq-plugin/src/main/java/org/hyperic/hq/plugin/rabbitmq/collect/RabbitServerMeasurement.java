ebb949b02a570c4bbad038d7ca38f176513943a9
German Laullon <glaullon@vmware.com>
2011-02-28 16:12:22 -0800

    server metrics without "process.query" prop.
    Former-commit-id: 86d2196a0e0383b7603070bd2f0a02e5444922d0

---
+++ hq-plugin/rabbitmq-plugin/src/main/java/org/hyperic/hq/plugin/rabbitmq/collect/RabbitServerMeasurement.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hyperic.hq.plugin.rabbitmq.collect;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hyperic.hq.plugin.rabbitmq.core.HypericRabbitAdmin;
+import org.hyperic.hq.plugin.rabbitmq.core.RabbitNode;
+import org.hyperic.hq.product.Collector;
+import org.hyperic.hq.product.MeasurementPlugin;
+import org.hyperic.hq.product.Metric;
+import org.hyperic.hq.product.MetricNotFoundException;
+import org.hyperic.hq.product.MetricUnreachableException;
+import org.hyperic.hq.product.MetricValue;
+import org.hyperic.hq.product.PluginException;
+import org.hyperic.hq.product.PluginManager;
+import org.hyperic.hq.product.SigarMeasurementPlugin;
+import org.hyperic.util.config.ConfigResponse;
+
+/**
+ *
+ * @author administrator
+ */
+public class RabbitServerMeasurement extends SigarMeasurementPlugin {
+
+    private static final Log logger = LogFactory.getLog(RabbitServerMeasurement.class);
+    private final static Map<String, Integer> pidsCahce = new HashMap();
+
+    @Override
+    public MetricValue getValue(Metric metric) throws PluginException, MetricNotFoundException, MetricUnreachableException {
+        MetricValue res = null;
+        if (metric.getDomainName().equals("rabbitmq-sigar")) {
+            String nName = metric.getObjectProperty("node");
+            Integer pid = pidsCahce.get(nName);
+            if (pid == null) {
+                HypericRabbitAdmin admin = new HypericRabbitAdmin(metric.getObjectProperties());
+                RabbitNode node = admin.getNode(nName);
+                pid = node.getOsPid();
+                pidsCahce.put(nName, pid);
+            }
+            Metric _metric = Metric.parse("sigar:Type=" + metric.getObjectProperty("Type") + ",Arg=Pid.Pid.eq=" + pid + ":" + metric.getAttributeName());
+            if (logger.isDebugEnabled()) {
+                logger.debug("[getValue] -> metric=" + metric);
+                logger.debug("[getValue] <- metric=" + _metric);
+            }
+            res = super.getValue(_metric);
+        } else {
+            res = Collector.getValue(this, metric);
+        }
+        return res;
+    }
+}
