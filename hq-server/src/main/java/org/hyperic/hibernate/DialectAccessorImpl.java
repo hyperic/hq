4b0963aefd7b83dad434586f56e53550ab185c88
Jennifer Hickey <jennifer.hickey@springsource.com>
2011-03-10 16:37:24 -0800

    Fixed DialectAccessor to not require an active EntityManager (for use with direct JDBC calls)
    Former-commit-id: cc28d8204963c6ca36209e6b74752cfffd8b5d44

--- hq-server/src/main/java/org/hyperic/hibernate/DialectAccessorImpl.java
+++ hq-server/src/main/java/org/hyperic/hibernate/DialectAccessorImpl.java
@@ -1,29 +1,24 @@
 package org.hyperic.hibernate;
 
-import javax.persistence.EntityManagerFactory;
-
-import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.SessionFactoryImplementor;
 import org.hyperic.hibernate.dialect.HQDialect;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.jpa.EntityManagerFactoryUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DialectAccessorImpl implements DialectAccessor {
 
     @Autowired
-    private EntityManagerFactory entityManagerFactory;
+    private SessionFactory sessionFactory;
 
     public HQDialect getHQDialect() {
         return (HQDialect) getDialect();
     }
 
     public Dialect getDialect() {
-        return ((SessionFactoryImplementor) ((Session) EntityManagerFactoryUtils
-            .getTransactionalEntityManager(entityManagerFactory).getDelegate()).getSessionFactory())
-            .getDialect();
+        return ((SessionFactoryImplementor) sessionFactory).getDialect();
     }
 
 }
