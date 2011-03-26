9d9e56098e36df455243295e6b9a96e62c179095
Jennifer Hickey <jennifer.hickey@springsource.com>
2011-03-25 18:36:50 -0700

    Repository using wrong ID type
    Former-commit-id: ef80a49becd2879ce7f490c9a607a1e71d590c4f

--- hq-server/src/main/java/org/hyperic/hq/measurement/data/MetricProblemRepository.java
+++ hq-server/src/main/java/org/hyperic/hq/measurement/data/MetricProblemRepository.java
@@ -1,8 +1,9 @@
 package org.hyperic.hq.measurement.data;
 
+import org.hyperic.hq.measurement.server.session.MeasurementDataId;
 import org.hyperic.hq.measurement.server.session.MetricProblem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MetricProblemRepository extends JpaRepository<MetricProblem, Integer> {
+public interface MetricProblemRepository extends JpaRepository<MetricProblem, MeasurementDataId> {
 
 }
