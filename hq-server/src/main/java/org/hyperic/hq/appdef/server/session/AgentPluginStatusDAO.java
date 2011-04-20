293d36ce8e3c47e3e15e714dc29428f30f3bcadc
Scott <scottmf@goku.(none)>
2011-04-20 16:32:41 -0700

    remove unused import
    Former-commit-id: f8d4ae2e78d986b23429fa882c608c1bfe6d46fb

--- hq-server/src/main/java/org/hyperic/hq/appdef/server/session/AgentPluginStatusDAO.java
+++ hq-server/src/main/java/org/hyperic/hq/appdef/server/session/AgentPluginStatusDAO.java
@@ -31,7 +31,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.hibernate.Hibernate;
 import org.hibernate.SQLQuery;
