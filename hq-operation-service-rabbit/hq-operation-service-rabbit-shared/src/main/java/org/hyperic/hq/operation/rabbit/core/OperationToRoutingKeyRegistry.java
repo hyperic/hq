7dab4cab300539938e1a8c554fd0fce94a2a7459
Helena Edelson <helena.edelson@springsource.com>
2011-04-20 19:20:19 -0600

    Completed OperationToRoutingKeyRegistry
    Former-commit-id: e82b1cb9328cad35b6cd568ae0a24c4d45921e47

--- hq-operation-service-rabbit/hq-operation-service-rabbit-shared/src/main/java/org/hyperic/hq/operation/rabbit/core/OperationToRoutingKeyRegistry.java
+++ hq-operation-service-rabbit/hq-operation-service-rabbit-shared/src/main/java/org/hyperic/hq/operation/rabbit/core/OperationToRoutingKeyRegistry.java
@@ -32,7 +32,6 @@ import org.hyperic.hq.operation.rabbit.annotation.OperationDispatcher;
 import org.hyperic.hq.operation.rabbit.annotation.OperationEndpoint;
 import org.hyperic.hq.operation.rabbit.api.BindingHandler;
 import org.hyperic.hq.operation.rabbit.api.RoutingRegistry;
-import org.hyperic.hq.operation.rabbit.connection.ChannelException;
 import org.hyperic.hq.operation.rabbit.util.MessageConstants;
 import org.hyperic.hq.operation.rabbit.util.OperationToRoutingMapping;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,9 +57,8 @@ public class OperationToRoutingKeyRegistry implements RoutingRegistry {
     private final Map<String, OperationToRoutingMapping> operationToRoutingMappings = new ConcurrentHashMap<String, OperationToRoutingMapping>();
 
     private final BindingHandler bindingHandler;
-
+    /* TODO */
     private static final String SERVER_PREFIX = "hq.server.";
-
     public static final String AGENT_PREFIX = "hq.agent.";
 
     private final String serverId;
@@ -72,37 +70,54 @@ public class OperationToRoutingKeyRegistry implements RoutingRegistry {
     }
 
     /**
-     * Delegates to the BindingHandler and registers the operation by routing mappings
-     * @param method   the operation to extract and register
-     * @param endpoint the @OperationEndpoint
-     * @throws org.hyperic.hq.operation.rabbit.connection.ChannelException
-     *
+     * Delegates to the BindingHandler then registers the operation by routing mappings
+     * @param method the operation to extract and register
      */
-    public void register(Method method, OperationEndpoint endpoint) throws ChannelException {
-        if (supports(method.getName(), endpoint.getClass())) return;
-        register(method, endpoint.exchange(), endpoint.routingKey(), endpoint.binding(), endpoint.getClass());
+    public void register(Method method) {
+        if (method.isAnnotationPresent(OperationDispatcher.class)) {
+            OperationDispatcher dispatcher = method.getAnnotation(OperationDispatcher.class);
+            if (!supports(method.getName(), dispatcher.getClass())) {
+                register(method, extractQueueName(method), dispatcher.exchange(), dispatcher.routingKey(), dispatcher.binding(), dispatcher.getClass());
+            }
+        }
+        if (method.isAnnotationPresent(OperationEndpoint.class)) {
+            OperationEndpoint endpoint = method.getAnnotation(OperationEndpoint.class);
+            if (!supports(method.getName(), endpoint.getClass())) {
+                register(method, extractQueueName(method), endpoint.exchange(), endpoint.routingKey(), endpoint.binding(), endpoint.getClass());
+            }
+        }
     }
 
     /**
-     * Delegates to the BindingHandler and registers the operation by routing mappings
+     * Delegates to the BindingHandler then registers the operation by routing mappings
      * @param method     the operation to extract and register
-     * @param dispatcher the @OperationDispatcher
-     * @throws org.hyperic.hq.operation.rabbit.connection.ChannelException
-     *
+     * @param queue      the queue name - if this is set, override the default method name as queue name
+     * @param exchange   the exchange name to declare an exchange
+     * @param routingKey the routing key for sending
+     * @param binding    the pattern to bind a queue to exchange
+     * @param annotation the annotation type
      */
-    public void register(Method method, OperationDispatcher dispatcher) throws ChannelException {
-        if (supports(method.getName(), dispatcher.getClass())) return;
-        register(method, dispatcher.exchange(), dispatcher.routingKey(), dispatcher.binding(), dispatcher.getClass());
+    private void register(Method method, String queue, String exchange, String routingKey, String binding, Class<? extends Annotation> annotation) {
+        bindingHandler.declareAndBind(queue, exchange, binding);
+
+        operationToRoutingMappings.put(method.getName(), new OperationToRoutingMapping(
+            queue, exchange, routingKey, method.getReturnType(), annotation));
+
+        logger.debug("registered method + " + method.getName() + " on bean type=" + method.getDeclaringClass());
     }
+ 
+    public String extractQueueName(Method method) {
+        if (method.isAnnotationPresent(OperationDispatcher.class)) {
+            OperationDispatcher dispatcher = method.getAnnotation(OperationDispatcher.class);
+            return hasValue(dispatcher.queue()) ? dispatcher.queue() : method.getName();
 
-
-    private void register(Method method, String exchange, String routingKey, String binding, Class<? extends Annotation> annotation) {
-        bindingHandler.declareAndBind(method.getName(), exchange, binding);
-
-        operationToRoutingMappings.put(method.getName(),
-                new OperationToRoutingMapping(method.getName(), exchange, routingKey, method.getReturnType(), annotation));
+        }
+        else if (method.isAnnotationPresent(OperationEndpoint.class)) {
+            OperationEndpoint endpoint = method.getAnnotation(OperationEndpoint.class);
+            return hasValue(endpoint.queue()) ? endpoint.queue() : method.getName();
+        }
+        return method.getName();
     }
-
 
     /**
      * @param operationName The operation's name
@@ -116,29 +131,21 @@ public class OperationToRoutingKeyRegistry implements RoutingRegistry {
     public boolean supports(String operationName, Class<? extends Annotation> annotation) {
         return operationToRoutingMappings.containsKey(createKey(operationName, annotation));
     }
-
-    /**
-     * Create the routing registry key name
-     */
+ 
     private String createKey(String operationName, Class<? extends Annotation> annotation) {
         return operationName + annotation.getSimpleName();
     }
 
+    private boolean hasValue(String entry) {
+        return entry != null && entry.length() > 0;
+    }
 
-    /* JIRA ticket in the backlog */
-
+    /* The following is related to a JIRA ticket in the backlog - work not started */
     private boolean isAuthenticatedUser(ConnectionFactory connectionFactory) {
         return !connectionFactory.getUsername().equalsIgnoreCase(MessageConstants.GUEST_USER)
                 && !connectionFactory.getPassword().equalsIgnoreCase(MessageConstants.GUEST_PASS);
     }
-
-    /*
-    id = 1302212470776-5028906219606536735-6762208433280624914
-    hq-agents.agent-{id}.operations.config.registration.request
-    new StringBuilder(SERVER_ROUTING_KEY_PREFIX + this.serverId).append(OPERATION_PREFIX).append(operation).toString());
-    new StringBuilder(AGENT_ROUTING_KEY_PREFIX).append(agentToken).append(OPERATION_PREFIX).append(operation).toString());
-    */
-
+ 
     /**
      * Returns the IP address as a String. If an error occurs getting
      * the host IP, a random UUID as String is used.
