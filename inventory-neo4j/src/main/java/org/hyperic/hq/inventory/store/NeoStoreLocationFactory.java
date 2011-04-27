4bff7bbbc21f9ca799ac26417f995dd70dbcd69c
Jennifer Hickey <jennifer.hickey@springsource.com>
2011-04-27 14:49:50 -0700

    Fixes for proper location of performance monitor aspect and NeoStore on integration tests
    Former-commit-id: 690d44aeba1d59b5268bd56501ade707e20c8c0a

--- inventory-neo4j/src/main/java/org/hyperic/hq/inventory/store/NeoStoreLocationFactory.java
+++ inventory-neo4j/src/main/java/org/hyperic/hq/inventory/store/NeoStoreLocationFactory.java
@@ -1,4 +1,4 @@
-package org.hyperic.hq.store;
+package org.hyperic.hq.inventory.store;
 
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Value;
