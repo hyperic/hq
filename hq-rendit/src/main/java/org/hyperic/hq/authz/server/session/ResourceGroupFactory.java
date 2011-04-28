7778cac100a7a2ba52267af56df00ac6fc8af4a4
Jennifer Hickey <jennifer.hickey@springsource.com>
2011-04-28 12:25:32 -0700

    Fixed issues with group creation
    Former-commit-id: 37f61b403843245692b12d9a77237b990bceba33

--- hq-rendit/src/main/java/org/hyperic/hq/authz/server/session/ResourceGroupFactory.java
+++ hq-rendit/src/main/java/org/hyperic/hq/authz/server/session/ResourceGroupFactory.java
@@ -1,18 +1,16 @@
 package org.hyperic.hq.authz.server.session;
 
-import org.hyperic.hq.auth.domain.AuthzSubject;
 
 abstract public class ResourceGroupFactory {
 
-    public static ResourceGroup create(org.hyperic.hq.inventory.domain.ResourceGroup group,
-                                       AuthzSubject owner) {
+    public static ResourceGroup create(org.hyperic.hq.inventory.domain.ResourceGroup group) {
         if(group == null) {
             return null;
         }
         ResourceGroup groupDto = new ResourceGroup();
         groupDto.setName(group.getName());
         groupDto.setId(group.getId());
-        groupDto.setResource(ResourceFactory.create(group,owner));
+        groupDto.setResource(ResourceFactory.create(group));
         //TODO other fields
         return groupDto;
     }
