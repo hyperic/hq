f81e5e8b0d0b604db1ab2d6d4dd9223b4cddfaef
Annie chen <achen@vmware.com>
2011-08-25 10:37:44 -0700

    create PluginController for prototyping plugin api
    Former-commit-id: cefbafe82643ddada6baf3cf79a9b1861bf7dd34

---
+++ hq-web/src/main/java/org/hyperic/hq/api/PluginController.java
@@ -0,0 +1,49 @@
+/**
+ * NOTE: This copyright does *not* cover user programs that use Hyperic
+ * program services by normal system calls through the application
+ * program interfaces provided as part of the Hyperic Plug-in Development
+ * Kit or the Hyperic Client Development Kit - this is merely considered
+ * normal use of the program, and does *not* fall under the heading of
+ *  "derived work".
+ *
+ *  Copyright (C) [2010], VMware, Inc.
+ *  This file is part of Hyperic.
+ *
+ *  Hyperic is free software; you can redistribute it and/or modify
+ *  it under the terms version 2 of the GNU General Public License as
+ *  published by the Free Software Foundation. This program is distributed
+ *  in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ *  even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ *  PARTICULAR PURPOSE. See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ *  USA.
+ *
+ */
+package org.hyperic.hq.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/plugin")
+public class PluginController{
+
+    @Autowired
+    public PluginController(){
+        
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value="/{id}")
+    public @ResponseBody String getById(@PathVariable int id){
+        return "here is the plugin!";
+    }
+    
+}
