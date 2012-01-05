5c8f9880da84962b82b9791f81c742eca3a1c507
Maya Anderson <andersonm@vmware.com>
2012-01-05 11:24:17 +0200

    Tidied up the code - divided into methods, replaced many returns with one, changed short parameters' names to long ones, moved public method translate() nearer to top.
    Former-commit-id: 82f063f9e48494e50abc5312fce4245d95fbc07f

--- hq-plugin/postgresql-plugin/src/main/java/org/hyperic/hq/plugin/postgresql/PostgreSQLMeasurementPlugin.java
+++ hq-plugin/postgresql-plugin/src/main/java/org/hyperic/hq/plugin/postgresql/PostgreSQLMeasurementPlugin.java
@@ -77,136 +77,155 @@ public class PostgreSQLMeasurementPlugin
         // Override JDBCMeasurementPlugin.
         return new ConfigSchema();
     }
-
-    @Override
-	protected String getQuery(Metric metric)
-    {
-        String attr = metric.getAttributeName();
-        String objectName = metric.getObjectName();
-        boolean isAvail = attr.equals(AVAIL_ATTR);
-
-        if (objectName.indexOf("Type=Server") != -1) {
-            if (isAvail) {
-                // Hardcode for availability.  Cannot have the same
-                // template else we get plugin dumper errors.
-                attr = "numbackends";
-            }
-
-            String url = metric.getProperties().getProperty(PROP_URL);
-            String db = url.substring(url.lastIndexOf("/") + 1,
-                                      url.length());
-
-            // Check metrics that require joins across tables.
-            if (attr.equals("LocksHeld")) {
-                return "SELECT COUNT(*) FROM PG_STAT_DATABASE, PG_LOCKS " +
-                    "WHERE PG_LOCKS.DATABASE = PG_STAT_DATABASE.DATID AND " +
-                    "PG_STAT_DATABASE.DATNAME = '" + db + "'";
-            } else if (attr.equals("DataSpaceUsed")) {
-                // XXX assumes 8k page size. (which is the default)
-                return "SELECT SUM(relpages) * 8 FROM pg_class WHERE " +
-                    "relname IN (SELECT relname from pg_stat_user_tables)";
-            } else if (attr.equals("IndexSpaceUsed")) {
-                return "SELECT SUM(relpages) * 8 FROM pg_class WHERE " +
-                    "relname IN (SELECT indexrelname from " +
-                    "pg_stat_user_indexes)";
-            }
-            // Else normal query from pg_stat_database
-            return "SELECT " + attr + " FROM pg_stat_database " +
-                "WHERE datname='" + db + "'";
-        } else if (objectName.indexOf("Type=Table") != -1) {
-
-            String table = metric.getObjectProperties().getProperty(PROP_TABLE);            
-            if (table == null) {
-                // Backwards compat
-                table = metric.getProperties().getProperty(PROP_TABLE);
-            }
-            if (isAvail) {
-                // Hardcode for availability.  Cannot have the same
-                // template else we get plugin dumper errors.
-                attr = "seq_scan";
-            }
-            String schema = metric.getObjectProperties().getProperty(PROP_SCHEMA);
-            if (schema == null) {
-                return createTableQueryNoSchema(attr, table);
-            }                      
-            
-            if (attr.equals("DataSpaceUsed")) {
-                return "SELECT SUM(relpages) * 8 FROM pg_class " +
-                		"JOIN pg_catalog.pg_namespace n ON n.oid = pg_class.relnamespace " +
-                		"WHERE pg_class.relname = '" + table + "' " +
-                		"AND n.nspname ='" + schema + "'";
-            } else if (attr.equals("IndexSpaceUsed")) {
-                return "SELECT SUM(relpages) * 8 FROM pg_class " +
-                		"JOIN pg_catalog.pg_namespace n ON n.oid = pg_class.relnamespace " +
-                		"WHERE n.nspname = '" + schema + "' " + 
-                		"AND relname IN (SELECT indexrelname FROM " +
-                		"pg_stat_user_indexes WHERE relname='" +
-                		table + "' AND schemaname='" + schema + "')";
-            }
-            
-            // Else normal query from pg_stat_user_table
-            return "SELECT " + attr + " FROM pg_stat_user_tables " +
-                "WHERE relname='" + table + "' " +
-                "AND schemaname='" + schema + "'";
-        } else if (objectName.indexOf("Type=Index") != -1) {
-
-            String index = metric.getObjectProperties().getProperty(PROP_INDEX);
-            String schema = metric.getObjectProperties().getProperty(PROP_SCHEMA);
-            if (isAvail) {
-                // Hardcode for availability.  Cannot have the same
-                // template else we get plugin dumper errors.
-                attr = "idx_scan";
-            }            
-            if (schema == null) {
-                return createIndexQueryNoSchema(attr, index);
-            }             
-
-            // Else normal query from pg_stat_user_table
-            return "SELECT " + attr + " FROM pg_stat_user_indexes " +
-                "WHERE indexrelname='" + index + "' " +
-                "AND schemaname='" + schema + "'";
-        }
-
-        // Most likely a hq-plugin.xml typo.  JDBCMeasurementPlugin
-        // will pick this up.
-        return null;
-    }
-
+    
 	@Override
 	public String translate(String template, ConfigResponse config) {
 		
 		// "Type=Table,table=%table%"  ==> "Type=Table,schema=%schema%,table=%table%"
 		final String tableProperty = "table=%table%";		
 		final String tableSchemaProperties = "schema=%schema%,table=%table%";
-		
+
 		template = StringUtil.replace(template, tableProperty, tableSchemaProperties);
-        // parse the template-config
-        template = super.translate(template, config);
+		// parse the template-config
+		template = super.translate(template, config);
 
-        return template;
+		return template;
 
+	}    
+
+    @Override
+	protected String getQuery(Metric metric)
+    {
+        String attributeName = metric.getAttributeName();
+        String objectName = metric.getObjectName();
+        boolean isAvailable = attributeName.equals(AVAIL_ATTR);
+
+        String query = null;
+        if (objectName.indexOf("Type=Server") != -1) {
+            query = getServerQuery(metric, attributeName, isAvailable);
+        } else if (objectName.indexOf("Type=Table") != -1) {
+            query = getTableQuery(metric, attributeName, isAvailable);
+        } else if (objectName.indexOf("Type=Index") != -1) {
+            query = getIndexQuery(metric, attributeName, isAvailable);
+        }
+
+        // If query is still null, then it's most likely 
+        // a hq-plugin.xml typo.  JDBCMeasurementPlugin
+        // will pick this up.
+        return query;
+    }    
+
+	private String getIndexQuery(Metric metric, String attributeName, boolean isAvailable) {
+		String indexName = metric.getObjectProperties().getProperty(PROP_INDEX);
+		String schemaName = metric.getObjectProperties().getProperty(PROP_SCHEMA);
+		if (isAvailable) {
+		    // Hardcode for availability.  Cannot have the same
+		    // template else we get plugin dumper errors.
+		    attributeName = "idx_scan";
+		}      
+		String indexQuery = null;
+		if (schemaName == null) {
+		    indexQuery = getIndexQueryNoSchema(attributeName, indexName);
+		} else {
+			// Else normal query from pg_stat_user_table
+			indexQuery = "SELECT " + attributeName + " FROM pg_stat_user_indexes " +
+			"WHERE indexrelname='" + indexName + "' " +
+			"AND schemaname='" + schemaName + "'";
+		}
+		return indexQuery;
+	}
+
+	private String getTableQuery(Metric metric, String attributeName, boolean isAvailable) {
+		String tableName = metric.getObjectProperties().getProperty(PROP_TABLE);            
+		if (tableName == null) {
+		    // Backwards compat
+		    tableName = metric.getProperties().getProperty(PROP_TABLE);
+		}
+		if (isAvailable) {
+		    // Hardcode for availability.  Cannot have the same
+		    // template else we get plugin dumper errors.
+		    attributeName = "seq_scan";
+		}
+		String schemaName = metric.getObjectProperties().getProperty(PROP_SCHEMA);
+		String tableQuery = null;
+		if (schemaName == null) {
+		    tableQuery = getTableQueryNoSchema(attributeName, tableName);
+		} else if (attributeName.equals("DataSpaceUsed")) {
+		    tableQuery = "SELECT SUM(relpages) * 8 FROM pg_class " +
+		    		"JOIN pg_catalog.pg_namespace n ON n.oid = pg_class.relnamespace " +
+		    		"WHERE pg_class.relname = '" + tableName + "' " +
+		    		"AND n.nspname ='" + schemaName + "'";
+		} else if (attributeName.equals("IndexSpaceUsed")) {
+		    tableQuery = "SELECT SUM(relpages) * 8 FROM pg_class " +
+		    		"JOIN pg_catalog.pg_namespace n ON n.oid = pg_class.relnamespace " +
+		    		"WHERE n.nspname = '" + schemaName + "' " + 
+		    		"AND relname IN (SELECT indexrelname FROM " +
+		    		"pg_stat_user_indexes WHERE relname='" +
+		    		tableName + "' AND schemaname='" + schemaName + "')";
+		} else {		
+			// Else normal query from pg_stat_user_table
+			tableQuery = "SELECT " + attributeName + " FROM pg_stat_user_tables " +
+			"WHERE relname='" + tableName + "' " +
+			"AND schemaname='" + schemaName + "'";
+		}
+		return tableQuery;
+	}
+
+	private String getServerQuery(Metric metric, String attributeName, boolean isAvailable) {
+		if (isAvailable) {
+		    // Hardcode for availability.  Cannot have the same
+		    // template else we get plugin dumper errors.
+		    attributeName = "numbackends";
+		}
+
+		String url = metric.getProperties().getProperty(PROP_URL);
+		String db = url.substring(url.lastIndexOf("/") + 1,
+		                          url.length());
+
+		String serverQuery = null;
+		// Check metrics that require joins across tables.
+		if (attributeName.equals("LocksHeld")) {
+		    serverQuery = "SELECT COUNT(*) FROM PG_STAT_DATABASE, PG_LOCKS " +
+		        "WHERE PG_LOCKS.DATABASE = PG_STAT_DATABASE.DATID AND " +
+		        "PG_STAT_DATABASE.DATNAME = '" + db + "'";
+		} else if (attributeName.equals("DataSpaceUsed")) {
+		    // XXX assumes 8k page size. (which is the default)
+		    serverQuery = "SELECT SUM(relpages) * 8 FROM pg_class WHERE " +
+		        "relname IN (SELECT relname from pg_stat_user_tables)";
+		} else if (attributeName.equals("IndexSpaceUsed")) {
+		    serverQuery = "SELECT SUM(relpages) * 8 FROM pg_class WHERE " +
+		        "relname IN (SELECT indexrelname from " +
+		        "pg_stat_user_indexes)";
+		} else {
+			// Else normal query from pg_stat_database
+			serverQuery = "SELECT " + attributeName + " FROM pg_stat_database " +
+			"WHERE datname='" + db + "'";
+		}
+		return serverQuery;
 	}
     
-	
-	private String createTableQueryNoSchema(String attr, String table) {
-        if (attr.equals("DataSpaceUsed")) {
-            return "SELECT SUM(relpages) * 8 FROM pg_class WHERE " +
-                    "relname = '" + table + "'";
-        } else if (attr.equals("IndexSpaceUsed")) {
-            return "SELECT SUM(relpages) * 8 FROM pg_class WHERE " +
-                    "relname IN (SELECT indexrelname FROM " +
-                    "pg_stat_user_indexes WHERE relname='" +
-                    table + "')";
-        }
-        
-        // Else normal query from pg_stat_user_table
-        return "SELECT " + attr + " FROM pg_stat_user_tables " +
-        "WHERE relname='" + table + "'";
+
+	private String getTableQueryNoSchema(String attributeName, String tableName) {
+		String tableQuery = null;
+		if (attributeName.equals("DataSpaceUsed")) {
+			tableQuery = "SELECT SUM(relpages) * 8 FROM pg_class WHERE " +
+					"relname = '" + tableName + "'";
+		} else if (attributeName.equals("IndexSpaceUsed")) {
+			tableQuery = "SELECT SUM(relpages) * 8 FROM pg_class WHERE " +
+					"relname IN (SELECT indexrelname FROM " +
+					"pg_stat_user_indexes WHERE relname='" +
+					tableName + "')";
+		} else {        
+			// Else normal query from pg_stat_user_table
+			tableQuery = "SELECT " + attributeName + " FROM pg_stat_user_tables " +
+					"WHERE relname='" + tableName + "'";
+		}
+		return tableQuery;
 	}	
 
-	private String createIndexQueryNoSchema(String attr, String index) {
-        return "SELECT " + attr + " FROM pg_stat_user_indexes " +
-                "WHERE indexrelname='" + index + "'";
+	private String getIndexQueryNoSchema(String attributeName, String indexName) {
+		return "SELECT " + attributeName + " FROM pg_stat_user_indexes " +
+				"WHERE indexrelname='" + indexName + "'";
 
 	}	
 	
