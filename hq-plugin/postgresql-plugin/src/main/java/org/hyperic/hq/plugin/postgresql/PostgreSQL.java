c2a6b5f647775146b19bd44cba3c2538b851687c
German Laullon <laullon@gmail.com>
2012-06-05 16:25:16 -0700

    new PostgreSQL plugin (first commit)
    Former-commit-id: fcf1bface74f5decdc538198a77656fa1630ad1a

---
+++ hq-plugin/postgresql-plugin/src/main/java/org/hyperic/hq/plugin/postgresql/PostgreSQL.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hyperic.hq.plugin.postgresql;
+
+/**
+ *
+ * @author laullon
+ */
+public class PostgreSQL {
+
+    protected static final String PROP_TABLE = "table";
+    protected static final String PROP_INDEX = "index";
+    protected static final String PROP_SCHEMA = "schema";
+    protected static final String PROP_DB = "db";
+    protected static final String PROP_USER = "postgresql.user";
+    protected static final String PROP_PASS = "postgresql.pass";
+    protected static final String PROP_HOST = "postgresql.host";
+    protected static final String PROP_PORT = "postgresql.port";
+    protected static final String PROP_DFDB = "postgresql.dfdb";
+    protected static final String PROP_TABLE_REG = "postgresql.table.regex";
+    protected static final String PROP_INDEX_REG = "postgresql.index.regex";
+    
+}
