485940235e61cd51df38abd615a1dee871c4371b
Guy Segev <guy@vmware.com>
2012-06-11 21:44:55 +0300

    Guys 11/6/2012 - removed dependency on a UrlHttpConnection delegate
    Former-commit-id: 94bb3fe8376359e4b268c8b704672a593e8cb839

--- hq-integration-tests/src/test/java/org/hyperic/hq/api/rest/cxf/TestHttpURLConnection.java
+++ hq-integration-tests/src/test/java/org/hyperic/hq/api/rest/cxf/TestHttpURLConnection.java
@@ -2,6 +2,7 @@ package org.hyperic.hq.api.rest.cxf;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -13,26 +14,31 @@ public class TestHttpURLConnection extends HttpURLConnection {
 	private HttpURLConnection delegate ; 
 	
 	protected TestHttpURLConnection(HttpURLConnection delegate, final String contentType) {
-		super(delegate.getURL()) ; 
-		this.contentTypes = Arrays.asList(new String[]{ contentType }) ; 
+		this(delegate.getURL(), contentType) ; 
 		this.delegate = delegate  ;
-	}//EOM 
+	}//EOM
+	
+	protected TestHttpURLConnection(final URL url, final String contentType) {
+		super(url); 
+		this.contentTypes = Arrays.asList(new String[]{ contentType }) ; 
+	}//EOM
 	
 	@Override
 	public void connect() throws IOException {
 		//DO nothing
-		this.delegate.connect() ; 
+		//this.delegate.connect() ; 
 	}//EOM 
 	
 	@Override
 	public void disconnect() {
 		//DO nothing 
-		this.delegate.disconnect() ; 
+		//this.delegate.disconnect() ; 
 	}//EOM 
 
 	@Override
 	public final Map<String, List<String>> getHeaderFields() {
-		final Map<String, List<String>> headerFields = new HashMap<String,List<String>>(this.delegate.getHeaderFields()) ;
+//		final Map<String, List<String>> headerFields = new HashMap<String,List<String>>(this.delegate.getHeaderFields()) ;
+		final Map<String, List<String>> headerFields = new HashMap<String,List<String>>() ;
 		headerFields.put("content-type",  contentTypes);
 		headerFields.put("Content-type",  contentTypes);
 		return headerFields ; 
