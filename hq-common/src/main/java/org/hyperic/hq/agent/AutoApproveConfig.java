546705af18ecd8a3f6dfe57f6ae923a421fda907
Adi Baron <adi@tikalk.com>
2012-06-28 08:47:45 +0300

    apply auto approval to platforms and servers
    Former-commit-id: 8fd40732da2faa5a521908f0a5608dea1c86cb22

--- hq-common/src/main/java/org/hyperic/hq/agent/AutoApproveConfig.java
+++ hq-common/src/main/java/org/hyperic/hq/agent/AutoApproveConfig.java
@@ -1,3 +1,27 @@
+/*
+ * NOTE: This copyright does *not* cover user programs that use HQ
+ * program services by normal system calls through the application
+ * program interfaces provided as part of the Hyperic Plug-in Development
+ * Kit or the Hyperic Client Development Kit - this is merely considered
+ * normal use of the program, and does *not* fall under the heading of
+ * "derived work".
+ *
+ * Copyright (C) [2004-2012], VMWare, Inc.
+ * This file is part of HQ.
+ *
+ * HQ is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
 package org.hyperic.hq.agent;
 
 import org.apache.commons.logging.Log;
@@ -9,9 +33,14 @@ import java.io.IOException;
 import java.util.Properties;
 
 /**
- * 2d0 - complete documentation
+ * Holds the configuration for auto-discovered resources auto-approval.
  */
 public class AutoApproveConfig {
+
+    /**
+     * Used for getting the platform's auto-approve value.
+     */
+    public static final String PLATFORM_PROPERTY_NAME = "platform";
 
     /**
      * The logger for this class
@@ -80,7 +109,7 @@ public class AutoApproveConfig {
      * @return true if the resource was marked as auto approve; false otherwise.
      */
     public boolean isAutoApproved(String resourceName) {
-        return this.autoApproveProps != null && Boolean.valueOf(this.autoApproveProps.getProperty(resourceName));
+        return this.exists() && Boolean.valueOf(this.autoApproveProps.getProperty(resourceName));
     } // EOM
 
     /**
