ddc8e764d1a5cdb3ef8c676ffcdd7c7b00a2ba67
Adi Baron <adi@tikalk.com>
2012-07-02 02:01:09 +0300

    move auto-approve configuration init to AutoinventoryCommandsServer
    Former-commit-id: 68fab3d088a3f39fd8303afea8412eae36349d24

--- hq-agent/hq-agent-handlers/hq-agent-handler-autoinventory/src/main/java/org/hyperic/hq/autoinventory/agent/server/AutoApproveConfig.java
+++ hq-agent/hq-agent-handlers/hq-agent-handler-autoinventory/src/main/java/org/hyperic/hq/autoinventory/agent/server/AutoApproveConfig.java
@@ -22,7 +22,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA.
  */
-package org.hyperic.hq.agent;
+package org.hyperic.hq.autoinventory.agent.server;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
