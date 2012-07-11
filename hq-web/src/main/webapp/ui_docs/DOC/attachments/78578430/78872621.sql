e493b9179568be70a4c622a839f62a0ca3c20fed
Maya Anderson <andersonm@vmware.com>
2012-07-11 14:44:19 +0300

    Documentation updates by Marie: updates to system requirements and supported configs, updated Plugin Manager.
    Former-commit-id: b271ff1bce58c708aff2a8334a856b47832ee8aa

--- hq-web/src/main/webapp/ui_docs/DOC/attachments/78578430/78872621.sql
+++
@@ -1,4 +0,0 @@
-spool 1
-set heading off
-select 'alter index '||owner||'.'||index_name||' rebuild ;' from dba_indexes where owner = 'HQADMIN' and table_name not like 'HQ_METRIC%' and index_name not like '%$$';
-spool off
