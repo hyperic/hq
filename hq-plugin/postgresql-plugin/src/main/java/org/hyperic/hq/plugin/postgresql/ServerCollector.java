8ee312434aa60f40267703266a385b1af68d4c55
German Laullon <laullon@gmail.com>
2012-07-31 20:40:33 -0700

    [postgresql plugin] new metrics from pg_top
    Former-commit-id: c4f8262b81605d077dc08b0b972e94004a9dc6f9

--- hq-plugin/postgresql-plugin/src/main/java/org/hyperic/hq/plugin/postgresql/ServerCollector.java
+++ hq-plugin/postgresql-plugin/src/main/java/org/hyperic/hq/plugin/postgresql/ServerCollector.java
@@ -26,6 +26,17 @@ public class ServerCollector extends Collector {
             + "count(datid) as connections,"
             + "(SELECT setting AS mc FROM pg_settings WHERE name = 'max_connections') as max_connections "
             + "from pg_stat_activity";
+    private String statsQ = "SELECT "
+            + "SUM(xact_commit) as xact_commit, "
+            + "SUM(xact_rollback) as xact_rollback, "
+            + "SUM(blks_read) as blks_read, "
+            + "SUM(blks_hit) as blks_hit, "
+            + "SUM(tup_fetched) as tup_fetched, "
+            + "SUM(tup_inserted) as tup_inserted, "
+            + "SUM(tup_updated) as tup_updated, "
+            + "SUM(tup_deleted) as tup_deleted "
+            + "FROM pg_stat_database "
+            + "where datname in (" + PostgreSQLServerDetector.DB_QUERY + ")";
 
     @Override
     public void collect() {
@@ -39,6 +50,7 @@ public class ServerCollector extends Collector {
             String url = PostgreSQL.prepareUrl(p, null);
             log.debug("[collect] url:'" + url + "'");
             conn = DriverManager.getConnection(url, user, pass);
+            getPGTOPStast(conn);
             getConnectionsMetrics(conn);
         } catch (Exception e) {
             final String msg = "Error getting metrics: " + e.getMessage();
@@ -46,6 +58,31 @@ public class ServerCollector extends Collector {
             log.debug("[collect] " + msg, e);
         } finally {
             DBUtil.closeJDBCObjects(log, conn, null, null);
+        }
+    }
+
+    private void getPGTOPStast(Connection conn) throws SQLException {
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            log.debug("[getConnectionsMetrics] query:'" + connectionQ + "'");
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(statsQ);
+            while (rs.next()) {
+                double numBlockRead = rs.getDouble("blks_read");
+                double numBlockHit = rs.getDouble("blks_hit");
+                double numBlockHitP = numBlockHit > 0 ? (numBlockHit / (numBlockRead + numBlockHit)) : 0;
+                setValue("numXact", rs.getDouble("xact_commit"));
+                setValue("numRollback", rs.getDouble("xact_rollback"));
+                setValue("numBlockRead", numBlockRead);
+                setValue("numBlockHit",numBlockHit);
+                setValue("numBlockHitP",numBlockHitP);
+                setValue("numTupleFetched", rs.getDouble("tup_fetched"));
+                setValue("numTupleAltered", rs.getDouble("tup_inserted") + rs.getDouble("tup_updated") + rs.getDouble("tup_deleted"));
+            }
+        } finally {
+            DBUtil.closeJDBCObjects(log, null, stmt, rs);
         }
     }
 
