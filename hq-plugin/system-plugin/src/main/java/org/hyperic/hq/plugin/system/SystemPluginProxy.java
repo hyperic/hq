d55a4b1f4262d22436bfd91138ee2e1063ddcadf
Guy Segev <guy@vmware.com>
2012-11-13 11:26:27 +0200

    Guys 13/11/2012 - documentation
    Former-commit-id: b79adddadf133e9a56dae261b78201f13b18e4d6

--- hq-plugin/system-plugin/src/main/java/org/hyperic/hq/plugin/system/SystemPluginProxy.java
+++ hq-plugin/system-plugin/src/main/java/org/hyperic/hq/plugin/system/SystemPluginProxy.java
@@ -1,3 +1,28 @@
+/* **********************************************************************
+/*
+ * NOTE: This copyright does *not* cover user programs that use Hyperic
+ * program services by normal system calls through the application
+ * program interfaces provided as part of the Hyperic Plug-in Development
+ * Kit or the Hyperic Client Development Kit - this is merely considered
+ * normal use of the program, and does *not* fall under the heading of
+ * "derived work".
+ *
+ * Copyright (C) [2004-2012], VMware, Inc.
+ * This file is part of Hyperic.
+ *
+ * Hyperic is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ */
 package org.hyperic.hq.plugin.system;
 
 import java.io.InputStream;
@@ -10,13 +35,26 @@ import org.hyperic.hq.product.TypeInfo;
 import org.hyperic.util.config.ConfigResponse;
 import org.hyperic.util.config.ConfigSchema;
 
+/**
+ * Proxy class delegating all operations to a SystemPlugin instance.<br/>
+ * <br/> 
+ * The class is defined as the product sub plugin the hq-plugin.xml file. 
+ * <br/>
+ * Its purpose it to support the org and com different implementations of the actual<br/>
+ * product system plugin instances 
+ *    
+ */
 public class SystemPluginProxy extends ProductPlugin{
     
     private static final String PRODUCT_PLUGIN_FILE_NAME = "/custom-system-product-plugin.txt" ;
     private SystemPlugin delegate ; 
     
+    /**
+     * If the {@link #PRODUCT_PLUGIN_FILE_NAME} file exists in classpath of the given system-plugin<br/>
+     * load the instance defined in it else resort to the {@link SystemPlugin}
+     */
     @Override 
-    public void init(PluginManager manager) throws PluginException {
+    public final void init(final PluginManager manager) throws PluginException {
         //look for the PRODUCT_PLUGIN_FILE_NAME resource and if found, 
         //read the custom delegate class name, else resort to the SystemPlugin class
         try{ 
