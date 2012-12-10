ecada601b37700948a4fd2fee61e7c2afaad1754
Nadav Yakar <yakarn@vmware.com>
2012-12-10 12:06:41 +0200

    vcops-phase2
    Former-commit-id: 5b604f0b7589d7d4c4fb4ca95a76be66993b2916

--- hq-server/src/main/java/org/hyperic/hq/measurement/server/session/IEvaluator.java
+++
@@ -1,9 +0,0 @@
-package org.hyperic.hq.measurement.server.session;
-
-import java.util.List;
-
-import javax.jms.Destination;
-
-public interface IEvaluator {
-    public List<Destination> getDestinations();
-}
