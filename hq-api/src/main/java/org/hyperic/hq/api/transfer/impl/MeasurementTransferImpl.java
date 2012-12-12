5fa23e3cf5fc3a7b01703ee8adb85cb91a92cb27
Nadav Yakar <yakarn@vmware.com>
2012-12-12 11:17:07 +0200

    vcops-phase2
    Former-commit-id: a292648c48e04e904eabf647e64db3c61e95bb2d

--- hq-api/src/main/java/org/hyperic/hq/api/transfer/impl/MeasurementTransferImpl.java
+++ hq-api/src/main/java/org/hyperic/hq/api/transfer/impl/MeasurementTransferImpl.java
@@ -29,7 +29,6 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
--- hq-server/src/main/java/org/hyperic/hq/notifications/DestinationEvaluator.java
+++ hq-server/src/main/java/org/hyperic/hq/notifications/DestinationEvaluator.java
@@ -16,7 +16,7 @@ import javax.jms.ObjectMessage;
 import org.hyperic.hq.notifications.filtering.FilterChain;
 import org.hyperic.hq.notifications.filtering.IFilter;
 
-public abstract class DestinationEvaluator<T> implements IEvaluator<T> {
+public abstract class DestinationEvaluator<T> {
     protected Map<Destination,FilterChain<T>> destToFilter = new HashMap<Destination,FilterChain<T>>();
 
     protected abstract FilterChain<T> instantiateFilterChain(Collection<IFilter<T>> filters);
