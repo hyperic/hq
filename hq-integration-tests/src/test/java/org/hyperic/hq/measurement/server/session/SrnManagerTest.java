6ab74c5f3231cbbf1c8c4fa8e3c77681e133ad34
Guy Segev <guy@vmware.com>
2012-12-16 11:57:55 +0200

    Guys - 16/12/2012 - added import for @Ignore which was dropped during merge
    Former-commit-id: 3e473046c97369bf655f8486b7b1f6fc30e95ca0

--- hq-integration-tests/src/test/java/org/hyperic/hq/measurement/server/session/SrnManagerTest.java
+++ hq-integration-tests/src/test/java/org/hyperic/hq/measurement/server/session/SrnManagerTest.java
@@ -65,6 +65,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import org.junit.Ignore;
 
 //Guys 16/12/2012 - temporarily disabled as the db unit population fails on constraint violation 
 //(the defferable statement is not working without creating the table with the defferable clause)
