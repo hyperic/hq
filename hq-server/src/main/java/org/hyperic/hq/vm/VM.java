f4ae0c5db529bc8bd5d157a30e6317e545bf152e
Nadav Yakar <yakarn@vmware.com>
2012-12-27 18:28:43 +0200

    vcops_VM_mapping
    Former-commit-id: 19dfe3bffeb3e60019805d442fbd2a2cc5abc79e

--- hq-server/src/main/java/org/hyperic/hq/vm/VM.java
+++ hq-server/src/main/java/org/hyperic/hq/vm/VM.java
@@ -2,4 +2,22 @@ package org.hyperic.hq.vm;
 
 public class VM {
     private String mac;
+    private String uuid;
+
+    public VM(String mac, String uuid) {
+        this.mac = mac;
+        this.uuid = uuid;
+    }
+    public String getMac() {
+        return mac;
+    }
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+    public String getUuid() {
+        return uuid;
+    }
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 }
--- hq-server/src/main/java/org/hyperic/hq/vm/VMManager.java
+++ hq-server/src/main/java/org/hyperic/hq/vm/VMManager.java
@@ -3,9 +3,12 @@ package org.hyperic.hq.vm;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 
+import org.hyperic.hq.authz.server.session.AuthzSubject;
+import org.hyperic.hq.authz.shared.PermissionException;
+
 
 
 
 public interface VMManager {
-    public void collect(final String url, final String usr, final String pass, String hostName) throws RemoteException, MalformedURLException;
+    public void collect(AuthzSubject subject, final String url, final String usr, final String pass, String hostName) throws RemoteException, MalformedURLException, PermissionException;
 }
