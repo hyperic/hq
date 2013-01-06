8fde8c17970f693c2521d17fb5e7643c9081f760
Nadav Yakar <yakarn@vmware.com>
2013-01-07 00:20:10 +0200

    REST-changes update file version
    Former-commit-id: 79fdc68e095d94ef5d5a08f8c5e344d25a8e9c43

--- hq-api/src/main/java/org/hyperic/hq/api/services/impl/MeasurementServiceImpl.java
+++ hq-api/src/main/java/org/hyperic/hq/api/services/impl/MeasurementServiceImpl.java
@@ -1,5 +1,5 @@
 package org.hyperic.hq.api.services.impl;
- 
+  
 import org.hyperic.hq.api.services.MeasurementService;
 
 public class MeasurementServiceImpl extends RestApiService implements MeasurementService {
