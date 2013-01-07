42394a93c2c45f9200b558f786da82fc5da26bbd
Nadav Yakar <yakarn@vmware.com>
2013-01-07 16:00:47 +0200

    vcops_VM_mapping
    Former-commit-id: 59f194c590e798b781b61845d98097b37bd38151

--- hq-api/src/main/java/org/hyperic/hq/api/services/MetricService.java
+++ hq-api/src/main/java/org/hyperic/hq/api/services/MetricService.java
@@ -1,11 +1,13 @@
 package org.hyperic.hq.api.services;
- 
+
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.Date;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,6 +19,8 @@ import org.apache.cxf.jaxrs.model.wadl.Descriptions;
 import org.apache.cxf.jaxrs.model.wadl.DocTarget;
 import org.hibernate.ObjectNotFoundException;
 import org.hyperic.hq.api.model.measurements.MeasurementRequest;
+import org.hyperic.hq.api.model.measurements.MetricFilterRequest;
+import org.hyperic.hq.api.model.measurements.MetricNotifications;
 import org.hyperic.hq.api.model.measurements.ResourceMeasurementBatchResponse;
 import org.hyperic.hq.api.model.measurements.ResourceMeasurementRequests;
 import org.hyperic.hq.api.model.measurements.MetricResponse;
@@ -29,9 +33,9 @@ import org.hyperic.hq.authz.shared.PermissionException;
 @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON}) 
 @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 public interface MetricService {
-	
-	@POST
-	@Path("/")
+
+    @POST
+    @Path("/")
     @Descriptions({ 
         @Description(value = "Get metric data/availability for measurements for a given time frame", target = DocTarget.METHOD),
         @Description(value = "Requested metric data per measuremet", target = DocTarget.RETURN),
@@ -53,9 +57,24 @@ public interface MetricService {
         @Description(value = "The body contains a list of requests which contains a resource ID and the names of the measurements defined on it, for which the user want the metric data. The time frame for which the metric data is requested is given in the query parameters", target = DocTarget.REQUEST),
         @Description(value = "Aggregation of the requested metric data per measuremet per resource", target = DocTarget.RESPONSE),
         @Description(value = "Metric data aggregation", target = DocTarget.RESOURCE)
-     })
+    })
     public ResourceMeasurementBatchResponse getAggregatedMetricData(final ResourceMeasurementRequests request, 
             @QueryParam("begin") final Date begin, 
             @QueryParam("end") final Date end) 
-            throws ParseException, PermissionException, SessionNotFoundException, SessionTimeoutException, ObjectNotFoundException, UnsupportedOperationException, SQLException;
+                    throws ParseException, PermissionException, SessionNotFoundException, SessionTimeoutException, ObjectNotFoundException, UnsupportedOperationException, SQLException;
+
+    @GET
+    @Path("/poll")
+    public MetricNotifications poll() throws SessionNotFoundException, SessionTimeoutException;
+
+    @POST
+    @Path("/register")
+    public void register(final MetricFilterRequest metricFilterReq) throws SessionNotFoundException, SessionTimeoutException;
+
+    @PUT
+    @Path("/unregister")
+    @Descriptions({ 
+        @Description(value = "unregister user session and all assigned filters this user destination has", target = DocTarget.METHOD)
+    })
+    public void unregister() throws SessionNotFoundException, SessionTimeoutException;
 }
