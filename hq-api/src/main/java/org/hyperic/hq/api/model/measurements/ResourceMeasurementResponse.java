dca0335ae4a204625432e3c2636cd790d0bfbcfa
Nadav Yakar <yakarn@vmware.com>
2013-01-07 12:15:11 +0200

    merge with REST-changes
    Former-commit-id: 4592058a403dcbaab0a7adcb75f5ac735c12adfc

--- hq-api/src/main/java/org/hyperic/hq/api/model/measurements/ResourceMeasurementResponse.java
+++ hq-api/src/main/java/org/hyperic/hq/api/model/measurements/ResourceMeasurementResponse.java
@@ -11,23 +11,23 @@ import org.hyperic.hq.api.model.RestApiConstants;
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name="resourceMeasurementResponse", namespace=RestApiConstants.SCHEMA_NAMESPACE)
 @XmlType(name="ResourceMeasurementResponseType", namespace=RestApiConstants.SCHEMA_NAMESPACE)
-public class ResourceMeasurementResponse extends MeasurementResponse {
+public class ResourceMeasurementResponse extends MetricResponse {
     @XmlAttribute
-    protected String rscId;
+    protected String resource;
 
     public ResourceMeasurementResponse() {}
     
     public ResourceMeasurementResponse(String rscId) {
         super();
-        this.rscId=rscId;
+        this.resource=rscId;
     }
 
-    public String getRscId() {
-        return rscId;
+    public String getResourceId() {
+        return resource;
     }
 
-    public void setRscId(String rscId) {
-        this.rscId = rscId;
+    public void setResourceId(String rscId) {
+        this.resource = rscId;
     }
 
 }
