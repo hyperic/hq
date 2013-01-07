719a4af8656c9ca51ad4b5098f6e3d30a3e776f4
Nadav Yakar <yakarn@vmware.com>
2013-01-07 10:31:02 +0200

    REST-changes
    Former-commit-id: 14296a9fd382b5caf307e3154670ed39d75072b3

--- hq-api/src/main/java/org/hyperic/hq/api/model/measurements/MetricResponse.java
+++ hq-api/src/main/java/org/hyperic/hq/api/model/measurements/MetricResponse.java
@@ -26,7 +26,7 @@ package org.hyperic.hq.api.model.measurements;
  
 import java.util.ArrayList;
 import java.util.List;
-
+ 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
