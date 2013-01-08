5ad20bc42ac46b1b4bf1b442cc24b8f0a0e96f99
Nadav Yakar <yakarn@vmware.com>
2013-01-08 11:03:36 +0200

    vm mapping
    Former-commit-id: d33f37668086acbb3dfab8a4d4debc20190ae091

--- hq-api/src/main/java/org/hyperic/hq/api/model/common/RegistrationID.java
+++ hq-api/src/main/java/org/hyperic/hq/api/model/common/RegistrationID.java
@@ -1,17 +1,25 @@
 package org.hyperic.hq.api.model.common;
 
+import java.io.Serializable;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 import org.hyperic.hq.api.model.RestApiConstants;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlRootElement(name = "registrationID", namespace=RestApiConstants.SCHEMA_NAMESPACE)
+@XmlRootElement(name="registrationID", namespace=RestApiConstants.SCHEMA_NAMESPACE)
 @XmlType(name="RegistrationID", namespace=RestApiConstants.SCHEMA_NAMESPACE)
-public class RegistrationID {
+public class RegistrationID implements Serializable {
+    private static final long serialVersionUID = 56150331462303510L;
+    public RegistrationID() {} 
+    
+    @XmlElement
+    protected Integer id;
     public RegistrationID(Integer id) {
         this.id = id;
     }
@@ -23,7 +31,4 @@ public class RegistrationID {
     public void setId(Integer id) {
         this.id = id;
     }
-
-    @XmlAttribute
-    protected Integer id;
 }
