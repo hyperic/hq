d1e8727cd85890e98e71767d1908194b880ab88d
Nadav Yakar <yakarn@vmware.com>
2013-01-08 17:21:09 +0200

    vcops_indicator_metrics
    Former-commit-id: cfc621ba40066e9562f571e40db8da289a0a1402

--- hq-api/src/main/java/org/hyperic/hq/api/model/measurements/MetricFilterDefinition.java
+++ hq-api/src/main/java/org/hyperic/hq/api/model/measurements/MetricFilterDefinition.java
@@ -1,11 +1,14 @@
 package org.hyperic.hq.api.model.measurements;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 import org.hyperic.hq.api.model.RestApiConstants;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name="metricFilterDefinitioin", namespace=RestApiConstants.SCHEMA_NAMESPACE)
 @XmlType(name="MetricFilterDefinitioin", namespace=RestApiConstants.SCHEMA_NAMESPACE)
 public class MetricFilterDefinition {
