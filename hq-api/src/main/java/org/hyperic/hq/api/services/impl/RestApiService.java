7e9e0735938434e230013fbbcaab99cc496823b5
Adi Baron <adi@tikalk.com>
2013-02-01 00:13:08 +0200

    make the logger and session manager protected
    Former-commit-id: ab9d0f3db6d913540be5a6d1cf22c989a39f9597

--- hq-api/src/main/java/org/hyperic/hq/api/services/impl/RestApiService.java
+++ hq-api/src/main/java/org/hyperic/hq/api/services/impl/RestApiService.java
@@ -25,12 +25,12 @@ public class RestApiService {
     @javax.ws.rs.core.Context
     protected MessageContext messageContext;
     
-    @Autowired 
-    SessionManager sessionManager;
+    @Autowired
+    protected SessionManager sessionManager;
     
     @Autowired
     @Qualifier("restApiLogger")
-    Log logger;    
+    protected Log logger;
  
 
     /**
