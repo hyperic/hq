fd84d4076577ef5ed6decf7c45e7b65859aa20cd
Eyal Maderer <eyal>
2013-02-24 16:24:08 +0200

    Keep vCenter mapping of VM -> MAC up to date 	modified:   dist/installer/src/main/resources/data/db-upgrade.xml 	modified:   hq-api/src/main/java/org/hyperic/hq/api/services/VMService.java 	modified:   hq-api/src/main/java/org/hyperic/hq/api/services/impl/VMServiceImpl.java 	deleted:    hq-server/src/main/java/org/hyperic/hq/vm/MacToUUID.java 	new file:   hq-server/src/main/java/org/hyperic/hq/vm/VCCredentials.java 	modified:   hq-server/src/main/java/org/hyperic/hq/vm/VCDAO.java 	modified:   hq-server/src/main/java/org/hyperic/hq/vm/VCManager.java 	modified:   hq-server/src/main/java/org/hyperic/hq/vm/VCManagerImpl.java 	modified:   hq-server/src/main/java/org/hyperic/hq/vm/VMID.java 	new file:   hq-server/src/main/java/org/hyperic/hq/vm/VmMapping.java 	modified:   hq-server/src/main/resources/META-INF/spring/app-context.xml 	renamed:    hq-server/src/main/resources/hibernate/vm/Uuid.hbm.xml -> hq-server/src/main/resources/hibernate/vm/VmMapping.hbm.xml 	modified:   hq-web/src/main/java/org/hyperic/hq/ui/action/admin/config/EditConfigAction.java 	modified:   hq-web/src/main/java/org/hyperic/hq/ui/action/admin/config/SystemConfigForm.java
    Former-commit-id: 86645c526bafadf479e1c60b551405d7b0345a33

--- hq-server/src/main/java/org/hyperic/hq/vm/VCManager.java
+++ hq-server/src/main/java/org/hyperic/hq/vm/VCManager.java
@@ -1,29 +1,31 @@
 package org.hyperic.hq.vm;
 
-import java.net.MalformedURLException;
-import java.rmi.RemoteException;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import org.hyperic.hq.appdef.shared.AppdefEntityNotFoundException;
-import org.hyperic.hq.appdef.shared.CPropKeyNotFoundException;
-import org.hyperic.hq.authz.server.session.AuthzSubject;
-import org.hyperic.hq.authz.shared.PermissionException;
-
-import com.vmware.vim25.mo.ManagedEntity;
 
 public interface VCManager {
-    public Map<VMID, Set<String>> collect(final AuthzSubject subject, final String url, final String usr, final String pass) throws RemoteException, MalformedURLException, PermissionException, CPropKeyNotFoundException, AppdefEntityNotFoundException;
-    public Map<VMID,Set<String>> collectUUIDs(ManagedEntity[] me, String vcUUID) throws RemoteException, MalformedURLException;
 
     /**
-     *  retrieve a uuid of the VM the platform
-     *  
-     * @param macs   mac addresses of a single platform
-     * @return a uuid of the VM the platform identified by these mac addresses is on
+     * @param url - the vCenter URL (https://address:ip/sdk)
+     * @param user - the vCenter user
+     * @param password - the vCenter password
+     * @return true if the credentials are valid and a successful connection to the vCenter was established
      */
-    public VMID getVMID(final List<String> macs);
-    
     boolean validateVCSettings(String url, String user, String password);
+    
+    /**
+     * @param url - the vCenter URL (https://address:ip/sdk)
+     * @param user - the vCenter user
+     * @param password - the vCenter password
+     * 
+     * Call this method only after you have validated the credentials,
+     * this method will start mapping the vCenter VM => mac addresses
+     */
+    void registerVC(String url, String user, String password);
+    
+    /**
+     * @param macs - a list of mac addresses
+     * @return - the VMID of the virtual machine with the given mac address
+     */
+    VMID getVMID(List<String> macs);
 }
