723b917ed046504a42f4a9cea34cb58cb941755e
Nadav Yakar <yakarn@vmware.com>
2013-03-21 11:05:21 +0200

    remove debugging leftovers
    Former-commit-id: 0ec807c9d07aea3005585fb675d0e78f6762de76

--- hq-server/src/main/java/org/hyperic/hq/notifications/HttpEndpoint.java
+++ hq-server/src/main/java/org/hyperic/hq/notifications/HttpEndpoint.java
@@ -112,7 +112,7 @@ public class HttpEndpoint extends NotificationEndpoint {
     private BasePostingStatus publishMessage(DefaultHttpClient client, String message, HttpHost targetHost,
                                 BasicHttpContext localcontext) {
         final boolean debug = log.isDebugEnabled();
-        final HttpPost post = new HttpPost(url.getPath() + "5");
+        final HttpPost post = new HttpPost(url.getPath());
         HttpEntity entity;
         long time = System.currentTimeMillis();
         try {
