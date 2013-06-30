8528eef7e6a9d307b486ea0836053f03b1f7d747
Eyal Maderer <eyal>
2013-06-30 18:02:02 +0300

    modified:   hq-agent/hq-agent-core/src/main/java/org/hyperic/hq/agent/server/AgentDListProvider.java 	modified:   hq-common/src/main/java/org/hyperic/hq/plugin/system/ProcessReport.java 	modified:   hq-common/src/main/java/org/hyperic/hq/plugin/system/TopReport.java
    Former-commit-id: 16fddedc66bd9087e911080751957dade08760a7

--- hq-common/src/main/java/org/hyperic/hq/plugin/system/TopReport.java
+++ hq-common/src/main/java/org/hyperic/hq/plugin/system/TopReport.java
@@ -1,18 +1,17 @@
 package org.hyperic.hq.plugin.system;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.Externalizable;
 import java.io.IOException;
+import java.io.ObjectInput;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-public class TopReport implements Serializable {
+public class TopReport implements Externalizable {
 
     private static final long serialVersionUID = 1L;
     private long createTime;
@@ -78,23 +77,26 @@ public class TopReport implements Serializable {
         this.processes.add(process);
     }
 
-    @SuppressWarnings("unchecked")
-    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        createTime = in.readLong();
-        upTime = in.readUTF();
-        cpu = in.readUTF();
-        mem = in.readUTF();
-        swap = in.readUTF();
-        processes = (Set<ProcessReport>) in.readObject();
-    }
 
-    private void writeObject(ObjectOutputStream out) throws IOException {
+
+    public void writeExternal(ObjectOutput out) throws IOException {
         out.writeLong(createTime);
         out.writeUTF(upTime);
         out.writeUTF(cpu);
         out.writeUTF(mem);
         out.writeUTF(swap);
         out.writeObject(processes);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        createTime = in.readLong();
+        upTime = in.readUTF();
+        cpu = in.readUTF();
+        mem = in.readUTF();
+        swap = in.readUTF();
+        processes = ((Set<ProcessReport>) in.readObject());
     }
 
     public byte[] toSerializedForm() throws IOException {
@@ -122,6 +124,7 @@ public class TopReport implements Serializable {
                 "swap="
                 + swap + ", processes=" + processes + "]";
     }
+
 
     /*public void insertTopN(List topNData) throws InterruptedException{
         throw new NotImplementedException();
