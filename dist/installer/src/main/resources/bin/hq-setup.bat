5d49fc0172ddfda2d1e434c020f1885a476c3825
Yaron Borenstein <yaron@vmware.com>
2014-05-10 02:25:36 +0300

    Update JRE to 1.7_51

--- dist/installer/src/main/resources/bin/hq-setup.bat
+++ dist/installer/src/main/resources/bin/hq-setup.bat
@@ -22,11 +22,11 @@ if exist "%JAVA_HOME%\bin\java.exe" goto basicAntSetup
 goto noJavaExe
 
 :haveBuiltinJRE:
-if exist "%INSTALL_DIR%\jres\x86-win32-1.7_40.exe" ( 
-	SET EXE=x86-win32-1.7_40.exe
+if exist "%INSTALL_DIR%\jres\x86-win32-1.7_51.exe" ( 
+	SET EXE=x86-win32-1.7_51.exe
 )else ( 
- 	if exist "%INSTALL_DIR%\jres\x86_64-win-1.7_40.exe" ( 
- 		SET EXE=x86_64-win-1.7_40.exe
+ 	if exist "%INSTALL_DIR%\jres\x86_64-win-1.7_51.exe" ( 
+ 		SET EXE=x86_64-win-1.7_51.exe
  	) 
  )
 "%INSTALL_DIR%\jres\%EXE%" -y -o"%TEMP%" > nul 
