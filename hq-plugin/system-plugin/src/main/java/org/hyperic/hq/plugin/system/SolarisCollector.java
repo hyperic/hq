87e723cf364b82a152e852b1b6d6d1c134427650
German Laullon <glaullon@vmware.com>
2014-05-26 10:52:03 +0200

    [OS counters] Page faults. Swap indicators. Initial physical disk (commented).

---
+++ hq-plugin/system-plugin/src/main/java/org/hyperic/hq/plugin/system/SolarisCollector.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hyperic.hq.plugin.system;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hyperic.hq.product.Collector;
+
+/**
+ *
+ * @author glaullon
+ */
+public class SolarisCollector extends Collector {
+
+    private static final Log log = LogFactory.getLog(SolarisCollector.class);
+    private static final String VMSTAT[] = {"vmstat", "-s"};
+    private static final String METRICS[][] = {{"pgfault", "minor (as) faults"}, {"pgmajfault", "major faults"}};
+
+    @Override
+    public void collect() {
+        log.debug("[collect]");
+        Process cmd;
+        try {
+            cmd = Runtime.getRuntime().exec(VMSTAT);
+            cmd.waitFor();
+            if (cmd.exitValue() != 0) {
+                String msg = inputStreamAsString(cmd.getErrorStream());
+                log.debug("[collect] cmd error: " + msg);
+                setErrorMessage(msg);
+            } else {
+                Map<String, Double> stats = parseVMStat(cmd.getInputStream());
+                for (String[] metric : METRICS) {
+                    final Double val = stats.get(metric[1]);
+                    log.debug("[collect] " + metric[0] + "=" + val);
+                    if (val != null) {
+                        setValue(metric[0], val);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            log.debug("[collect] error: " + e.getMessage(), e);
+            setErrorMessage(e.getLocalizedMessage(), e);
+        } catch (InterruptedException e) {
+            log.debug("[collect] error: " + e.getMessage(), e);
+            setErrorMessage(e.getLocalizedMessage(), e);
+        }
+    }
+
+    static final Map<String, Double> parseVMStat(InputStream stream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        Map<String, Double> res = new HashMap<String, Double>();
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                int sep = line.indexOf(" ");
+                log.debug("[parseVMStat] line='" + line + "' (" + sep + ")");
+                if (sep > 0) {
+                    String key = line.substring(sep).trim();
+                    String val = line.substring(0, sep).trim();
+                    log.debug("[parseVMStat] " + key + " = " + val);
+                    res.put(key, Double.parseDouble(val));
+                }
+            }
+        } finally {
+            br.close();
+        }
+        return res;
+    }
+
+    static final String inputStreamAsString(InputStream stream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder sb = new StringBuilder();
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } finally {
+            br.close();
+        }
+        return sb.toString();
+    }
+}
