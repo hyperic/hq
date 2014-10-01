2f7ccee74d8dab5a086b4efaa8023e552a0d2090
German Laullon <glaullon@vmware.com>
2014-10-01 15:46:14 +0200

    [HHQ-5990] Postgres plugin query for idle connections needs to be updated for 9.2+

--- hq-plugin/postgresql-plugin/src/main/java/org/hyperic/hq/plugin/postgresql/DataBaseCollector.java
+++ hq-plugin/postgresql-plugin/src/main/java/org/hyperic/hq/plugin/postgresql/DataBaseCollector.java
@@ -1,6 +1,26 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * NOTE: This copyright does *not* cover user programs that use HQ
+ * program services by normal system calls through the application
+ * program interfaces provided as part of the Hyperic Plug-in Development
+ * Kit or the Hyperic Client Development Kit - this is merely considered
+ * normal use of the program, and does *not* fall under the heading of
+ * "derived work".
+ * 
+ * Copyright (C) [2004 - 2014], Hyperic, Inc.
+ * This file is part of HQ.
+ * 
+ * HQ is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
  */
 package org.hyperic.hq.plugin.postgresql;
 
@@ -15,6 +35,7 @@ import java.util.List;
 import java.util.Properties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hyperic.hq.plugin.postgresql.ServerCollector.PGVersion;
 import org.hyperic.hq.product.Collector;
 import org.hyperic.hq.product.Metric;
 import org.hyperic.util.jdbc.DBUtil;
@@ -25,64 +46,34 @@ import org.hyperic.util.jdbc.DBUtil;
  */
 public class DataBaseCollector extends Collector {
 
-    private static Log log = LogFactory.getLog(DataBaseCollector.class);
-    private List<String> db_cache = new ArrayList<String>();
-    private String query[] = {
-        "SELECT "
-        + "* "
-        + "FROM pg_stat_database where datname in (" + PostgreSQLServerDetector.DB_QUERY + ")",
-        "SELECT "
-        + "COUNT(datid) AS connections,"
-        + "(SELECT setting AS mc FROM pg_settings WHERE name = 'max_connections') AS max_connections,"
-        + "d.datname "
-        + "FROM pg_database d "
-        + "LEFT JOIN pg_stat_activity s ON (s.datid = d.oid) "
-        + "GROUP BY 2,3"
+    private static final Log log = LogFactory.getLog(DataBaseCollector.class);
+    private final List<String> db_cache = new ArrayList<String>();
+    private final String query[] = {
+        "SELECT * FROM pg_stat_database where datname in (" + PostgreSQLServerDetector.DB_QUERY + ")",
+        "SELECT COUNT(datid) AS connections, (SELECT setting AS mc FROM pg_settings WHERE name = 'max_connections') AS max_connections, d.datname FROM pg_database d LEFT JOIN pg_stat_activity s ON (s.datid = d.oid) GROUP BY 2,3"
     };
-    private String queryDB[] = {
-        "SELECT "
-        + "(select current_database()) as datname,"
-        + "COALESCE(SUM(idx_scan),0) AS idx_scan,"
-        + "COALESCE(SUM(idx_tup_read),0) AS idx_tup_read,"
-        + "COALESCE(SUM(idx_tup_fetch),0) AS idx_tup_fetch "
-        + "from pg_stat_user_indexes",
-        "SELECT "
-        + "(select current_database()) as datname,"
-        + "COALESCE(SUM(idx_blks_read),0) AS idx_blks_read,"
-        + "COALESCE(SUM(idx_blks_hit),0) AS idx_blks_hit "
-        + "from pg_statio_user_indexes",
-        "SELECT "
-        + "(select current_database()) as datname,"
-        + "COALESCE(SUM(seq_scan),0) AS seq_scan,"
-        + "COALESCE(SUM(seq_tup_read),0) AS seq_tup_read "
-        + "FROM pg_stat_user_tables",
-        "SELECT "
-        + "count(mode) granted_locks, "
-        + "datname "
-        + "FROM pg_locks l "
-        + "JOIN pg_database d ON (d.oid=l.database) "
-        + "WHERE l.granted=true "
-        + "GROUP BY 2",
-        "SELECT "
-        + "count(mode) awaited_locks, "
-        + "datname "
-        + "FROM pg_locks l "
-        + "JOIN pg_database d ON (d.oid=l.database) "
-        + "WHERE l.granted=false "
-        + "GROUP BY 2",
-        "SELECT "
-        + "datname, "
-        + "count(current_query) AS idle_backends "
-        + "FROM pg_stat_activity "
-        + "WHERE current_query = '<IDLE>' "
-        + "GROUP BY 1;"
+    private final String queryDB[] = {
+        "SELECT (select current_database()) as datname, COALESCE(SUM(idx_scan),0) AS idx_scan, COALESCE(SUM(idx_tup_read),0) AS idx_tup_read, COALESCE(SUM(idx_tup_fetch),0) AS idx_tup_fetch from pg_stat_user_indexes",
+        "SELECT (select current_database()) as datname, COALESCE(SUM(idx_blks_read),0) AS idx_blks_read, COALESCE(SUM(idx_blks_hit),0) AS idx_blks_hit from pg_statio_user_indexes",
+        "SELECT (select current_database()) as datname, COALESCE(SUM(seq_scan),0) AS seq_scan, COALESCE(SUM(seq_tup_read),0) AS seq_tup_read FROM pg_stat_user_tables",
+        "SELECT count(mode) granted_locks, datname FROM pg_locks l JOIN pg_database d ON (d.oid=l.database) WHERE l.granted=true GROUP BY 2",
+        "SELECT count(mode) awaited_locks, datname FROM pg_locks l JOIN pg_database d ON (d.oid=l.database) WHERE l.granted=false GROUP BY 2"};
+    private final String queryDB_PRE_92[] = {
+        "SELECT datname, count(current_query) AS idle_backends FROM pg_stat_activity WHERE current_query != '<IDLE>' GROUP BY 1;",
+        "SELECT datname, count(current_query) AS idle_backends FROM pg_stat_activity WHERE current_query = '<IDLE>' GROUP BY 1;"
     };
-    private String posibleNULLMetrics[] = {"granted_locks", "awaited_locks"};
+    private final String queryDB_POST_92[] = {
+        "SELECT datname, 0 AS idle_backends FROM pg_stat_activity WHERE state != 'idle' GROUP BY 1;",
+        "SELECT datname, count(state) AS idle_backends FROM pg_stat_activity WHERE state = 'idle' GROUP BY 1;"
+    };
+    private final String posibleNULLMetrics[] = {"granted_locks", "awaited_locks"};
+    private PGVersion version = PGVersion.UNKNOW;
 
     @Override
     public void collect() {
-        db_cache.clear();
         Properties p = getProperties();
+        log.debug("[collect] p:" + p);
+        db_cache.clear();
         String user = p.getProperty(PostgreSQL.PROP_USER);
         String pass = p.getProperty(PostgreSQL.PROP_PASS);
 
@@ -97,19 +88,8 @@ public class DataBaseCollector extends Collector {
                 extartMetrics(q, conn, j == 0);
             }
 
-            DBUtil.closeJDBCObjects(log, conn, null, null);
-
-            for (int i = 0; i < db_cache.size(); i++) {
-                String db = db_cache.get(i);
-                setValue(db + "." + Metric.ATTR_AVAIL, Metric.AVAIL_UP);
-                url = PostgreSQL.prepareUrl(p, db);
-                log.debug("[collect] url:'" + url + "'");
-                conn = DriverManager.getConnection(url, user, pass);
-
-                for (int j = 0; j < queryDB.length; j++) {
-                    String q = queryDB[j];
-                    extartMetrics(q, conn, false);
-                }
+            if (version == PGVersion.UNKNOW) {
+                version = ServerCollector.checkVersion(conn);
             }
         } catch (Exception e) {
             final String msg = "Error getting metrics: " + e.getMessage();
@@ -119,8 +99,34 @@ public class DataBaseCollector extends Collector {
             DBUtil.closeJDBCObjects(log, conn, null, null);
         }
 
+        log.debug("[collect] db_cache: " + db_cache);
+
+        for (String db : db_cache) {
+            try {
+                String url = PostgreSQL.prepareUrl(p, db);
+                log.debug("[collect] url:'" + url + "'");
+                conn = DriverManager.getConnection(url, user, pass);
+                for (String q : queryDB) {
+                    extartMetrics(q, conn, false);
+                }
+
+                String[] extraQueryDB = (version == PGVersion.POST_92) ? queryDB_POST_92 : queryDB_PRE_92;
+                for (String q : extraQueryDB) {
+                    extartMetrics(q, conn, false);
+                }
+            } catch (Exception e) {
+                final String msg = "Error getting metrics: " + e.getMessage();
+                setErrorMessage(msg, e);
+                log.debug("[collect] " + msg, e);
+            } finally {
+                DBUtil.closeJDBCObjects(log, conn, null, null);
+            }
+        }
+
         for (int i = 0; i < db_cache.size(); i++) {
             String db = db_cache.get(i);
+            setValue(db + "." + Metric.ATTR_AVAIL, Metric.AVAIL_UP);
+
             double c = Double.parseDouble((String) getResult().getValues().get(db + ".connections"));
             double m = Double.parseDouble((String) getResult().getValues().get(db + ".max_connections"));
             setValue(db + ".connections_usage", c / m);
@@ -133,9 +139,7 @@ public class DataBaseCollector extends Collector {
                     + Double.parseDouble((String) getResult().getValues().get(db + ".tup_deleted"));
             setValue(db + ".blks_hit_p", blksHitP);
             setValue(db + ".tup_altered", tupAltered);
-
-            for (int j = 0; j < posibleNULLMetrics.length; j++) {
-                String metric = posibleNULLMetrics[j];
+            for (String metric : posibleNULLMetrics) {
                 if (getResult().getValues().get(db + "." + metric) == null) {
                     setValue(db + "." + metric, 0);
                 }
--- hq-plugin/postgresql-plugin/src/main/java/org/hyperic/hq/plugin/postgresql/ServerCollector.java
+++ hq-plugin/postgresql-plugin/src/main/java/org/hyperic/hq/plugin/postgresql/ServerCollector.java
@@ -1,6 +1,26 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * NOTE: This copyright does *not* cover user programs that use HQ
+ * program services by normal system calls through the application
+ * program interfaces provided as part of the Hyperic Plug-in Development
+ * Kit or the Hyperic Client Development Kit - this is merely considered
+ * normal use of the program, and does *not* fall under the heading of
+ * "derived work".
+ * 
+ * Copyright (C) [2004 - 2014], Hyperic, Inc.
+ * This file is part of HQ.
+ * 
+ * HQ is free software; you can redistribute it and/or modify
+ * it under the terms version 2 of the GNU General Public License as
+ * published by the Free Software Foundation. This program is distributed
+ * in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
  */
 package org.hyperic.hq.plugin.postgresql;
 
@@ -22,11 +42,15 @@ import org.hyperic.util.jdbc.DBUtil;
  */
 public class ServerCollector extends Collector {
 
+    protected enum PGVersion {
+
+        UNKNOW, PRE_92, POST_92
+    }
+    private PGVersion version = PGVersion.UNKNOW;
+
     private static Log log = LogFactory.getLog(ServerCollector.class);
-    private String connectionQ = "select "
-            + "count(datid) as connections,"
-            + "(SELECT setting AS mc FROM pg_settings WHERE name = 'max_connections') as max_connections "
-            + "from pg_stat_activity";
+    private String connectionQ = "SELECT COUNT(datid) AS connections, (SELECT setting AS mc FROM pg_settings WHERE name = 'max_connections') AS max_connections, d.datname FROM pg_database d LEFT JOIN pg_stat_activity s ON (s.datid = d.oid) GROUP BY 2,3";
+
     private String statsQ = "SELECT "
             + "SUM(xact_commit) as xact_commit, "
             + "SUM(xact_rollback) as xact_rollback, "
@@ -38,7 +62,9 @@ public class ServerCollector extends Collector {
             + "SUM(tup_deleted) as tup_deleted "
             + "FROM pg_stat_database "
             + "where datname in (" + PostgreSQLServerDetector.DB_QUERY + ")";
-    private String[] queries = {"select count(*) AS idle_backends from pg_stat_activity where current_query = '<IDLE>'"};
+    private final String[] queries_POST_92 = {"SELECT count(s.state) AS idle_backends FROM pg_database d LEFT JOIN pg_stat_activity s ON (s.datid = d.oid) where s.state = 'idle'"};
+    private final String[] queries_PRE_92 = {"SELECT count(s.current_query) AS idle_backends FROM pg_database d LEFT JOIN pg_stat_activity s ON (s.datid = d.oid) where s.current_query = '<IDLE>'"};
+//"select count(*) AS idle_backends from pg_stat_activity where current_query = '<IDLE>'"};
 
     @Override
     public void collect() {
@@ -52,9 +78,17 @@ public class ServerCollector extends Collector {
             String url = PostgreSQL.prepareUrl(p, null);
             log.debug("[collect] url:'" + url + "'");
             conn = DriverManager.getConnection(url, user, pass);
-            for (int j = 0; j < queries.length; j++) {
-                extartMetrics(queries[j], conn);
+
+            if (version == PGVersion.UNKNOW) {
+                version = ServerCollector.checkVersion(conn);
             }
+
+            String[] q = (version == PGVersion.POST_92) ? queries_POST_92 : queries_PRE_92;
+
+            for (int j = 0; j < q.length; j++) {
+                extartMetrics(q[j], conn);
+            }
+
             getPGTOPStast(conn);
             getConnectionsMetrics(conn);
         } catch (Exception e) {
@@ -100,12 +134,14 @@ public class ServerCollector extends Collector {
             log.debug("[getConnectionsMetrics] query:'" + connectionQ + "'");
             stmt = conn.createStatement();
             rs = stmt.executeQuery(connectionQ);
+            double c = 0;
+            double m = 0;
             while (rs.next()) {
-                double c = rs.getDouble("connections");
-                double m = rs.getDouble("max_connections");
-                setValue("connections", c);
-                setValue("connections_usage", c / m);
+                c += rs.getDouble("connections");
+                m = rs.getDouble("max_connections");
             }
+            setValue("connections", c);
+            setValue("connections_usage", c / m);
         } finally {
             DBUtil.closeJDBCObjects(log, null, stmt, rs);
         }
@@ -128,5 +164,37 @@ public class ServerCollector extends Collector {
         } finally {
             DBUtil.closeJDBCObjects(log, null, stmt, rs);
         }
+    }
+
+    /**
+     * Check if the PG is 9.2+ or not by checking the exitance of
+     * 'current_query' on 'pg_stat_activity'
+     *
+     * @param conn JDCB Connection to PG
+     * @return PGVersion.PRE_92 or PGVersion.POST_92
+     * @throws SQLException in case of a SQL problem not related to
+     * 'current_query' column.
+     */
+    protected static PGVersion checkVersion(Connection conn) throws SQLException {
+        Statement stmt = null;
+        ResultSet rs = null;
+        PGVersion version = PGVersion.PRE_92;
+
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("select current_query from pg_stat_activity");
+        } catch (SQLException ex) {
+            if (ex.getMessage().contains("current_query")) {
+                log.debug("[checkVersion] " + ex.getMessage().trim());
+                version = PGVersion.POST_92;
+            } else {
+                throw ex;
+            }
+        } finally {
+            DBUtil.closeJDBCObjects(log, null, stmt, rs);
+        }
+
+        log.debug("[checkVersion] version: " + version);
+        return version;
     }
 }
