546450b3e1c9780e65ee3e1c7041c18e3dae9eba
Yaron Borenstein <yaron@vmware.com>
2014-10-27 16:28:38 +0200

    Update JRE to JRE 7u71 HHQ-6025

--- dist/installer/src/main/resources/bin/hq-setup.bat
+++ dist/installer/src/main/resources/bin/hq-setup.bat
@@ -22,11 +22,11 @@ if exist "%JAVA_HOME%\bin\java.exe" goto basicAntSetup
 goto noJavaExe
 
 :haveBuiltinJRE:
-if exist "%INSTALL_DIR%\jres\x86-win32-1.7_51.exe" ( 
-	SET EXE=x86-win32-1.7_51.exe
+if exist "%INSTALL_DIR%\jres\x86-win32-1.7_71.exe" ( 
+	SET EXE=x86-win32-1.7_71.exe
 )else ( 
- 	if exist "%INSTALL_DIR%\jres\x86_64-win-1.7_51.exe" ( 
- 		SET EXE=x86_64-win-1.7_51.exe
+ 	if exist "%INSTALL_DIR%\jres\x86_64-win-1.7_71.exe" ( 
+ 		SET EXE=x86_64-win-1.7_71.exe
  	) 
  )
 "%INSTALL_DIR%\jres\%EXE%" -y -o"%TEMP%" > nul 
