0ccbe30e5d21133026afca5f2ff2993637d6b88f
German Laullon <glaullon@vmware.com>
2014-10-30 00:59:45 +0100

    [MSSQL Health] new metrics and config options validation.

--- hq-plugin/mssql-plugin/src/main/java/org/hyperic/hq/plugin/mssql/MsSQLDataBaseCollector.java
+++ hq-plugin/mssql-plugin/src/main/java/org/hyperic/hq/plugin/mssql/MsSQLDataBaseCollector.java
@@ -16,6 +16,7 @@ import org.apache.commons.logging.LogFactory;
 import org.hyperic.hq.product.CollectorResult;
 import org.hyperic.hq.product.Metric;
 import org.hyperic.hq.product.MetricValue;
+import org.hyperic.hq.product.PluginException;
 import org.hyperic.hq.product.ProductPluginManager;
 import org.hyperic.util.exec.Execute;
 import org.hyperic.util.exec.ExecuteWatchdog;
@@ -39,18 +40,23 @@ public class MsSQLDataBaseCollector extends MsSQLCollector {
         super.collect();
 
         List<String> scriptPropertiesList = getScript(properties);
-        List<List<String>> res = executeSqlCommand(scriptPropertiesList);
-
-        for (List<String> line : res) {
-            if (line.size() == 4) {
-                String db = line.get(0);
-                String val = line.get(3);
-                log.debug("Database:'" + db + "' Value='" + val + "'");
-                setValue(db, val);
-            } else {
-                log.debug("Unknown formatting from script output:" + line);
+        List<List<String>> res;
+        try {
+            res = executeSqlCommand(scriptPropertiesList);
+            for (List<String> line : res) {
+                if (line.size() == 4) {
+                    String db = line.get(0);
+                    String val = line.get(3);
+                    log.debug("Database:'" + db + "' Value='" + val + "'");
+                    setValue(db, val);
+                } else {
+                    log.debug("Unknown formatting from script output:" + line);
+                }
             }
+        } catch (PluginException ex) {
+            log.debug(ex, ex);
         }
+
     }
 
     @Override
@@ -63,59 +69,54 @@ public class MsSQLDataBaseCollector extends MsSQLCollector {
         }
     }
 
-    public static List<List<String>> executeSqlCommand(List<String> scriptPropertiesList) {
+    public static List<List<String>> executeSqlCommand(List<String> scriptPropertiesList) throws PluginException {
+        final List<List<String>> res = new ArrayList<List<String>>();
+
         if (log.isDebugEnabled()) {
-            List<String> p = new ArrayList<String>(scriptPropertiesList);
-            int i = p.indexOf("-P");
-            if (i != -1) {
-                p.set(i + 1, "**********");
-            }
-            log.debug("[executeSqlCommand] Script Properties = " + scriptPropertiesList);
+            debug("[executeSqlCommand] Script Properties = " + scriptPropertiesList);
         }
 
-        final List<List<String>> res = new ArrayList<List<String>>();
         String output = runCommand(scriptPropertiesList.toArray(new String[scriptPropertiesList.size()]), 60);
         List<String> lines = Arrays.asList(output.split("\n"));
         for (String line : lines) {
             line = line.trim();
-            if ((line.length() > 0) && !line.startsWith("(") && !line.endsWith(")")) {
-                List<String> lineSplit = new ArrayList<String>();
-                for (String str : line.split(",")) {
-                    lineSplit.add(str.trim());
+            if (line.length() > 0) {
+                log.debug("[executeSqlCommand] line: '" + line + "'");
+                if (!line.startsWith("(") && !line.endsWith(")")) {
+                    List<String> lineSplit = new ArrayList<String>();
+                    for (String str : line.split(",")) {
+                        lineSplit.add(str.trim());
+                    }
+                    res.add(lineSplit);
                 }
-                if (log.isDebugEnabled()) {
-                    log.debug("[executeSqlCommand] line:" + lineSplit);
-                }
-                res.add(lineSplit);
             }
         }
+
         return res;
     }
 
     public static List<String> prepareSqlCommand(Properties props) {
-        log.debug("==>" + props);
+        debug("==>" + props);
         List<String> scriptPropertiesList = new ArrayList<String>();
 
-        String serverName = getServerName(props);
-        String instance = props.getProperty("instance");
-        if ((instance != null) && (!instance.equals(MsSQLDetector.DEFAULT_SQLSERVER_SERVICE_NAME))) {
-            serverName += "\\" + instance;
-        }
-        String username = props.getProperty("user");
-        String password = props.getProperty("password");
-        File pdkWorkDir = new File(ProductPluginManager.getPdkWorkDir());
-
-        File sqlScript;
-        if ("2005".equals(props.getProperty("v"))) {
-            scriptPropertiesList.add("sqlcmd");
-            sqlScript = new File(pdkWorkDir, "/scripts/mssql/" + MDF_FREE_SPACE_PCT2005_SQL);
-        } else {
+        if ("2000".equals(props.getProperty("v"))) {
             scriptPropertiesList.add("osql");
             scriptPropertiesList.add("-n");
-            sqlScript = new File(pdkWorkDir, "/scripts/mssql/" + MDF_FREE_SPACE_PCT2000_SQL);
+        } else {
+            scriptPropertiesList.add("sqlcmd");
         }
-        scriptPropertiesList.add("-S");
-        scriptPropertiesList.add(serverName);
+
+        String serverName = props.getProperty("sqlserver_name", "").replaceAll("\\%[^\\%]*\\%", "").trim();
+        String instance = props.getProperty("instance", props.getProperty("instance-name", "")).replaceAll("\\%[^\\%]*\\%", "").trim();
+        log.debug("sqlserver_name='" + serverName + "' instance=" + instance + "");
+        if (serverName.length() > 0) {
+            if (!MsSQLDetector.DEFAULT_SQLSERVER_SERVICE_NAME.equals(instance) && (instance.length() > 0)) {
+                serverName += "\\" + instance;
+            }
+            scriptPropertiesList.add("-S");
+            scriptPropertiesList.add(serverName);
+        }
+
         scriptPropertiesList.add("-s");
         scriptPropertiesList.add(",");
         scriptPropertiesList.add("-l");
@@ -125,13 +126,16 @@ public class MsSQLDataBaseCollector extends MsSQLCollector {
         scriptPropertiesList.add("65535");
 
         if (log.isDebugEnabled()) {
-            log.debug("Script Properties = " + scriptPropertiesList);
+            debug("Script Properties = " + scriptPropertiesList);
         }
 
         /* If the user specifies the username and password then it is it will use sql authentication.
          Otherwise, it will use the trusted connection user to access the db. 
          */
-        if (username != null && !"%user%".equals(username) && password != null && !"%password%".equals(password)) {
+        String username = props.getProperty("user", "").replaceAll("\\%[^\\%]*\\%", "").trim();
+        String password = props.getProperty("password", "").replaceAll("\\%[^\\%]*\\%", "").trim();
+
+        if ((username.length() > 0) && (password.length() > 0)) {
             log.debug("Adding username to script properties: -U " + username);
             scriptPropertiesList.add("-U");
             scriptPropertiesList.add(username);
@@ -139,10 +143,6 @@ public class MsSQLDataBaseCollector extends MsSQLCollector {
             log.debug("Adding password to script properties: -P *******");
             scriptPropertiesList.add("-P");
             scriptPropertiesList.add(password);
-        } else {
-            // -E means it is a trusted connection
-            log.debug("Setting as trusted connection on script properties: -E");
-            scriptPropertiesList.add("-E");
         }
 
         return scriptPropertiesList;
@@ -169,33 +169,28 @@ public class MsSQLDataBaseCollector extends MsSQLCollector {
         return scriptPropertiesList;
     }
 
-    public static String runCommand(String[] command, int timeout) {
+    public static String runCommand(String[] command, int timeout) throws PluginException {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         final PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(output);
         ExecuteWatchdog wdog = new ExecuteWatchdog(timeout * 1000);
         Execute exec = new Execute(pumpStreamHandler, wdog);
         exec.setCommandline(command);
-        log.debug("Running: " + exec.getCommandLineString());
+        debug("Running: " + exec.getCommandLineString());
         try {
             exec.execute();
         } catch (Exception e) {
-            log.debug("Fail to run command: " + exec.getCommandLineString() + " " + e.getMessage());
-            return null;
+            throw new PluginException("Fail to run command: " + e.getMessage(), e);
         }
         String out = output.toString().trim();
         log.debug("out: " + out);
-        log.debug("done: " + exec.getCommandLineString());
+        log.debug("ExitValue: " + exec.getExitValue());
+        if (exec.getExitValue() != 0) {
+            throw new PluginException(out);
+        }
         return out;
     }
-
-    private static String getServerName(Properties props) {
-        String serverName = props.getProperty("ServerName");
-        log.debug("ServerName from config=" + serverName);
-        // there is bug causing the default not to be set for sqlserver_name
-        if (serverName == null || "".equals(serverName) || "%sqlserver_name%".equals(serverName)) {
-            serverName = "localhost";
-            log.debug("Setting serverName to default=" + serverName);
-        }
-        return serverName;
+    
+    private static void debug(String msg) {
+        log.debug(msg.replaceAll("(-P,? ?)([^ ,]+)", "$1******").replaceFirst("(pass[^=]*=)(\\w*)", "$1******"));
     }
 }
