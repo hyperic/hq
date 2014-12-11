46a79c938d941194fb2d160519eb7d102be88641
German Laullon <glaullon@vmware.com>
2014-12-11 08:37:44 +0100

    IaaS web - round 2

--- hq-plugin/vRealize-plugin/src/main/java/org/hyperic/plugin/vrealize/automation/DiscoveryVRAIaasWeb.java
+++ hq-plugin/vRealize-plugin/src/main/java/org/hyperic/plugin/vrealize/automation/DiscoveryVRAIaasWeb.java
@@ -72,11 +72,11 @@ public class DiscoveryVRAIaasWeb extends DaemonDetector {
 
                 File webCfg = new File(installPath, "Server/Model Manager Web/Web.config");
                 log.debug("[newServerResource] webCfg=" + getCanonicalPath(webCfg));
-                String vRealizeAutomationVA = executeXMLQuery("//repository/@store", webCfg);
-                vRealizeAutomationVA = vRealizeAutomationVA.replaceAll("\\w*://([^:/]*).*", "$1");
-                log.debug("[newServerResource] vRealizeAutomationVA=" + vRealizeAutomationVA);
+                String vRAServer = executeXMLQuery("//repository/@store", webCfg);
+                vRAServer = vRAServer.replaceAll("\\w*://([^:/]*).*", "$1");
+                log.debug("[newServerResource] vRAServer=" + vRAServer);
 
-                String model = VRAUtils.marshallResource(getIaaSWebServerRelationsModel(vRealizeAutomationVA, vRAIaaSWebLoadBalancer));
+                String model = VRAUtils.marshallResource(getIaaSWebServerRelationsModel(vRAServer, vRAIaaSWebLoadBalancer));
                 ConfigResponse c = new ConfigResponse();
                 c.setValue(PROP_EXTENDED_REL_MODEL, new String(Base64.encodeBase64(model.getBytes())));
 
@@ -89,10 +89,9 @@ public class DiscoveryVRAIaasWeb extends DaemonDetector {
         return server;
     }
 
-    public static Resource getIaaSWebServerRelationsModel(String vravaName, String vRAIaaSWebLoadBalancer) {
+    public static Resource getIaaSWebServerRelationsModel(String vRaApplicationName, String vRAIaaSWebLoadBalancer) {
         ObjectFactory factory = new ObjectFactory();
 
-        final String vRaApplicationName = "${application.name}";
         final String iaasWebServerResourceType = "vRealize Automation IaaS Web";
         final String iaasWebServerName = "iaas-web-server-1.local";
         Resource iaasWebServer = factory.createResource(!CREATE_IF_NOT_EXIST, iaasWebServerResourceType,
@@ -106,32 +105,44 @@ public class DiscoveryVRAIaasWeb extends DaemonDetector {
         Resource vraAppTagResource = getVraAppTag(factory, vRaApplicationName, vraAppTagType);
         iaasWebServerTag.addRelations(factory.createRelation(vraAppTagResource, PARENT));
 
+        //This is only if there is a load balancer. If not then nothing load balancer related should be created.
         final String lbResourceType = "vRealize Automation IaaS Web Load Balancer Service";
-        Resource loadBalancer = getLoadBalancer(factory, vRaApplicationName, lbResourceType);
+        final String lbResourceName = "lg-web.ra.local";
+        Resource loadBalancer = factory.createResource(Boolean.FALSE, lbResourceType, lbResourceName, SERVER);;
         iaasWebServer.addRelations(factory.createRelation(loadBalancer, SIBLING));
 
-        final String lbTagType = "vRealize Automation IaaS Web Load Balancer Service";
-        Resource loadBalancerTag = createLogialResource(factory, vRAIaaSWebLoadBalancer, lbTagType);
+        final String lbTagType = "vRealize Automation Load Balancer";
+        Resource loadBalancerTag = createLogialResource(factory, vRaApplicationName, lbTagType);
         loadBalancer.addRelations(factory.createRelation(loadBalancerTag, PARENT));
         loadBalancerTag.addRelations(factory.createRelation(vraAppTagResource, PARENT));
 
         final String lbsTagType = "Load Balancer";
-        Resource loadBalancerSuperTag = createLogialResource(factory, vRaApplicationName, lbsTagType);
+        Resource loadBalancerSuperTag = createLogialResource(factory, lbTagType, lbsTagType);
         loadBalancerTag.addRelations(factory.createRelation(loadBalancerSuperTag, PARENT));
 
-        final String vcoName = "lg-vco.ra.local";
-        final String vcoType = "vCO App Server";
+        final String vcoName = "ra-vco-a2-bg-01.refarch.eng.vmware.com";
+        final String vcoType = " vCenter Orchestrator App Server";
         Resource vco = factory.createResource(!CREATE_IF_NOT_EXIST, vcoType, vcoName, SERVER);
         iaasWebServer.addRelations(factory.createRelation(vco, SIBLING));
 
-        final String vcoTagType = "vCO";
+        final String vcoTagType = "vCenter Orchestrator";
         Resource vcoTag = createLogialResource(factory, vRaApplicationName, vcoTagType);
         vcoTag.addRelations(factory.createRelation(vraAppTagResource, PARENT));
         vco.addRelations(factory.createRelation(vcoTag, PARENT, CREATE_IF_NOT_EXIST));
 
+        /**
+         * Adding both vRAVA directly and the vRAVA load balancer because we
+         * won't to which the FQDN we have points.
+         */
         final String vravaType = "vRealize Automation Server";
         Resource vrava = factory.createResource(!CREATE_IF_NOT_EXIST, vravaType, vravaName, SERVER);
         iaasWebServer.addRelations(factory.createRelation(vrava, SIBLING));
+
+        final String vravaLoadBalancername = vravaName;
+        final String vravaLoadBalancerType = "Manager Server Load Balancer Service";
+        Resource vravaPossibleLoadBalancer = factory.createResource(!CREATE_IF_NOT_EXIST, vravaLoadBalancerType,
+                vravaLoadBalancername, SERVER);
+        iaasWebServer.addRelations(factory.createRelation(vravaPossibleLoadBalancer, SIBLING, !CREATE_IF_NOT_EXIST));
 
         final String vravaTagType = "vRealize Automation Server";
         Resource vravaTag = createLogialResource(factory, vRaApplicationName, vravaTagType);
@@ -157,11 +168,6 @@ public class DiscoveryVRAIaasWeb extends DaemonDetector {
         Resource vraAppTagResource = createLogicalResource(factory, vraAppTagType, vraAppTagName);
         vraAppTagResource.addIdentifiers(vraAppTagIdentifier);
         return vraAppTagResource;
-    }
-
-    private static Resource getLoadBalancer(ObjectFactory factory, String vRaApplicationName, String lbResourceType) {
-        final String lbResourceName = VRAUtils.getFullResourceName(lbResourceType, vRaApplicationName);
-        return factory.createResource(Boolean.FALSE, lbResourceType, lbResourceName, SERVER);
     }
 
     private String executeXMLQuery(String path, File xmlFile) {
