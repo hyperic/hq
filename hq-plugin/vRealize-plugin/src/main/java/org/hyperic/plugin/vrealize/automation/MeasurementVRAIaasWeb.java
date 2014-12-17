76f4bd5ac85414dc383c6f6576ef4adb236e83dc
German Laullon <glaullon@vmware.com>
2014-12-17 11:12:29 +0100

    Avail metric for "vCO discovery" service

---
+++ hq-plugin/vRealize-plugin/src/main/java/org/hyperic/plugin/vrealize/automation/MeasurementVRAIaasWeb.java
@@ -0,0 +1,30 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hyperic.plugin.vrealize.automation;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hyperic.hq.product.MeasurementPlugin;
+import org.hyperic.hq.product.Metric;
+import org.hyperic.hq.product.MetricNotFoundException;
+import org.hyperic.hq.product.MetricUnreachableException;
+import org.hyperic.hq.product.MetricValue;
+import org.hyperic.hq.product.PluginException;
+
+/**
+ *
+ * @author glaullon
+ */
+public class MeasurementVRAIaasWeb extends MeasurementPlugin {
+
+    private static final Log log = LogFactory.getLog(MeasurementVRAIaasWeb.class);
+
+    @Override
+    public MetricValue getValue(Metric metric) throws PluginException, MetricNotFoundException, MetricUnreachableException {
+        return new MetricValue(Metric.AVAIL_UP);
+    }
+
+}
