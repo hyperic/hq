0c4f45066aa09b4b402509318e2db7275cfc4018
unknown <abarides@VMWARE-TKKD76KF.vmware.com>
2015-10-26 10:14:50 +0200

    fix json for loading control status

---
+++ hq-web/src/main/java/org/hyperic/hq/ui/action/resource/group/control/GroupControllerActionNG.java
@@ -0,0 +1,12 @@
+package org.hyperic.hq.ui.action.resource.group.control;
+
+import org.hyperic.hq.ui.action.resource.common.control.ResourceControlControllerNG;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+@Component("groupControllerActionNG")
+@Scope(value="prototype")
+public class GroupControllerActionNG extends ResourceControlControllerNG {
+
+}
