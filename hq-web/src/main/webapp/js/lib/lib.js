cff396365ddc89e6ecb44d9b023cb9b58a73b819
Yaron Borenstein <yaron@vmware.com>
2015-10-29 10:38:52 +0200

    fix header light in resources for views internal tab

--- hq-web/src/main/webapp/js/lib/lib.js
+++ hq-web/src/main/webapp/js/lib/lib.js
@@ -826,6 +826,7 @@ function activateHeaderTab(dojo){
 				l.indexOf("StatusServerController")!=-1 || 
 				l.indexOf("NewService")!=-1 ||
 				l.indexOf("NewPlatform")!=-1 ||
+				l.indexOf("BodyAction")!=-1 ||
 				l.indexOf("ToGroup")!=-1 ) {
 		dojo.addClass("resTab", "activeTab");
 	} else if ( l.indexOf("rep")!=-1 || 
